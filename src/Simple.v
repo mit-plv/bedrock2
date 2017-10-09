7fba03ece5635667dc9e5bc6306cb6a7c00cac2f
Samuel Gruetter <samuel.gruetter@epfl.ch>
2017-10-09 10:53:10 -0400

    register freshness conditions were simpler than expected, only took 40min

--- src/Simple.v
+++ src/Simple.v
@@ -169,6 +169,10 @@ Qed.
 Axiom distrib_plus_over_natToWord: forall n1 n2 sz,
   (natToWord sz n1) ^+ (natToWord sz n2) = (natToWord sz (n1 + n2)).
 
+(* TODO only if within bounds *)
+Axiom distrib_mult_over_natToWord: forall n1 n2 sz,
+  (natToWord sz n1) ^* (natToWord sz n2) = (natToWord sz (n1 * n2)).
+
 Lemma destruct_run_S: forall f s1 s3,
   run (S f) s1 = Some s3 ->
   exists s2, step s1 = Some s2 /\ run f s2 = Some s3.
@@ -201,36 +205,49 @@ Qed.
 
 Lemma compile_correct_aux: forall e lastUsed im0 prog im1 resReg rs,
   compile (S lastUsed) e = (prog, resReg) ->
+  rs 0 = Some $0 ->
   exists fuel final,
     run fuel (aboutToRun im0 (prog ++ im1) rs) = Some final /\
     final.(pc) = length (im0 ++ prog) /\
-    final.(regs) resReg = Some $(interp e).
+    final.(regs) resReg = Some $(interp e) /\
+    resReg > lastUsed /\
+    forall r, r <= lastUsed \/ r > resReg -> rs r = final.(regs) r.
 Proof.
-  intro e. induction e; introv CEq.
+  intro e. induction e; introv CEq Rs0.
   - simpl in CEq. inversions CEq.
     exists 1. simpl.
     rewrite nth_error_app2 by omega.
     replace (length im0 - length im0) with 0 by omega.
     simpl.
     eexists. apply (conj eq_refl). simpl. rewrite app_length.
-    simpl. split; [omega|].
-    unfold update. simpl.
-    rewrite Nat.eqb_refl. f_equal.
-    rewrite wplus_unit. rewrite zext_bounds. reflexivity.
+    simpl. split; [omega|]. repeat split.
+    + unfold update. simpl.
+      rewrite Nat.eqb_refl. f_equal.
+      rewrite wplus_unit. rewrite zext_bounds. reflexivity.
+    + omega.
+    + introv B. unfold update. destruct r.
+      * rewrite <- beq_nat_refl. assumption.
+      * destruct (S r =? 0) eqn: E; [ apply beq_nat_true in E; omega |].
+        destruct (S r =? S lastUsed) eqn: E2;
+          [ apply beq_nat_true in E2 | apply beq_nat_false in E2].
+        { inversions E2. omega. }
+        { reflexivity. }
   - simpl in CEq.
     destruct (compile (S lastUsed) e1) as [p1 r1] eqn: E1.
     destruct (compile (S r1) e2) as [p2 r2] eqn: E2.
     inversions CEq.
-    specialize (IHe1 _ im0 p1 (p2 ++ [Add (S r2) r1 r2] ++ im1) _ rs E1).
-    destruct IHe1 as [fuel1 [final1 [Run1 [Pc1 Regs1]]]].
+    specialize (IHe1 _ im0 p1 (p2 ++ [Add (S r2) r1 r2] ++ im1) _ rs E1 Rs0).
+    destruct IHe1 as [fuel1 [final1 [Run1 [Pc1 [Regs1 [B1 U1]]]]]].
     destruct final1 as [im pc1 rs1].
     lets Ei1: (imem_readonly_run _ _ _ Run1). simpl in Ei1. subst im.
-    specialize (IHe2 _ (im0 ++ p1) p2 ([Add (S r2) r1 r2] ++ im1) _ rs1 E2).
-    destruct IHe2 as [fuel2 [final2 [Run2 [Pc2 Regs2]]]].
+    simpl in U1.
+    lets Rs1: (U1 0). specializes Rs1; [omega|]. symmetry in Rs1. rewrite Rs0 in Rs1.
+    specialize (IHe2 _ (im0 ++ p1) p2 ([Add (S r2) r1 r2] ++ im1) _ rs1 E2 Rs1).
+    destruct IHe2 as [fuel2 [final2 [Run2 [Pc2 [Regs2 [B2 U2]]]]]].
     destruct final2 as [im pc2 rs2].
     lets Ei2: (imem_readonly_run _ _ _ Run2). simpl in Ei2. subst im.
     simpl in Pc1, Pc2. subst pc1 pc2.
-    simpl in Regs1, Regs2.
+    simpl in Regs1, Regs2, U2.
     unfold aboutToRun in Run2.
     repeat rewrite <- app_assoc in Run2.
     rewrite (reg0 rs1) in Run1. simpl in Run2.
@@ -247,9 +264,8 @@ Proof.
                          (length (im0 ++ p1 ++ p2)))
          with (Some (Add (S r2) r1 r2)).
       * rewrite Regs2.
-        (* TODO show that prog2 doesn't touch lower registers *)
-        assert (rs1 r1 = rs2 r1) as ER by admit.
-        rewrite <- ER. rewrite Regs1.
+        specialize (U2 r1). specializes U2; [omega|].
+        rewrite <- U2. rewrite Regs1.
         reflexivity.
       * replace (im0 ++ p1 ++ p2 ++ Add (S r2) r1 r2 :: im1)
            with ((im0 ++ p1 ++ p2) ++ Add (S r2) r1 r2 :: im1).
@@ -257,7 +273,7 @@ Proof.
           replace (length (im0 ++ p1 ++ p2) - length (im0 ++ p1 ++ p2)) with 0 by omega.
           simpl. reflexivity. }
         { repeat rewrite <- app_assoc. reflexivity. }
-    + simpl. split.
+    + simpl. repeat split.
       * replace (im0 ++ p1 ++ p2 ++ [Add (S r2) r1 r2])
            with ((im0 ++ p1 ++ p2) ++ [Add (S r2) r1 r2]).
         { remember (im0 ++ p1 ++ p2) as l.
@@ -268,7 +284,79 @@ Proof.
         { symmetry in E. apply beq_nat_eq in E. omega. }
         { rewrite <- beq_nat_refl.
           f_equal. apply distrib_plus_over_natToWord. }
-  - admit. (* same story but with Mult *)
+      * omega.
+      * introv B. unfold update.
+        destruct (r =? 0) eqn: E;
+          [apply beq_nat_true in E; subst; assumption | apply beq_nat_false in E].
+        destruct r; [omega|].
+        clear E.
+        destruct (S r =? S r2) eqn: E;
+          [apply beq_nat_true in E | apply beq_nat_false in E].
+        { inversions E. omega. (* contradiction *) }
+        { rewrite U1 by omega. rewrite U2 by omega. reflexivity. }
+  - (* TODO code duplication alert! *)
+    simpl in CEq.
+    destruct (compile (S lastUsed) e1) as [p1 r1] eqn: E1.
+    destruct (compile (S r1) e2) as [p2 r2] eqn: E2.
+    inversions CEq.
+    specialize (IHe1 _ im0 p1 (p2 ++ [Mul (S r2) r1 r2] ++ im1) _ rs E1 Rs0).
+    destruct IHe1 as [fuel1 [final1 [Run1 [Pc1 [Regs1 [B1 U1]]]]]].
+    destruct final1 as [im pc1 rs1].
+    lets Ei1: (imem_readonly_run _ _ _ Run1). simpl in Ei1. subst im.
+    simpl in U1.
+    lets Rs1: (U1 0). specializes Rs1; [omega|]. symmetry in Rs1. rewrite Rs0 in Rs1.
+    specialize (IHe2 _ (im0 ++ p1) p2 ([Mul (S r2) r1 r2] ++ im1) _ rs1 E2 Rs1).
+    destruct IHe2 as [fuel2 [final2 [Run2 [Pc2 [Regs2 [B2 U2]]]]]].
+    destruct final2 as [im pc2 rs2].
+    lets Ei2: (imem_readonly_run _ _ _ Run2). simpl in Ei2. subst im.
+    simpl in Pc1, Pc2. subst pc1 pc2.
+    simpl in Regs1, Regs2, U2.
+    unfold aboutToRun in Run2.
+    repeat rewrite <- app_assoc in Run2.
+    rewrite (reg0 rs1) in Run1. simpl in Run2.
+    lets R12: (concat_run _ _ _ _ _ Run1 Run2).
+    evar (iF: list Instruction). evar (pF: nat). evar (rF: Regs).
+    exists (S (fuel1 + fuel2)).
+    exists (mkMachineState iF pF rF).
+    subst iF pF rF.
+    repeat rewrite <- app_assoc.
+    split.
+    + eapply run_snoc; [ eapply R12 |].
+      simpl.
+      replace (nth_error (im0 ++ p1 ++ p2 ++ Mul (S r2) r1 r2 :: im1)
+                         (length (im0 ++ p1 ++ p2)))
+         with (Some (Mul (S r2) r1 r2)).
+      * rewrite Regs2.
+        specialize (U2 r1). specializes U2; [omega|].
+        rewrite <- U2. rewrite Regs1.
+        reflexivity.
+      * replace (im0 ++ p1 ++ p2 ++ Mul (S r2) r1 r2 :: im1)
+           with ((im0 ++ p1 ++ p2) ++ Mul (S r2) r1 r2 :: im1).
+        { rewrite nth_error_app2 by omega.
+          replace (length (im0 ++ p1 ++ p2) - length (im0 ++ p1 ++ p2)) with 0 by omega.
+          simpl. reflexivity. }
+        { repeat rewrite <- app_assoc. reflexivity. }
+    + simpl. repeat split.
+      * replace (im0 ++ p1 ++ p2 ++ [Mul (S r2) r1 r2])
+           with ((im0 ++ p1 ++ p2) ++ [Mul (S r2) r1 r2]).
+        { remember (im0 ++ p1 ++ p2) as l.
+          rewrite app_length. simpl. omega. }
+        { repeat rewrite <- app_assoc. reflexivity. }
+      * unfold update. clear.
+        destruct (S r2 =? 0) eqn: E.
+        { symmetry in E. apply beq_nat_eq in E. omega. }
+        { rewrite <- beq_nat_refl.
+          f_equal. apply distrib_mult_over_natToWord. }
+      * omega.
+      * introv B. unfold update.
+        destruct (r =? 0) eqn: E;
+          [apply beq_nat_true in E; subst; assumption | apply beq_nat_false in E].
+        destruct r; [omega|].
+        clear E.
+        destruct (S r =? S r2) eqn: E;
+          [apply beq_nat_true in E | apply beq_nat_false in E].
+        { inversions E. omega. (* contradiction *) }
+        { rewrite U1 by omega. rewrite U2 by omega. reflexivity. }
 Qed.
 
 Lemma compile_correct: forall e prog resReg,
@@ -277,8 +365,8 @@ Lemma compile_correct: forall e prog resReg,
     run fuel (initialState prog) = Some final /\ final.(regs) resReg = Some $(interp e).
 Proof.
   introv C.
-  lets E: (compile_correct_aux _ _ [] prog [] _ (initialState prog).(regs) C).
-  destruct E as [fuel [final [R [Pc Regs]]]].
+  lets E: (compile_correct_aux _ _ [] prog [] _ (initialState prog).(regs) C eq_refl).
+  destruct E as [fuel [final [R [Pc [Regs [B U]]]]]].
   exists fuel final.
   unfold aboutToRun in R.
   rewrite app_nil_l, app_nil_r in *.
