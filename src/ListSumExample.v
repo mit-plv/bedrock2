9b0bbf0f832921a74bf1fdce88d8bcc7941a0562
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-06-14 12:16:12 -0400

    only axioms of listsum_compiled_correctly are
    functional_extensionality_dep and JMeq_eq, will have to figure out
    where they sneaked in

--- src/ListSumExample.v
+++ src/ListSumExample.v
@@ -124,6 +124,21 @@ Definition initialize_with_wXLEN_list_H{Bw : RiscvBitWidths}(l: list (word wXLEN
       None.
 *)
 
+Definition in_range_dec: forall {w: nat} (addr: word w) (alignment start size: nat),
+    Decidable (Memory.in_range addr alignment start size).
+Proof.
+  intros. unfold Memory.in_range. apply dec_and.
+Defined.
+
+Definition initialize_with_wXLEN_list_H{Bw : RiscvBitWidths}(l: list (word wXLEN))
+           (offset: word wXLEN) : Memory.mem :=
+  fun (a: word wXLEN) =>
+    if dec (Memory.in_range a wXLEN_in_bytes #offset (wXLEN_in_bytes * length l)) then
+      nth_error l (#(a ^- offset) / wXLEN_in_bytes)
+    else
+      None.
+
+(*
 Definition initialize_with_wXLEN_list_H{Bw : RiscvBitWidths}(l: list (word wXLEN))
            (offset: word wXLEN) : Memory.mem :=
   fun (a: word wXLEN) =>
@@ -131,6 +146,7 @@ Definition initialize_with_wXLEN_list_H{Bw : RiscvBitWidths}(l: list (word wXLEN
       nth_error l (#(a ^- offset) / wXLEN_in_bytes)
     else
       None.
+ *)
 
 Lemma invert_initialize_Some: forall l offset w a,
     Memory.read_mem a (initialize_with_wXLEN_list_H l offset) = Some w ->
@@ -139,8 +155,27 @@ Lemma invert_initialize_Some: forall l offset w a,
     #a mod 4 = 0.
 Proof.
   intros. unfold Memory.read_mem, initialize_with_wXLEN_list_H in *.
-  repeat (destruct_one_match_hyp; try discriminate). auto.
+  repeat (destruct_one_match_hyp; try discriminate).
+  unfold Memory.in_range in *. clear E0. intuition idtac.
 Qed.
+
+(*
+Lemma invert_initialize_Some': forall l offset w a,
+    Memory.read_mem a (initialize_with_wXLEN_list_H l offset) = Some w ->
+    nth_error l (#(a ^- offset) / wXLEN_in_bytes) = Some w /\
+    #offset <= #a /\
+    #a + wXLEN_in_bytes <= #offset + wXLEN_in_bytes * length l /\
+    #a mod 4 = 0.
+Proof.
+  intros. unfold Memory.read_mem, initialize_with_wXLEN_list_H in *.
+  repeat (destruct_one_match_hyp; try discriminate).
+  assert (# (a ^- offset) / wXLEN_in_bytes < length l). {
+    apply nth_error_Some. congruence.
+  }
+  
+  repeat split; auto.
+Qed.
+ *)
 
 Definition initialMemH(l: list nat): Memory.mem :=
   initialize_with_wXLEN_list_H (mk_input l) $input_base.
@@ -157,21 +192,76 @@ Definition listsum_res_H(fuel: nat)(l: list nat): option (word 32) :=
 Eval vm_compute in (listsum_res_H 40 [3; 7; 6]).
 
 Lemma store_word_list_contains_initialize: forall words offset m,
+    #offset mod 4 = 0 ->
+    #offset + 4 * length words <= Memory.memSize m ->
     FlatToRiscv.containsMem (Memory.store_word_list words offset m)
                             (initialize_with_wXLEN_list_H words offset).
 Proof.
-Admitted.
+  unfold FlatToRiscv.containsMem.
+  unfold FlatToRiscv.loadWordwXLEN, wXLEN, wXLEN_in_bytes,
+     RiscvBitWidths.bitwidth, RiscvBitWidths32.
+  intros.
+  unfold Memory.read_mem, initialize_with_wXLEN_list_H in H1.
+  repeat (destruct_one_match_hyp; try discriminate).
+  clear E0.
+  unfold Memory.in_range in *.
+  rewrite Memory.store_word_list_preserves_memSize.
+  Memory.destruct_list_length.
+  - apply FlatToRiscv.nth_error_nil_Some in H1. contradiction.
+  - unfold wXLEN_in_bytes, RiscvBitWidths.bitwidth, RiscvBitWidths32 in *.
+    (* TODO why doesn't omega work here? *)
+    intuition (try (eapply le_trans; eassumption)).
+    apply nth_error_nth with (d := $0) in H1. rewrite <- H1.
+    pose proof pow2_wXLEN_4 as P. unfold wXLEN, RiscvBitWidths.bitwidth, RiscvBitWidths32 in P.
+    apply Memory.loadWord_inside_store_word_list;
+      unfold Memory.in_range;
+      unfold Memory.valid_addr;
+      intuition omega.
+Qed.
+
+Lemma memory_size_infJalMem:  Memory.memSize (w := wXLEN) infJalMem = memory_size.
+Proof.
+  pose proof @Memory.store_word_list_preserves_memSize as R.
+  unfold infJalMem.
+  unfold wXLEN, bitwidth, RiscvBitWidths.bitwidth, RiscvBitWidths32 in R|-*; rewrite R.
+  unfold zero_mem, Memory.memSize, mem_is_Memory.
+  apply const_mem_mem_size.
+  - reflexivity.
+  - change 32 with (10 + 22). rewrite Nat.pow_add_r.
+    pose proof (one_le_pow2 22).
+    replace memory_size with (memory_size * 1) by omega.
+    forget (pow2 22) as x.
+    apply Nat.mul_le_mono; cbv; omega.
+Qed.  
+
+Lemma memory_size_without_instructions: forall l,
+  Memory.memSize (machineMem (initialRiscvMachine_without_instructions l)) = memory_size.
+Proof.
+  intros.
+  unfold initialRiscvMachine_without_instructions, putProgram.
+  cbv [machineMem with_pc with_nextPC with_machineMem].
+  pose proof @Memory.store_word_list_preserves_memSize as R.
+  unfold wXLEN, bitwidth, RiscvBitWidths.bitwidth, RiscvBitWidths32 in R|-*; rewrite R.
+  apply memory_size_infJalMem.
+Qed.
+
+Lemma cons_length: forall (A: Type) (a: A) (l: list A),
+    length (a :: l) = S (length l).
+Proof.
+  intros. reflexivity.
+Qed.
 
 (* Note: the high-level program also runs on a memory of size memory_size and also uses just
    32-bit words, so you might have overflows in the high-level program, and the compiler does
    not help you prevent these, but it provably does not introduce any new overflow or memory
    size problems. *)
 Lemma listsum_compiled_correctly: forall l fuelH res,
+    input_base + 4 * S (length l) <= memory_size ->
     listsum_res_H fuelH l = Some res ->
     exists fuelL, listsum_res fuelL l = res.
 Proof.
   intros.
-  unfold listsum_res_H, evalH in H. destruct_one_match_hyp; try discriminate.
+  unfold listsum_res_H, evalH in H0. destruct_one_match_hyp; try discriminate.
   destruct p as [finalRegsH finalMemH].
   unfold listsum_res, listsum_final.
   pose proof exprImp2Riscv_correct as Q.
@@ -185,45 +275,42 @@ Proof.
   - match goal with
     | |- context [length ?x] => let r := eval cbv in (length x) in change (length x) with r
     end.
-    unfold initialRiscvMachine_without_instructions, putProgram.
-    cbv [machineMem with_pc with_nextPC with_machineMem].
-    pose proof @Memory.store_word_list_preserves_memSize as R.
-    unfold wXLEN, bitwidth, RiscvBitWidths.bitwidth, RiscvBitWidths32 in R|-*; rewrite R.
-    unfold infJalMem. rewrite R.
-    unfold zero_mem, Memory.memSize, mem_is_Memory.
-    rewrite const_mem_mem_size.
-    + apply Nat.le_trans with (m := input_base).
-      * cbv. omega.
-      * unfold input_base, memory_size. omega.
-    + reflexivity.
-    + change 32 with (10 + 22). rewrite Nat.pow_add_r.
-      pose proof (one_le_pow2 22).
-      replace memory_size with (memory_size * 1) by omega.
-      forget (pow2 22) as x.
-      apply Nat.mul_le_mono; cbv; omega.
+    rewrite memory_size_without_instructions.
+    apply Nat.le_trans with (m := input_base).
+    + cbv. omega.
+    + unfold input_base, memory_size. omega.
   - match goal with
     | |- _ _ _ ?x => let x' := eval cbv in x in change x with x'
     end.
     unfold initialMemH, FlatToRiscv.mem_inaccessible.
     intros.
-    apply invert_initialize_Some in H0. intuition idtac.
+    apply invert_initialize_Some in H1. intuition idtac.
     unfold Memory.not_in_range.
     right.
     unfold input_base in *.
     unfold wXLEN, RiscvBitWidths32, RiscvBitWidths.bitwidth in *.
     assert (# (natToWord 32 512) = 512) as F by (abstract reflexivity).
-    rewrite F in H0.
+    rewrite F in H1.
     omega.
   - unfold initialRiscvMachine_without_instructions, machineMem, initialMemH.
     unfold wXLEN, RiscvBitWidths32, RiscvBitWidths.bitwidth in *.
-    forget (natToWord 32 input_base) as offset.
-    forget (mk_input l) as words.
-    apply store_word_list_contains_initialize.
+    apply store_word_list_contains_initialize; [ reflexivity | ].
+    unfold mem, ListMemoryNatAddr.mem.
+    rewrite memory_size_infJalMem.
+    unfold mk_input.
+    rewrite wordToNat_natToWord_idempotent'.
+    + rewrite cons_length. rewrite map_length. exact H.
+    + change 32 with (10 + 22). rewrite Nat.pow_add_r.
+      pose proof (one_lt_pow2 21).
+      replace input_base with (input_base * 1) by omega.
+      forget (pow2 22) as x.
+      apply Nat.mul_lt_mono; cbv; omega.
   - exists fuelL.
     unfold initialRiscvMachine, putProgram.
-    apply P. apply H.
+    apply P. apply H0.
 Qed.
 
+Print Assumptions listsum_compiled_correctly.
 
 Definition sum_gallina(l: list nat): nat := List.fold_right plus 0 l.
 
@@ -235,10 +322,10 @@ Admitted.
 
 
 Lemma listsum_will_run_correctly: forall l,
-    (* TODO: bound on length of l *)
+    input_base + 4 * S (length l) <= memory_size ->
     exists fuelL, listsum_res fuelL l = $(sum_gallina l).
 Proof.
   intros.
   destruct (hl_listsum_correct l) as [fuelH E].
-  apply (listsum_compiled_correctly l fuelH _ E).
+  refine (listsum_compiled_correctly l fuelH _ H E).
 Qed.
