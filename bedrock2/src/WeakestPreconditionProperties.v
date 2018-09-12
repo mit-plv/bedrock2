2ad2788f258576bcffe3a576f215204aa331622d
Gregory Malecha <gmalecha@gmail.com>
2018-09-11 20:49:10 -0400

    fixing the broken proofs in the bedrock2 directory
    - the automation needs some work right now.

--- bedrock2/src/WeakestPreconditionProperties.v
+++ bedrock2/src/WeakestPreconditionProperties.v
@@ -3,6 +3,16 @@ Require bedrock2.WeakestPrecondition.
 
 Require Import Coq.Classes.Morphisms.
 
+Inductive Roption {T} : option T -> option T -> Prop :=
+| Rnone {x} : Roption None x
+| Rsome {x} : Roption (Some x) (Some x).
+
+Global Instance Reflexive_Roption {T} : Reflexive (@Roption T).
+Proof. compute. destruct x; constructor. Qed.
+
+Global Instance Transitive_Roption {T} : Transitive (@Roption T).
+Proof. compute. destruct 1; inversion 1; subst; auto. constructor. Qed.
+
 Section WeakestPrecondition.
   Context {p : unique! Semantics.parameters}.
 
@@ -18,23 +28,30 @@ Section WeakestPrecondition.
   Global Instance Proper_get : Proper (pointwise_relation _ (pointwise_relation _ ((pointwise_relation _ Basics.impl) ==> Basics.impl))) WeakestPrecondition.get.
   Proof. cbv [WeakestPrecondition.get]; cbv [Proper respectful pointwise_relation Basics.impl]; firstorder idtac. Qed.
 
+  Global Instance Proper_global : Proper ((pointwise_relation _ Roption) ==> (pointwise_relation _ ((pointwise_relation _ Basics.impl) ==> Basics.impl))) WeakestPrecondition.global.
+  Proof.
+    cbv [WeakestPrecondition.global]; cbv [Proper respectful pointwise_relation Basics.impl]; firstorder idtac.
+    destruct (H a); try congruence.
+    injection H1; clear H1; intros; subst.
+    eexists; split; eauto.
+  Qed.
+
   Global Instance Proper_load : Proper (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ ((pointwise_relation _ Basics.impl) ==> Basics.impl)))) WeakestPrecondition.load.
   Proof. cbv [WeakestPrecondition.load]; cbv [Proper respectful pointwise_relation Basics.impl]; firstorder idtac. Qed.
 
   Global Instance Proper_store : Proper (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ ((pointwise_relation _ Basics.impl) ==> Basics.impl))))) WeakestPrecondition.store.
   Proof. cbv [WeakestPrecondition.load]; cbv [Proper respectful pointwise_relation Basics.impl]; firstorder idtac. Qed.
 
-(*
   Global Instance Proper_expr
-  : Proper (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ ((pointwise_relation _ Basics.impl) ==> Basics.impl)))) WeakestPrecondition.expr.
+  : Proper (pointwise_relation _ Roption ==> (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ ((pointwise_relation _ Basics.impl) ==> Basics.impl))))) WeakestPrecondition.expr.
   Proof.
     cbv [Proper respectful pointwise_relation Basics.impl]; ind_on Syntax.expr.expr;
       cbn in *; intuition (try typeclasses eauto with core).
     eapply Proper_literal; eauto.
     eapply Proper_get; eauto.
+    eapply Proper_global; eauto.
     { eapply IHa1; eauto; intuition idtac. eapply Proper_load; eauto using Proper_load. }
   Qed.
-*)
 
   Global Instance Proper_list_map {A B} :
     Proper ((pointwise_relation _ (pointwise_relation _ Basics.impl ==> Basics.impl)) ==> pointwise_relation _ (pointwise_relation _ Basics.impl ==> Basics.impl)) (WeakestPrecondition.list_map (A:=A) (B:=B)).
@@ -43,19 +60,19 @@ Section WeakestPrecondition.
       cbn in *; intuition (try typeclasses eauto with core).
   Qed.
 
-(* 
   Global Instance Proper_cmd :
     Proper (
      (pointwise_relation _ (Basics.flip Basics.impl)) ==> (
      (pointwise_relation _ Basics.impl) ==> (
      (pointwise_relation _ (pointwise_relation _ Basics.impl)) ==> (
+     (pointwise_relation _ Roption) ==> (
      (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ ((pointwise_relation _ (pointwise_relation _ Basics.impl))) ==> Basics.impl)))) ==>
      pointwise_relation _ (
      pointwise_relation _ (
      pointwise_relation _ (
      pointwise_relation _ (
      (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ Basics.impl))) ==>
-     Basics.impl))))))))) WeakestPrecondition.cmd.
+     Basics.impl)))))))))) WeakestPrecondition.cmd.
   Proof.
     cbv [Proper respectful pointwise_relation Basics.flip Basics.impl]; ind_on Syntax.cmd.cmd;
       cbn in *; intuition (try typeclasses eauto with core).
@@ -67,7 +84,7 @@ Section WeakestPrecondition.
     { eapply IHa1; try solve [typeclasses eauto with core]. eapply H.
       intros. eapply IHa2; try solve [typeclasses eauto with core]. eapply H.
       eauto. }
-    { destruct H4 as (?&?&?&?&?&HH).
+    { destruct H5 as (?&?&?&?&?&HH).
       eassumption || eexists.
       eassumption || eexists.
       eassumption || eexists.
@@ -79,17 +96,18 @@ Section WeakestPrecondition.
       specialize (HH X Y Z T W).
       eapply Proper_expr; eauto; cbv [pointwise_relation Basics.impl]; intuition eauto 2.
       eapply IHa; eauto; cbn; intros.
-      destruct H7 as (v'&H7); exists v'.
+      destruct H8 as (v'&H8); exists v'.
       intuition eauto. }
     { eapply Proper_list_map; eauto; cbv [respectful pointwise_relation Basics.impl]; intuition eauto 2.
       { eapply Proper_expr; eauto. }
-      { eapply H2; eauto; firstorder eauto. } }
+      { eapply Proper_global; eauto. do 2 red. intros.
+        eapply H3; eauto; firstorder eauto. } }
     { eapply Proper_list_map; eauto; cbv [respectful pointwise_relation Basics.impl].
       { eapply Proper_expr; eauto. }
       intros.
-      edestruct H5.
+      edestruct H6.
       split; [solve [eauto]|]; intros.
-      destruct (H7 ltac:(eauto)); intuition eauto. }
+      destruct (H8 ltac:(eauto)); intuition eauto. }
   Qed.
 
   Global Instance Proper_func :
@@ -97,6 +115,7 @@ Section WeakestPrecondition.
      (pointwise_relation _ (Basics.flip Basics.impl)) ==> (
      (pointwise_relation _ Basics.impl) ==> (
      (pointwise_relation _ (pointwise_relation _ Basics.impl)) ==> (
+     (pointwise_relation _ Roption) ==>
      (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ ((pointwise_relation _ (pointwise_relation _ Basics.impl))) ==> Basics.impl)))) ==>
      pointwise_relation _ (
      pointwise_relation _ (
@@ -107,7 +126,7 @@ Section WeakestPrecondition.
   Proof.
     cbv [Proper respectful pointwise_relation Basics.flip Basics.impl  WeakestPrecondition.func]; intros.
     destruct a. destruct p0.
-    destruct H4; intuition idtac.
+    destruct H5; intuition idtac.
     eexists.
     split.
     eauto.
@@ -130,6 +149,7 @@ Section WeakestPrecondition.
      (pointwise_relation _ (Basics.flip Basics.impl)) ==> (
      (pointwise_relation _ Basics.impl) ==> (
      (pointwise_relation _ (pointwise_relation _ Basics.impl)) ==> (
+     pointwise_relation _ Roption ==>
      (pointwise_relation _ (
      (pointwise_relation _ (
      pointwise_relation _ (
@@ -138,10 +158,10 @@ Section WeakestPrecondition.
      (pointwise_relation _ (pointwise_relation _ (pointwise_relation _ Basics.impl))) ==>
      Basics.impl))))))))))) WeakestPrecondition.call.
   Proof.
-    cbv [Proper respectful pointwise_relation Basics.impl]; ind_on (list (Syntax.funname * (list Syntax.varname * list Syntax.varname * Syntax.cmd.cmd)));
+    cbv [Proper respectful pointwise_relation Basics.impl]; ind_on (list (Semantics.word * (list Syntax.varname * list Syntax.varname * Syntax.cmd.cmd)));
       cbn in *; intuition (try typeclasses eauto with core).
     destruct a.
-    destruct (Semantics.funname_eqb f a1); eauto.
+    destruct (Semantics.word_eqb w a1); eauto.
     eapply Proper_func; 
       cbv [Proper respectful pointwise_relation Basics.flip Basics.impl  WeakestPrecondition.func];
       eauto.
@@ -152,6 +172,7 @@ Section WeakestPrecondition.
      (pointwise_relation _ (Basics.flip Basics.impl)) ==> (
      (pointwise_relation _ Basics.impl) ==> (
      (pointwise_relation _ (pointwise_relation _ Basics.impl)) ==> (
+     pointwise_relation _ Roption ==>
      pointwise_relation _ (
      pointwise_relation _ (
      pointwise_relation _ (
@@ -171,5 +192,5 @@ Section WeakestPrecondition.
     try solve [typeclasses eauto with core].
     eauto.
   Qed.
-*)
+
 End WeakestPrecondition.
