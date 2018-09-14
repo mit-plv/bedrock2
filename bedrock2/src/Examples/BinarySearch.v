38869015f50e1dd85da04e6d749c06266bbbcf33
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-09-14 10:01:42 -0400

    move Import bedrock2.NotationsInConstr. inside section so that we can print AST

--- bedrock2/src/Examples/BinarySearch.v
+++ bedrock2/src/Examples/BinarySearch.v
@@ -1,10 +1,12 @@
 Require Import bedrock2.Macros bedrock2.Syntax.
-Require Import bedrock2.StringNamesSyntax bedrock2.BasicALU bedrock2.NotationsInConstr.
+Require Import bedrock2.StringNamesSyntax bedrock2.BasicALU.
+Require bedrock2.NotationsInConstr.
 
 Import BinInt String.
 Local Open Scope string_scope. Local Open Scope Z_scope. Local Open Scope list_scope.
 
 Section bsearch.
+  Import bedrock2.NotationsInConstr.
   Context {p : unique! StringNamesSyntax.parameters} {b : BasicALU.operations}.
   Local Coercion literal (z : Z) : expr := expr.literal z.
   Local Coercion var (x : String.string) : expr := expr.var x.
