989cc06ee8b7fe00cd244ec75cf041ec3fb001e6
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-09-20 18:38:29 -0400

    start checker correctness lemma

--- compiler/src/RegAlloc2.v
+++ compiler/src/RegAlloc2.v
@@ -6,6 +6,8 @@ Require Import riscv.Utility.
 Require Import compiler.Op.
 Require Import compiler.util.Map.
 Require Import compiler.util.Set.
+Require Import compiler.Memory.
+Require Import compiler.util.Tactics.
 
 Section TODO.
   Context {K V: Type}.
@@ -14,9 +16,23 @@ Section TODO.
   Axiom get_in_domain: forall k m, k \in (domain m) -> exists v, get m k = Some v.
   Axiom domain_put: forall k v m, domain (put m k v) = union (domain m) (singleton_set k).
   *)
+
+  (* operations *)
   Axiom reverse_get: map K V -> V -> option K.
   Axiom intersect_map: map K V -> map K V -> map K V.
+  Axiom remove_by_value: map K V -> V -> map K V.
+
+  (* specs *)
   Axiom put_put_same: forall k v1 v2 m, put (put m k v1) k v2 = put m k v2.
+  Axiom reverse_reverse_get: forall k v m, reverse_get m v = Some k -> get m k = Some v.
+  Axiom get_in_range: forall k v m, get m k = Some v -> v \in range m.
+  Axiom remove_by_value_spec: forall k v m, get (remove_by_value m v) k <> Some v.
+
+  (* TODO some of this should go into state calculus *)
+  (* probably derived *)
+  Axiom not_in_range_of_remove_by_value: forall m v, ~ v \in range (remove_by_value m v).
+  Axiom extends_remove_by_value: forall m v, extends m (remove_by_value m v).
+
 End TODO.
 
 Local Notation "'bind_opt' x <- a ; f" :=
@@ -64,7 +80,11 @@ Section RegAlloc.
   Definition mappings :=
     fix rec(m: map impvar srcvar)(s: astmt): map impvar srcvar :=
       match s with
-      | ASLoad x x' _ | ASLit x x' _ | ASOp x x' _ _ _ | ASSet x x' _ => put m x' x
+      | ASLoad x x' _ | ASLit x x' _ | ASOp x x' _ _ _ | ASSet x x' _ =>
+          (* if several impvars store the value of x, they won't all store the value of
+             x after the update, but only x' will, because only x' is written in the target
+             program, so we first have to remove the others *)
+          put (remove_by_value m x) x' x
       | ASStore a v => m
       | ASIf cond s1 s2 => intersect_map (rec m s1) (rec m s2)
       | ASLoop s1 cond s2 =>
@@ -90,11 +110,13 @@ Section RegAlloc.
     induction s; intros; simpl in *;
       try reflexivity;
       try (subst; apply put_put_same).
+(*
     {
       erewrite IHs1 with (m2 := m2); [erewrite IHs2 with (m2 := m2)|].
       subst.
       - admit. (* ok *)
       - symmetry. eapply IHs2. (* stuck in a loop *)
+*)
   Abort.
 
   Definition checker :=
@@ -153,4 +175,109 @@ Section RegAlloc.
 
   (* claim: for all astmt a, if checker succeeds and returns s', then
      (erase a) behaves the same as s' *)
+
+  Context {mword: Set}.
+  Context {MW: MachineWidth mword}.
+  Context {srcStateMap: MapFunctions srcvar mword}.
+  Context {impStateMap: MapFunctions impvar mword}.
+  Context {srcFuncMap: MapFunctions func (list srcvar * list srcvar * stmt)}.
+  Context {impFuncMap: MapFunctions func (list impvar * list impvar * stmt')}.
+
+  Definition eval: nat -> map srcvar mword -> mem -> stmt -> option (map srcvar mword * mem)
+    := eval_stmt _ _ empty_map.
+
+  Definition eval': nat -> map impvar mword -> mem -> stmt' -> option (map impvar mword * mem)
+    := eval_stmt _ _ empty_map.
+
+  (*
+  Definition states_compat(st: map srcvar mword)(r: map impvar srcvar)(st': map impvar mword) :=
+    forall (x: srcvar) (w: mword),
+      (* TODO restrict to live variables *)
+      get st x = Some w ->
+      exists (x': impvar), get r x' = Some x /\ get st' x' = Some w.
+  *)
+
+  Definition states_compat(st: map srcvar mword)(r: map impvar srcvar)(st': map impvar mword) :=
+    forall (x: srcvar) (x': impvar),
+      get r x' = Some x ->
+      forall w,
+        get st x = Some w ->
+        get st' x' = Some w.
+
+  Lemma states_compat_put: forall st1 st1' v x x' r,
+      ~ x \in (range r) ->
+      states_compat st1 r st1' ->
+      states_compat (put st1 x v) (put r x' x) (put st1' x' v).
+  Proof.
+    unfold states_compat.
+    intros.
+    rewrite get_put.
+    do 2 match goal with
+    | H: get (put _ _ _) _ = _ |- _ => rewrite get_put in H
+    end.
+    destruct_one_match; clear E.
+    - subst.
+      replace x0 with x in H2 by congruence.
+      destruct_one_match_hyp; [assumption|contradiction].
+    - destruct_one_match_hyp.
+      + subst.
+        apply get_in_range in H1.
+        contradiction.
+      + eauto.
+  Qed.
+
+  Lemma states_compat_extends: forall st st' r1 r2,
+      extends r1 r2 ->
+      states_compat st r1 st' ->
+      states_compat st r2 st'.
+  Proof.
+    unfold states_compat. eauto.
+  Qed.
+
+  Lemma checker_correct: forall n r st1 st1' m1 st2 m2 s annotated s',
+      erase annotated = s ->
+      checker r annotated = Some s' ->
+      states_compat st1 r st1' ->
+      eval n st1 m1 s = Some (st2, m2) ->
+      exists st2',
+        eval' n st1' m1 s' = Some (st2', m2) /\
+        states_compat st2 (mappings r annotated) st2'.
+  Proof.
+    induction n; intros; [
+      match goal with
+      | H: eval 0 _ _ _ = Some _ |- _ => solve [inversion H]
+      end
+    |].
+    unfold eval, eval' in *.
+    invert_eval_stmt;
+      try destruct_pair_eqs;
+      match goal with
+      | H: erase ?s = _ |- _ =>
+        destruct s;
+        inversion H;
+        subst;
+        clear H
+      end;
+      subst;
+      simpl in *;
+      repeat (destruct_one_match_hyp; [|discriminate]);
+      repeat match goal with
+             | H: Some _ = Some _ |- _ => inversion H; subst; clear H
+             | H: reverse_get _ _ = Some _ |- _ => apply reverse_reverse_get in H
+             | H: states_compat _ _ _ |- _ => erewrite H by eassumption
+             end;
+      repeat match goal with
+             | H: states_compat _ _ _ |- _ => erewrite H by eassumption
+             | H: _ = _ |- _ => rewrite H
+             end.
+    {
+      eexists; split; [reflexivity|].
+      apply states_compat_put.
+      - apply not_in_range_of_remove_by_value.
+      - eapply states_compat_extends; [|eassumption].
+        apply extends_remove_by_value.
+    }
+
+  Abort.
+
 End RegAlloc.
