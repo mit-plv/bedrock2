086946ea19d56a3c341ee30a8898823548b02141
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-09-26 20:33:54 -0400

    move

--- bedrock2/src/Examples/Demos.v
+++ bedrock2/src/Examples/Demos.v
@@ -6,7 +6,7 @@ Import BinInt String.
 Local Open Scope string_scope. Local Open Scope Z_scope. Local Open Scope list_scope.
 
 
-Section bsearch.
+Section Demos.
 
   (* note: this coercion must have its own p, because varname depends on p, and the
      coercion only kicks in if p can be chosen freely *)
@@ -42,7 +42,7 @@ Section bsearch.
       }}
     }}
   )).
-End bsearch.
+End Demos.
 
 Require bedrock2.BasicC64Syntax.
 
