9a690d30d7476ff699bf1ba3d9e3ba32c6ad37d8
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-09-26 20:25:34 -0400

    rearrange

--- bedrock2/src/Examples/BinarySearch.v
+++ bedrock2/src/Examples/BinarySearch.v
@@ -5,23 +5,31 @@ Require bedrock2.NotationsInConstr.
 Import BinInt String.
 Local Open Scope string_scope. Local Open Scope Z_scope. Local Open Scope list_scope.
 
-Class bsearch_names {p: unique! Syntax.parameters} := {
-  left : varname;
-  right : varname;
-  target : varname;
-  mid : varname;
-  tmp : varname;
-}.
 
 Section bsearch.
-  Import bedrock2.NotationsInConstr.
+
+  (* note: this coercion must have its own p, because varname depends on p, and the
+     coercion only kicks in if p can be chosen freely *)
   Local Coercion var{p : unique! Syntax.parameters}(x : @varname p): @expr.expr p :=
     @expr.var p x.
 
   Context {p : unique! Syntax.parameters} {b : BasicALU.operations}.
+
+  (* note: this coercion must use the section's p, because its argument z does not
+     allow Coq to infer p *)
   Local Coercion literal (z : Z) : expr := expr.literal z.
 
+  Class bsearch_names := {
+    left : varname;
+    right : varname;
+    target : varname;
+    mid : varname;
+    tmp : varname;
+  }.
+
   Context {names: unique! bsearch_names}.
+
+  Import bedrock2.NotationsInConstr.
 
   Definition bsearch := ((left::right::target::nil), (left::nil), bedrock_func_body:(
     while (left < right) {{
