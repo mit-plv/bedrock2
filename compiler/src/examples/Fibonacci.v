0e9e66b22c30e418ec6806c3370caa5ba29dfbbf
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-10-30 10:25:03 -0400

    print regalloced fib

--- compiler/src/examples/Fibonacci.v
+++ compiler/src/examples/Fibonacci.v
@@ -88,6 +88,44 @@ Definition resVar :=
 Definition fib_riscv0(n: Z): list Instruction :=
   compileFunc (fib_ExprImp n).
 
+Module PrintFlatImp.
+  Import FlatImp bopname.
+  Eval cbv in (flatten (fib_ExprImp 6)).
+End PrintFlatImp.
+
+Module PrintRegAllocAnnotatedFlatImp.
+  Import bopname.
+
+  Notation "a ; b" := (ASSeq _ _ _ a b) (only printing, right associativity,
+      at level 50, format "a ; '//' b").
+  Notation "'$x' a '($r' b ')' = c" := (ASLit _ _ _ a b c) (only printing,
+      at level 40, format "'$x' a '($r' b ')'  =  c").
+  Notation "'$x' a '($r' b ')' = '$x' c" := (ASSet _ _ _ a b c) (only printing,
+      at level 40, format "'$x' a '($r' b ')'  =  '$x' c").
+  Notation "'$x' a '($r' b ')' = op '$x' c '$x' d" := (ASOp _ _ _ a b op c d) (only printing,
+      at level 40, format "'$x' a '($r' b ')'  =  op  '$x' c  '$x' d").
+  Notation "'loop' a 'breakUnless' '$x' cond b" := (ASLoop _ _ _ a cond b)
+      (only printing, at level 50, a at level 40,
+       format "'loop' '[v ' '//' a '//' 'breakUnless'  '$x' cond '//' b ']'").
+
+  (* TODO
+       ASLoad(x: srcvar)(x': impvar)(a: srcvar)
+       ASStore(a: srcvar)(v: srcvar)
+       ASIf(cond: srcvar)(bThen bElse: astmt)
+       ASSkip
+       ASCall(binds: list (srcvar * impvar))(f: func)(args: list srcvar). *)
+
+  Goal False.
+    let r := eval cbv in
+      (regalloc var var func
+                Register0
+                riscvRegisters
+                empty_map
+                (flatten (fib_ExprImp 6))
+                (@singleton_set var _ Demos.Fibonacci.b)) in idtac r.
+  Abort.
+End PrintRegAllocAnnotatedFlatImp.
+
 Time Definition fib6_riscv := Eval vm_compute in fib_riscv0 6.
 (* empty list (failure) if do_regalloc is set to true *)
 
