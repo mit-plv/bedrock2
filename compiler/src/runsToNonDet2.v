d0d3efe1f7f04909af47f50c80e68fd4abc93af8
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-11-09 13:32:48 -0500

    runsToNonDet

--- compiler/src/runsToNonDet2.v
+++ compiler/src/runsToNonDet2.v
@@ -16,41 +16,24 @@ Section RunsTo.
         (forall mid, midset mid -> runsTo mid P) ->
         runsTo initial P.
 
-  (*
-  Lemma runsToSatisfying_exists_fuel: forall initial P,
-    runsTo initial P ->
-    exists fuel, P (power_func step fuel initial).
-  Proof.
-    introv R. induction R.
-    - exists 0. exact H.
-    - destruct IHR as [fuel IH]. exists (S fuel).
-      simpl.
-      rewrite <- power_func_one_plus_n.
-      exact IH.
-  Qed.
-  *)
+  Hint Constructors runsTo.
 
   Lemma runsTo_trans: forall P Q initial,
     runsTo initial P ->
     (forall middle, P middle -> runsTo middle Q) ->
     runsTo initial Q.
   Proof.
-    introv R1. induction R1; introv R2; [solve [auto]|].
-    eapply runsToStep.
-    (*
-    - exact H. eauto. eassumption.
-    - apply IHR1. apply R2.
-  Qed. *)
-  Admitted.
+    induction 1; eauto.
+  Qed.
 
+  Hint Resolve runsTo_trans.
 
   Lemma runsToSatisfying_imp: forall (P Q : State -> Prop) initial,
     runsTo initial P ->
     (forall final, P final -> Q final) ->
     runsTo initial Q.
   Proof.
-    introv R1 R2. eapply runsTo_trans; [eassumption|].
-    intros final Pf. apply runsToDone. auto.
+    eauto.
   Qed.
 
 End RunsTo.
