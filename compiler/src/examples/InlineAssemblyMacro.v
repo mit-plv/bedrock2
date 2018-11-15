e548ff501bce0b556c9dd23070b033b563074206
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-11-15 17:37:09 -0500

    can run compiler, but riscv.MinimalND currently is hardcoded to MMIO

--- compiler/src/examples/InlineAssemblyMacro.v
+++ compiler/src/examples/InlineAssemblyMacro.v
@@ -4,15 +4,24 @@ Require Import riscv.util.BitWidths.
 Require Import compiler.util.Common.
 Require Import compiler.Op.
 Require Import riscv.util.BitWidth32.
+Require Import riscv.util.Monads.
 Require Import compiler.util.List_Map.
 Require Import compiler.util.List_Set.
 Require Import compiler.FlatImp.
 Require Import riscv.MachineWidth32.
 Require Import riscv.util.ListLib.
 Require Import riscv.Decode.
+Require Import riscv.Utility.
 Require Import riscv.PseudoInstructions.
 Require Import riscv.InstructionCoercions.
 Require Import riscv.Program.
+Require Import compiler.FlatToRiscvDef.
+Require Import compiler.FlatToRiscv.
+Require Import riscv.ListMemory.
+Require Import compiler.FlatToRiscv32Proofs.
+Require Import compiler.FlatToRiscv32Specifics.
+Require Import riscv.RiscvMachine.
+Require Import riscv.MinimalND.
 Import ListNotations.
 
 Open Scope ilist_scope.
@@ -56,11 +65,11 @@ Definition map_with_index{A B: Type}(f: A -> Z -> B)(l: list A): list B :=
 
 (* later, we'll modify the compiler to receive the absolute position of the code
    as an argument, which would allow us to use JALR here and get rid of the helpervar *)
-Definition compile_ext_call(args: list var)(a: act)(results: list var): list Instruction :=
+Definition compile_ext_call(results: list var)(a: act)(args: list var): list Instruction :=
   match a with
   | Select =>
-    match args, results with
-    | selectorvar :: argvars, resvar :: helpervar :: nil => [[
+    match results, args with
+    | resvar :: helpervar :: nil, selectorvar :: argvars => [[
         Auipc helpervar 0;
         Add helpervar helpervar selectorvar;
         Jalr Register0 helpervar 8
@@ -91,7 +100,7 @@ Definition _a: varname := 4.
 Definition _b: varname := 5.
 Definition _c: varname := 6.
 Definition _r: varname := 7.
-Definition _garbage: varname := 8.
+Definition _garbage: varname := 31.
 Definition _s: varname := 9.
 
 Definition test: stmt :=
@@ -100,3 +109,33 @@ Definition test: stmt :=
   (SSeq (SOp _b bopname.add _inp1 _inp2)
   (SSeq (SOp _c bopname.sub _inp1 _inp2)
         (SInteract [_r; _garbage] Select [_s; _a; _b; _c]))))).
+
+Instance compilation_params: FlatToRiscvDef.parameters := {|
+  FlatToRiscvDef.actname := act;
+  FlatToRiscvDef.LwXLEN := Lw;
+  FlatToRiscvDef.SwXLEN := Sw;
+  FlatToRiscvDef.compile_ext_call := compile_ext_call;
+  FlatToRiscvDef.max_ext_call_code_size := 100;
+|}. abstract omega. Defined.
+
+Definition compiled: list Instruction := Eval cbv in compile_stmt test.
+
+Print compiled.
+
+Instance FlatToRiscv_params: FlatToRiscv.parameters := {|
+  FlatToRiscv.def_params := compilation_params;
+  FlatToRiscv.mword := word 32;
+  FlatToRiscv.MachineWidth_Inst := MachineWidth32;
+  FlatToRiscv.varMap_Inst := List_Map _ _;
+  FlatToRiscv.Event := Empty_set;
+  FlatToRiscv.Memory_Inst := mem_is_Memory (word 32);
+  FlatToRiscv.BWS := _;
+  FlatToRiscv.BWSP := _;
+  FlatToRiscv.M := OStateND (RiscvMachine Register (word 32) Empty_set);
+  FlatToRiscv.MM := OStateND_Monad _;
+  FlatToRiscv.RVM := _;(* IsRiscvMachineL; TODO we don't have an appropratiate Machine in risv *)
+  FlatToRiscv.RVS := _;
+  FlatToRiscv.RVAX := _;
+  FlatToRiscv.ext_spec := _;
+|}.
+Abort.
