24abd309a46c039e22e732f9390aabeba4516fce
Andres Erbsen <andreser@mit.edu>
2018-11-19 16:37:23 -0500

    tailrec without separation, ipow build but slow

--- bedrock2/src/Examples/ipow.v
+++ bedrock2/src/Examples/ipow.v
@@ -34,7 +34,7 @@ End WithT.
 
 Definition spec_of_ipow := fun functions =>
   forall x e t m,
-    WeakestPrecondition.call (fun _ => True) (fun _ => False) (fun _ _ => True) functions
+    WeakestPrecondition.call (fun _ => True) (fun _ => False) (fun _ _ => False) functions
       (fst ipow) t m [x; e]
       (fun t' m' rets => t=t'/\ m=m' /\ exists v, rets = [v]).
 
@@ -49,6 +49,11 @@ Ltac bind_body_of_function f :=
   let P := lazymatch eval pattern ipow in G with ?P _ => P end in
   change (bindcmd fbody (fun c : cmd => P (fname, (fargs, frets, c))));
   cbv beta iota delta [bindcmd]; intros.
+
+Tactic Notation "eabstract" tactic3(tac) :=
+  let G := match goal with |- ?G => G end in
+  let pf := lazymatch constr:(ltac:(tac) : G) with ?pf => pf end in
+  abstract exact_no_check pf.
 
 Ltac refine_ex :=
   hnf;
@@ -75,13 +80,62 @@ Ltac subst_and_bind_all :=
     assert_succeeds (subst x);
     let x' := fresh "x" in rename x into x';
     simple refine (let x := v in _);
-    replace x' with x by congruence; clear H
+    change (x' = x) in H;
+    symmetry in H;
+    destruct H
   end.
+
+(* FIXME: this is a hack as it may reduce in the values as well *)
+Ltac reduce_locals_map :=
+cbv [
+    locals parameters
+    map.put map.get map.empty
+    SortedListString.map
+    SortedList.parameters.key SortedList.parameters.value
+    SortedList.parameters.key_eqb SortedList.parameters.key_ltb
+    SortedList.map SortedList.sorted SortedList.put
+    SortedList.value SortedList.ok SortedList.minimize_eq_proof
+    String.eqb String.ltb String.prefix
+    Ascii.eqb Ascii.ltb Ascii.ascii_dec Ascii.ascii_rec Ascii.ascii_rect
+    Ascii.N_of_ascii Ascii.N_of_digits
+    BinNatDef.N.ltb BinNatDef.N.eqb BinNatDef.N.compare BinNat.N.add BinNat.N.mul
+    Pos.mul Pos.add Pos.eqb Pos.ltb Pos.compare Pos.compare_cont
+    Bool.bool_dec bool_rec bool_rect sumbool_rec sumbool_rect
+    andb orb
+  ].
+
+Lemma shiftr_decreases
+      (x1 : Word.word 64)
+      (n : Word.wzero 64 <> x1)
+  : (Word.wordToNat (Word.wrshift' x1 1) < Word.wordToNat x1)%nat.
+  SearchAbout Word.wrshift'.
+  cbv [Word.wrshift'].
+Admitted.
+
+Set Printing Depth 999999.
+Local Notation "'need!' y 's.t.' Px 'let' x ':=' v 'using' pfPx 'in' pfP" :=
+  (let x := v in ex_intro (fun y => Px /\ _) x (conj pfPx pfP))
+  (right associativity, at level 200, pfPx at next level,
+    format "'need!'  y  's.t.'  Px  '/' 'let'  x  ':='  v  'using'  pfPx  'in'  '/' pfP").
+Local Notation "'need!' x 's.t.' Px 'let' x ':=' v 'using' pfPx 'in' pfP" :=
+  (let x := v in ex_intro (fun x => Px /\ _) x (conj pfPx pfP))
+  (only printing, right associativity, at level 200, pfPx at next level,
+    format "'need!'  x  's.t.'  Px  '/' 'let'  x  ':='  v  'using'  pfPx  'in'  '/' pfP").
+Local Notation "'need!' y 's.t.' Px 'let' x ':=' v 'using' pfPx 'in' pfP" :=
+  (let x := v in ex_intro (fun y => Px /\ _) x (conj pfPx pfP))
+  (right associativity, at level 200, pfPx at next level,
+   format "'need!'  y  's.t.'  Px  '/' 'let'  x  ':='  v  'using'  pfPx  'in'  '/' pfP")
+  : type_scope.
+Local Notation "'need!' x 's.t.' Px 'let' x ':=' v 'using' pfPx 'in' pfP" :=
+  (let x := v in ex_intro (fun x => Px /\ _) x (conj pfPx pfP))
+  (only printing, right associativity, at level 200, pfPx at next level,
+   format "'need!'  x  's.t.'  Px  '/' 'let'  x  ':='  v  'using'  pfPx  'in'  '/' pfP")
+  : type_scope.
 
 Lemma ipow_ok : forall functions, spec_of_ipow (ipow::functions).
 Proof.
   bind_body_of_function ipow.
-  cbv [spec_of_ipow]. intros.
+  cbv [spec_of_ipow]. intros x0 e0 t0 m0.
 
   refine_ex_and.
   { exact eq_refl. }
@@ -90,62 +144,126 @@ Proof.
     { repeat (refine_ex_and || exact eq_refl). }
     exact eq_refl. }
 
-  eapply TailRecursion.tailrecursion with
-      (lt:=Z.lt)
-      (P:=fun _ t' l => emp (exists x e ret, t' = t /\ l = (map.put (map.put (map.put map.empty "x" x) "e" e) "ret" ret)))
-      (Q:=fun _ t' l => emp (exists x e ret, t' = t /\ l = (map.put (map.put (map.put map.empty "x" x) "e" e) "ret" ret)))
-      (R0 := fun m' => m'=m).
-  admit.
-  admit.
-  admit.
-  intros.
+  let T := match type of l0 with ?T => T end in
+  set (P_ := fun v t m (l:T) =>
+            exists x e ret,
+              t = t0 /\ l = (map.put (map.put (map.put map.empty "x" x) "e" e) "ret" ret) /\ m = m0 /\ v = Word.wordToNat e);
+  set (Q_ := fun (v:nat) t m (l:T) =>
+            exists x e ret,
+              t = t0 /\ l = (map.put (map.put (map.put map.empty "x" x) "e" e) "ret" ret) /\ m = m0);
+  eapply TailRecursion.tailrec with (lt:=lt) (P:=P_) (Q:=Q_).
+  exact Wf_nat.lt_wf.
+  { eabstract (repeat refine_ex; repeat split; repeat exact eq_refl). }
+  { intros; cbv [P_ Q_] in * |- ; break_ex; break_and; subst_and_bind_all.
 
-  eapply sep_emp_l in H. destruct H.
-  break_ex.
-  break_and.
-  subst_and_bind_all.
-
-  refine_ex_and.
-  { repeat (refine_ex_and || exact eq_refl). }
-  split; intros.
-  { refine_ex_and.
-    { repeat (refine_ex_and || exact eq_refl). }
+    refine_ex_and.
+    eabstract (repeat (refine_ex_and || exact eq_refl)).
     split; intros.
     { refine_ex_and.
-      { repeat (refine_ex_and || exact eq_refl). }
-      refine_ex_and.
-      { repeat (refine_ex_and || exact eq_refl). }
-      refine_ex_and.
-      { repeat (refine_ex_and || exact eq_refl). }
-      eexists. eexists. split.
-      { eapply sep_emp_l. split.
-        { do 3 eexists. split; exact eq_refl. }
-        { eapply sep_emp_r. split. assumption. exact I. } }
-      { split.
-        { admit. }
-        { intros.
-          eapply impl1_l_sep_emp; intros.
-          admit. } } }
-    { refine_ex_and.
-      { repeat (refine_ex_and || exact eq_refl). }
-      refine_ex_and.
-      { repeat (refine_ex_and || exact eq_refl). }
-      eexists. eexists. split.
-      { eapply sep_emp_l. split.
-        { do 3 eexists. split; exact eq_refl. }
-        { eapply sep_emp_r. split. assumption. exact I. } }
-      { split.
-        { admit. }
-        { intros.
-          eapply impl1_l_sep_emp; intros.
-          admit. } } } }
-  { eapply sep_emp_l. split. solve[repeat eexists]. assumption. }
-  intros.
-  eapply sep_emp_l in H. destruct H.
-  break_ex.
-  break_and.
-  subst_and_bind_all.
-  refine_ex_and.
-  { repeat (refine_ex_and || exact eq_refl). }
-  repeat eexists.
-Abort.
+      eabstract (repeat (refine_ex_and || exact eq_refl)).
+      split; intros.
+
+      { refine_ex_and.
+        eabstract (repeat (refine_ex_and || exact eq_refl)).
+        refine_ex_and.
+        { refine_ex_and. exact eq_refl.
+          refine_ex_and. exact eq_refl.
+          (* [exact eq_refl] reduces too much here *)
+          exact (eq_refl (interp_binop bop_sru l7 l)). }
+        refine_ex_and.
+        eabstract (repeat (refine_ex_and || exact eq_refl)).
+        refine_ex_and.
+        { repeat refine_ex. split.
+          { exact eq_refl. }
+          { split.
+            { (* local variables *)
+              subst l1.
+              reduce_locals_map.
+              exact eq_refl. }
+            split.
+            { exact eq_refl. }
+            { exact eq_refl. } } }
+        { split.
+          { right.
+            (* measure decreases *)
+            abstract (
+            cbv [l l6 v l3] in *;
+            revert H; clear; intros H;
+            cbv [parameters word_test] in H;
+            destruct (Word.weq (Word.wzero 64) x1) in H; try congruence; clear H;
+            cbv [parameters
+                   interp_binop
+                   Basic_bopnames.BasicALU
+                   BasicC64Semantics.shiftWidth
+                   bop_sru];
+            rewrite (eq_refl 
+                     : Word.wordToNat (Word.wand (Word.ZToWord 64 1) (Word.NToWord 64 63)) = 1%nat); shelve_unifiable;
+            eapply shiftr_decreases; eauto). }
+        { intros; cbv [P_ Q_] in * |-; break_ex; break_and; subst_and_bind_all.
+          refine_ex. refine_ex. refine_ex. split.
+          { exact eq_refl. }
+          split.
+          { exact eq_refl. }
+          { exact eq_refl. } } } }
+
+      { refine_ex_and.
+        { refine_ex_and. exact eq_refl.
+          refine_ex_and. exact eq_refl.
+          (* [exact eq_refl] reduces too much here *)
+          exact (eq_refl (interp_binop bop_sru _ _)). }
+        refine_ex_and.
+        eabstract (repeat (refine_ex_and || exact eq_refl)).
+        refine_ex_and.
+        { repeat refine_ex. split.
+          { exact eq_refl. }
+          { split.
+            { exact eq_refl. }
+            split.
+            { exact eq_refl. }
+            { exact eq_refl. } } }
+        { split.
+          { right.
+            (* measure decreases *)
+            abstract (
+            cbv [l v l3 l5] in *;
+            revert H; clear; intros H;
+            cbv [parameters word_test] in H;
+            destruct (Word.weq (Word.wzero 64) x1) in H; try congruence; clear H;
+            cbv [parameters
+                   interp_binop
+                   Basic_bopnames.BasicALU
+                   BasicC64Semantics.shiftWidth
+                   bop_sru];
+            rewrite (eq_refl 
+                     : Word.wordToNat (Word.wand (Word.ZToWord 64 1) (Word.NToWord 64 63)) = 1%nat); shelve_unifiable;
+            eapply shiftr_decreases; eauto). }
+        { intros; cbv [P_ Q_] in * |-; break_ex; break_and; subst_and_bind_all.
+          repeat refine_ex. split.
+          { exact eq_refl. }
+          split.
+          { exact eq_refl. }
+          { exact eq_refl. } } } } }
+
+    repeat refine_ex. split.
+    { exact eq_refl. }
+    split.
+    { exact eq_refl. }
+    { exact eq_refl. } }
+
+  { intros; cbv [P_ Q_] in * |-; break_ex; break_and; subst_and_bind_all.
+    refine_ex_and.
+    { exact eq_refl. }
+    { split.
+      { exact eq_refl. }
+      split.
+      { exact eq_refl. }
+      { refine_ex.
+        exact eq_refl. } } }
+
+  Unshelve.
+  eapply WeakestPreconditionProperties.Proper_call; cbv; intuition idtac.
+Time Defined. (* 1 minute *)
+
+Time Print ipow_ok. (* 3 minutes *)
+
+(* 262.75user 0.89system 4:31.68elapsed 97%CPU (0avgtext+0avgdata 2465616maxresident)k *)
--- bedrock2/src/TailRecursion.v
+++ bedrock2/src/TailRecursion.v
@@ -4,15 +4,106 @@ From bedrock2 Require Import WeakestPrecondition WeakestPreconditionProperties.
 From bedrock2 Require Import Map Map.Separation Map.SeparationLogic.
 
 Section TailRecrsion.
-  Context {p : unique! Semantics.parameters}.
-  Local Infix "*" := Separation.sep.
-  Local Infix "*" := Separation.sep : type_scope.
-  Local Infix "==>" := Lift1Prop.impl1.
-  Lemma tailrecursion {mem_ok:Map.map.ok mem} {rely guarantee progress call}
+  Context
+    {p : unique! Semantics.parameters}
+    {rely guarantee : trace -> Prop}
+    {progress : trace -> trace -> Prop}
+    {call : funname -> trace -> mem -> list word -> (trace -> mem -> list word -> Prop) -> Prop}
     {Proper_call : Proper (pointwise_relation _ (pointwise_relation _
       (pointwise_relation _ (pointwise_relation _ (pointwise_relation _
       (pointwise_relation _ (pointwise_relation _ Basics.impl)) ==>
-      Basics.impl)))))%signature call}
+      Basics.impl)))))%signature call}.
+
+  Lemma tailrec
+    e c t (m : mem) l (post : _->_->_-> Prop)
+    {measure : Type} (P Q:_->_->_->_->Prop) lt (Hwf : well_founded lt) (v0 : measure)
+    (Hpre : P v0 t m l)
+    (Hbody : forall v t m l, P v t m l ->
+      exists br, expr m l e (eq br) /\
+      (word_test br = true -> cmd rely guarantee progress call c t m l
+        (fun t' m' l' => exists v', P v' t' m' l' /\
+          (progress t' t \/ lt v' v) /\
+          forall T M L, Q v' T M L -> Q v T M L)) /\
+      (word_test br = false -> Q v t m l))
+    (Hpost : forall t m l, Q v0 t m l -> post t m l)
+    : cmd rely guarantee progress call (cmd.while e c) t m l post.
+  Proof.
+    eexists measure, lt, (fun v t m l => P v t m l /\
+                          forall T M L, Q v T M L -> Q v0 T M L).
+    split. assumption.
+    split; [solve[eauto]|].
+    intros vi ti mi li (?&Qi).
+    destruct (Hbody _ _ _ _ ltac:(eassumption)) as (br&?&X); exists br; split; [assumption|].
+    destruct X as (Htrue&Hfalse). split; intros Hbr;
+      [pose proof(Htrue Hbr)as Hpc|pose proof(Hfalse Hbr)as Hpc]; clear Hbr Htrue Hfalse.
+    { eapply Proper_cmd; [reflexivity..|eassumption| |eapply Hpc].
+      intros tj mj lj (vj&dP&?&dQ); eauto 9. }
+    { eapply Hpost, Qi, Hpc. }
+  Qed.
+
+  Lemma tailrec2
+    e c t (m : mem) l (post : _->_->_-> Prop)
+    {measure : Type} {X} (P Q:_->X->_->_->_->Prop) lt (Hwf : well_founded lt)
+    (v0 : measure) (x0 : X)
+    (Hpre : P v0 x0 t m l)
+    (Hbody : forall v x t m l, P v x t m l ->
+      exists br, expr m l e (eq br) /\
+      (word_test br = true -> cmd rely guarantee progress call c t m l
+        (fun t' m' l' => exists v' x', P v' x' t' m' l' /\
+          (progress t' t \/ lt v' v) /\
+          forall T M L, Q v' x' T M L -> Q v x T M L)) /\
+      (word_test br = false -> Q v x t m l))
+    (Hpost : forall t m l, Q v0 x0 t m l -> post t m l)
+    : cmd rely guarantee progress call (cmd.while e c) t m l post.
+  Proof.
+    eexists measure, lt, (fun v t m l => exists x, P v x t m l /\
+                          forall T M L, Q v x T M L -> Q v0 x0 T M L).
+    split. assumption.
+    split; [solve[eauto]|].
+    intros vi ti mi li (?&?&Qi).
+    destruct (Hbody _ _ _ _ _ ltac:(eassumption)) as (br&?&XX); exists br; split; [assumption|].
+    destruct XX as (Htrue&Hfalse). split; intros Hbr;
+      [pose proof(Htrue Hbr)as Hpc|pose proof(Hfalse Hbr)as Hpc]; clear Hbr Htrue Hfalse.
+    { eapply Proper_cmd; [reflexivity..|eassumption| |eapply Hpc].
+      intros tj mj lj (vj&xj&dP&?&dQ); eauto 9. }
+    { eapply Hpost, Qi, Hpc. }
+  Qed.
+  Notation tailrec1 := tailrec.
+
+  Lemma tailrec3
+    e c t (m : mem) l (post : _->_->_-> Prop)
+    {measure : Type} {X Y} (P Q:_->X->Y->_->_->_->Prop) lt (Hwf : well_founded lt)
+    (v0 : measure) (x0 : X) (y0 : Y)
+    (Hpre : P v0 x0 y0 t m l)
+    (Hbody : forall v x y t m l, P v x y t m l ->
+      exists br, expr m l e (eq br) /\
+      (word_test br = true -> cmd rely guarantee progress call c t m l
+        (fun t' m' l' => exists v' x' y', P v' x' y' t' m' l' /\
+          (progress t' t \/ lt v' v) /\
+          forall T M L, Q v' x' y' T M L -> Q v x y T M L)) /\
+      (word_test br = false -> Q v x y t m l))
+    (Hpost : forall t m l, Q v0 x0 y0 t m l -> post t m l)
+    : cmd rely guarantee progress call (cmd.while e c) t m l post.
+  Proof.
+    eexists measure, lt, (fun v t m l => exists x y, P v x y t m l /\
+                          forall T M L, Q v x y T M L -> Q v0 x0 y0 T M L).
+    split. assumption.
+    split; [solve[eauto]|].
+    intros vi ti mi li (?&?&?&Qi).
+    destruct (Hbody _ _ _ _ _ _ ltac:(eassumption)) as (br&?&XX); exists br; split; [assumption|].
+    destruct XX as (Htrue&Hfalse). split; intros Hbr;
+      [pose proof(Htrue Hbr)as Hpc|pose proof(Hfalse Hbr)as Hpc]; clear Hbr Htrue Hfalse.
+    { eapply Proper_cmd; [reflexivity..|eassumption| |eapply Hpc].
+      intros tj mj lj (vj&xj&yj&dP&?&dQ); eauto 9. }
+    { eapply Hpost, Qi, Hpc. }
+  Qed.
+
+  Context {mem_ok : map.ok mem}.
+  Local Infix "*" := Separation.sep.
+  Local Infix "*" := Separation.sep : type_scope.
+  Local Infix "==>" := Lift1Prop.impl1.
+
+  Lemma tailrec_sep
     e c t (m : mem) l (post : _->_->_-> Prop)
     {measure : Type} P Q lt (Hwf : well_founded lt) (v0 : measure) R0
     (Hpre : (P v0 t l * R0) m)
