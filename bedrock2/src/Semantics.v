8ac2ca6f1e4392350d61aeb3c256dfdab5fda104
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-12-10 13:45:05 -0500

    newest ExprImp semantics are already in compiler.Semantics, should be moved to bedrock2.Semantics soon

--- bedrock2/src/Semantics.v
+++ bedrock2/src/Semantics.v
@@ -26,7 +26,7 @@ Class parameters := {
 
 Section semantics.
   Context {pp : unique! parameters}.
-  
+
   Section WithMem.
     Context (m:mem).
     Fixpoint load_bytes (n : nat) (a : word) {struct n} : option (tuple byte n) :=
@@ -55,48 +55,4 @@ Section semantics.
   Context (rely guarantee : trace -> Prop) (progress : trace -> trace -> Prop).
   Implicit Types post : trace -> mem -> locals -> Prop. (* COQBUG(unification finds Type instead of Prop and fails to downgrade *)
 
-  Print cmd.cmd.
-  Inductive exec
-    : cmd
-    -> trace -> mem -> locals
-    -> (trace -> mem -> locals -> Prop)
-    -> Prop :=
-  | skip t m l post (_:post t m l)
-    : exec cmd.skip t m l post
-  | set x e t m l post
-        v (_ : eval l e v)
-        (_ : post t m (map.put l x v))
-    : exec (cmd.set x e) t m l post
-  | seq c1 c2 t m l post
-        mid (_ : exec c1 t m l mid)
-        (_ : forall t m l, exec c2 t m l post)
-    : exec (cmd.seq c1 c2) t m l post
-  | while e c t m l post
-          v (_ : eval l e v)
-          (_ : Logic.or
-                 (word_test v = false /\ post t m l)
-                 (word_test v = true /\ exec (cmd.seq c (cmd.while e c)) t m l post))
-    : exec (cmd.while e c) t m l post
-  | interact bindvars action arges t m l post
-             args (_ : List.Forall2 (eval l) arges args)
-             (output := (m, action, args))
-             (_ : forall m rets (t := cons (output, (m, rets)) t),
-                 guarantee t /\
-                 rely t -> (bind_ex_Some l <- map.putmany bindvars rets l; post t m l))
-    : exec (cmd.interact bindvars action arges) t m l post
-           (*
-  | call
-      env (* FIXME : move above *)
-      bindvars fname arges t m l post
-      args (_ : List.Forall2 (eval l) arges args)
-      params c retvars (_ : map.get env fname = Some (params, c, retvars))
-      l0 (_ : map.putmany params args map.empty = Some l0)
-      beforeret (_ : exec c t m l0 beforeret)
-      (_ : forall t' m' l', beforeret t' m' l' ->
-                            exists rets, List.Forall2 (fun x v => map.get l' x = Some v) retvars rets /\
-                            exists l'', map.putmany bindvars rets l = Some l'' /\
-                            post t' m' l'')
-    : exec (cmd.call bindvars fname arges) t m l post
-*)
-  .
 End semantics.
