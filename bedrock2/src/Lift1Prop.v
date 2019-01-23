45c44d0ce0c971a612959be68b4e5fae3f657e18
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-01-23 08:23:10 -0500

    sketch how a squarer program with IO would be specified and how IO would be instantiated to syscalls or bare-metal MMIO

--- bedrock2/src/Lift1Prop.v
+++ bedrock2/src/Lift1Prop.v
@@ -1,7 +1,15 @@
 Require Import Coq.Classes.Morphisms.
-Definition impl1 {T} (P Q:_->Prop) : Prop := forall x:T, P x -> Q x.
-Definition iff1 {T} (P Q:_->Prop) : Prop := forall x:T, P x <-> Q x.
+
+Section Binary.
+  Context {T: Type} (P Q: T -> Prop).
+  Definition impl1 := forall x, P x -> Q x.
+  Definition iff1 := forall x, P x <-> Q x.
+  Definition and1 := forall x, P x /\ Q x.
+  Definition or1 := forall x, P x \/ Q x.
+End Binary.
+
 Definition ex1 {A B} (P : A -> B -> Prop) := fun (b:B) => exists a:A, P a b.
+
 Global Instance Transitive_impl1 T : Transitive (@impl1 T). firstorder idtac. Qed.
 Global Instance Reflexive_impl1 T : Reflexive (@impl1 T). firstorder idtac. Qed.
 Global Instance Proper_impl1_impl1 T : Proper (Basics.flip impl1 ==> impl1 ==> Basics.impl) (@impl1 T). firstorder idtac. Qed.
@@ -12,6 +20,7 @@ Global Instance Proper_iff1_impl1 T : Proper (Basics.flip impl1 ==> impl1 ==> Ba
 Global Instance Proper_impl1_iff1 T : Proper (iff1 ==> iff1 ==> iff) (@impl1 T). firstorder idtac. Qed.
 Global Instance Proper_ex1_iff1 A B : Proper (pointwise_relation _ iff1 ==> iff1) (@ex1 A B). firstorder idtac. Qed.
 Global Instance Proper_ex1_impl1 A B : Proper (pointwise_relation _ impl1 ==> impl1) (@ex1 A B). firstorder idtac. Qed.
+
 Lemma impl1_ex1_l {A B} p q : impl1 (@ex1 A B p) q <-> (forall x, impl1  (p x) q).
 Proof. firstorder idtac. Qed.
 Lemma impl1_ex1_r {A B} p q x : impl1 p (q x) -> impl1 p (@ex1 A B q).
---
+++ bedrock2/src/ListPred.v
@@ -0,0 +1,36 @@
+Require Import Coq.Lists.List. Import ListNotations.
+
+Section ListPred.
+  Context {T: Type}.
+
+  Definition one(t: T): list T -> Prop := eq [t].
+
+  Definition concat(P1 P2: list T -> Prop): list T -> Prop :=
+    fun l => exists l1 l2, l = l1 ++ l2 /\ P1 l1 /\ P2 l2.
+
+  Definition choice(P1 P2: list T -> Prop): list T -> Prop :=
+    fun l => P1 l \/ P2 l.
+
+  Inductive kleene(P: list T -> Prop): list T -> Prop :=
+  | kleene_empty:
+      kleene P nil
+  | kleene_step: forall l1 l2,
+      P l1 ->
+      kleene P l2 ->
+      kleene P (l1 ++ l2).
+
+  (* more powerful than regex: *)
+
+  Definition both(P1 P2: list T -> Prop): list T -> Prop :=
+    fun l => P1 l /\ P2 l.
+
+  Definition existsl{A: Type}(P: A -> list T -> Prop): list T -> Prop :=
+    fun l => exists a, P a l.
+
+End ListPred.
+
+Module ListPredNotations.
+  Notation "P ^*" := (kleene P) (at level 50).
+  Infix "+++" := concat (at level 60).
+  Infix "|||" := choice (at level 70).
+End ListPredNotations.
