1f5472b11392229ccd02e5695cc4f34ab96f7271
Andres Erbsen <andreser@mit.edu>
2019-02-04 17:40:46 -0500

    bsearch: fix lia call, more automatable, start inv

--- bedrock2/src/Examples/bsearch.v
+++ bedrock2/src/Examples/bsearch.v
@@ -7,10 +7,26 @@ Definition bsearch := @Demos.bsearch _ Demos.BinarySearch.StringNames.Inst.
 From coqutil Require Import Word.Interface Map.Interface. (* coercions word and rep *)
 From bedrock2 Require Import Semantics BasicC64Semantics.
 
+Local Infix "^+" := word.add  (at level 50, left associativity).
+Local Infix "^-" := word.sub  (at level 50, left associativity).
+Local Infix "^<<" := word.slu  (at level 37, left associativity).
+Local Infix "^>>" := word.sru  (at level 37, left associativity).
+Local Notation "/_" := word.of_Z.
+Local Notation "\_" := word.unsigned.
+Local Open Scope Z_scope.
+Lemma word__add_sub x y : (x^+y^-x) = y.
+Proof.
+  apply Properties.word.unsigned_inj.
+  rewrite word.unsigned_sub, word.unsigned_add.
+  rewrite Zminus_mod_idemp_l, Z.add_simpl_l.
+  apply Properties.word.wrap_unsigned.
+Qed.
+
 Import HList List.
 Instance spec_of_bsearch : spec_of "bsearch"%string := fun functions =>
   forall left right target xs R t m,
     sep (array scalar (word.of_Z 8) left xs) R m ->
+    \_ (right ^- left) = 8*Z.of_nat (Datatypes.length xs) -> 
     WeakestPrecondition.call (fun _ => True) (fun _ => False) (fun _ _ => True) functions
       "bsearch"%string t m (left::right::target::nil)%list
       (fun t' m' rets => t=t' /\ sep (array scalar (word.of_Z 8) left xs) R m' /\ exists i, rets = (i::nil)%list /\
@@ -21,17 +37,10 @@ From coqutil.Tactics Require Import eabstract letexists rdelta.
 From coqutil.Macros Require Import symmetry.
 Import PrimitivePair.
 
-Lemma word__sub_add_l_same_l (x y : word) : (word.sub (word.add x y) x) = y.
-Proof.
-  eapply Properties.word.unsigned_inj.
-  rewrite word.unsigned_sub, word.unsigned_add, Zminus_mod_idemp_l.
-  unshelve erewrite (_:(_ - _=_)%Z); shelve_unifiable; [ring_simplify; exact eq_refl|].
-  eapply Properties.word.wrap_unsigned.
-Qed.
-
 From coqutil Require Import Z.div_mod_to_equations.
 
 Axiom __mem_ok : map.ok mem. Local Existing Instance __mem_ok.
+
 Lemma swap_swap_ok : program_logic_goal_for_function! bsearch.
 Proof.
   pose proof __mem_ok.
@@ -42,7 +51,9 @@ Proof.
   refine (
     tailrec (HList.polymorphic_list.cons _ (HList.polymorphic_list.cons _ HList.polymorphic_list.nil)) ("left"::"right"::"target"::nil)%list%string
         (fun l xs R t m left right target => PrimitivePair.pair.mk
-          (sep (array scalar (word.of_Z 8) left xs) R m /\ List.length xs = l)
+                                               (sep (array scalar (word.of_Z 8) left xs) R m /\
+                                                \_ (right ^- left) = 8*Z.of_nat (Datatypes.length xs) /\
+                                                List.length xs = l)
         (fun        T M LEFT RIGHT TARGET => T = t /\ sep (array scalar (word.of_Z 8) left xs) R M))
         lt _ _ _ _ _ _ _);
     cbn [reconstruct map.putmany_of_list HList.tuple.to_list
@@ -67,48 +78,95 @@ Proof.
     { repeat letexists. split. repeat straightline.
       repeat letexists; repeat split; repeat straightline.
       { cbn [interp_binop] in *. subst v2; subst x7. SeparationLogic.ecancel_assumption. }
+      { cbn [interp_binop] in *. subst v2; subst x7. subst v0.
+        replace (x2 ^- (x1 ^+ (x2 ^- x1) ^>> /_ 4 ^<< /_ 3 ^+ /_ 8))
+           with (x2 ^- x1 ^- (x2 ^- x1) ^>> /_ 4 ^<< /_ 3 ^- /_ 8) by admit.
+        replace (x1 ^+ (x2 ^- x1) ^>> /_ 4 ^<< /_ 3 ^- x1)
+           with ((x2 ^- x1) ^>> /_ 4 ^<< /_ 3) by admit.
+        set (delta := (x2 ^- x1) ^>> /_ 4).
+        replace (word.divu (delta ^<< /_ 3) (/_ 8)) with delta by admit.
+        unshelve erewrite (_ : forall xs, Datatypes.length xs <> 0%nat -> Datatypes.length (List.tl xs) = pred (Datatypes.length xs)). admit.
+        unshelve erewrite (_ : forall xs delta, (Datatypes.length xs > delta)%nat -> Datatypes.length (List.skipn delta xs) = (Datatypes.length xs - delta)%nat). admit.
+        replace (Z.of_nat (Init.Nat.pred (Datatypes.length x - Z.to_nat (\_ delta))))
+           with (Z.of_nat (Datatypes.length x) - \_ delta -1) by admit.
+        ring_simplify.
+        rewrite <-H3.
+        rewrite word.unsigned_sub, Z.mod_small.
+        rewrite word.unsigned_sub, Z.mod_small.
+        rewrite word.unsigned_slu, Z.mod_small.
+        rewrite Z.shiftl_mul_pow2 by discriminate.
+        change (2^\_ (/_ 3)) with 8.
+        change (\_ (/_ 8)) with 8.
+        ring.
+        admit.
+        admit.
+        admit.
+        admit.
+        admit.
+        admit. }
       { left. exact I. }
-      subst x8. SeparationLogic.seprewrite_in (symmetry! @array_address_inbounds) H6.
+
+      rename H3 into length_rep.
+
+      cbv [interp_binop br] in H5; destruct (word.ltu x1 x2) eqn:Hbr; [clear H5|contradiction H5; exact eq_refl].
+      rewrite word.unsigned_ltu, Z.ltb_lt in Hbr.
+      assert (\_ (x2 ^- x1) <> 0) as Hnz. {
+        assert (\_ x2 <> \_ x1) as HC by Lia.lia.
+        intros HX. apply HC. clear -HX.
+        rewrite word.unsigned_sub in HX.
+        pose proof Properties.word.unsigned_range x1.
+        pose proof Properties.word.unsigned_range x2.
+        change width with 64 in *.
+        Z.div_mod_to_equations; Lia.lia. }
+
+      subst x8. SeparationLogic.seprewrite_in (symmetry! @array_address_inbounds) H8.
+
       { cbn [interp_binop] in *.
-        clear.
         subst v0.
-        Local Infix "^+" := word.add  (at level 50, left associativity).
-        Local Infix "^-" := word.sub  (at level 50, left associativity).
-        Local Infix "^<<" := word.slu  (at level 37, left associativity).
-        Local Infix "^>>" := word.sru  (at level 37, left associativity).
-        Local Notation "/_" := word.of_Z.
-        Local Notation "\_" := word.unsigned.
-        Local Open Scope Z_scope.
-        assert (word__add_sub : forall x y : word, (x^+y^-x) = y) by admit.
-        assert (Z.of_nat (Datatypes.length x) <> 0) by admit.
-        assert (\_ (x2 ^- x1) = 8*Z.of_nat (Datatypes.length x)) by admit.
 
         rewrite word__add_sub.
 
-        pose proof Properties.word.unsigned_sru_nowrap (x2 ^- x1) (/_ 4) eq_refl.
-        rewrite H0 in H1 ; change (\_ (/_ 4)) with 4 in H1.
+        pose proof Properties.word.unsigned_range (x2 ^- x1) as Hb; rewrite length_rep in Hb; change width with 64 in Hb.
+        pose proof Properties.word.unsigned_sru_nowrap (x2 ^- x1) (/_ 4) eq_refl as HH.
+        rewrite length_rep in HH ; change (\_ (/_ 4)) with 4 in HH.
 
-        pose proof word.unsigned_slu ((x2 ^- x1) ^>> /_ 4) (/_ 3) eq_refl as H2.
-        change (\_ (/_ 3)) with 3 in H2.
-        setoid_rewrite H1 in H2.
+        pose proof word.unsigned_slu ((x2 ^- x1) ^>> /_ 4) (/_ 3) eq_refl as HH2.
+        change (\_ (/_ 3)) with 3 in HH2.
+        setoid_rewrite HH in HH2.
+        rewrite Z.shiftr_div_pow2, Z.shiftl_mul_pow2 in HH2 by discriminate.
+        rewrite Z.mod_small in HH2 by (Z.div_mod_to_equations; Lia.lia).
 
-        setoid_rewrite H2.
-        clear -H.
         change (\_ (/_ 8)) with 8.
-        rewrite Z.shiftr_div_pow2, Z.shiftl_mul_pow2 by discriminate .
-        zify. Z.div_mod_to_equations. admit. (* Lia.lia. stack overflow *)
-      }
-      {
-        rewrite word.unsigned_of_Z, (Z.mod_small 8) by (split; (discriminate || exact eq_refl)).
-        assert (word.unsigned (word.sub v0 x1) mod 8 = 0)%Z by admit; trivial.
-      }
+        setoid_rewrite HH2.
+
+        Z.div_mod_to_equations; Lia.lia. }
+      { cbn [interp_binop] in *.
+        subst v0.
+
+        rewrite word__add_sub.
+
+        pose proof Properties.word.unsigned_range (x2 ^- x1) as Hb; rewrite length_rep in Hb; change width with 64 in Hb.
+        pose proof Properties.word.unsigned_sru_nowrap (x2 ^- x1) (/_ 4) eq_refl as HH.
+        rewrite length_rep in HH ; change (\_ (/_ 4)) with 4 in HH.
+
+        pose proof word.unsigned_slu ((x2 ^- x1) ^>> /_ 4) (/_ 3) eq_refl as HH2.
+        change (\_ (/_ 3)) with 3 in HH2.
+        setoid_rewrite HH in HH2.
+        rewrite Z.shiftr_div_pow2, Z.shiftl_mul_pow2 in HH2 by discriminate.
+        rewrite Z.mod_small in HH2 by (Z.div_mod_to_equations; Lia.lia).
+
+        change (\_ (/_ 8)) with 8.
+        setoid_rewrite HH2.
+        
+        Z.div_mod_to_equations; Lia.lia. } 
       { exact eq_refl. }
       { SeparationLogic.ecancel_assumption. } }
     { repeat letexists. split. repeat straightline.
       repeat letexists; repeat split; repeat straightline.
       { cbn [interp_binop] in *. subst v1; subst x7; subst x8. SeparationLogic.ecancel_assumption. }
+      { admit. }
       { left. exact I. }
-      subst x8. SeparationLogic.seprewrite_in (symmetry! @array_address_inbounds) H6.
+      subst x8. SeparationLogic.seprewrite_in (symmetry! @array_address_inbounds) H8.
       admit. admit. exact eq_refl.
       SeparationLogic.ecancel_assumption. } }
 
