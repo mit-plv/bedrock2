47df839f76e343ff0598b4995584c6250c91d3c3
Andres Erbsen <andreser@mit.edu>
2019-02-25 22:56:40 -0500

    Revert "FE310CompilerDemo: down to 31 registers"
    This reverts commit 9b8c9ff526ce9af713369e9f1b0f033c7aa86d8d.

--- bedrock2/src/Examples/FE310CompilerDemo.v
+++ bedrock2/src/Examples/FE310CompilerDemo.v
@@ -71,13 +71,16 @@ Local Coercion var {p} (x : @varname p) : expr := expr.var x. (* COQBUG(4593) *)
 Local Coercion literal (x : Z) : expr := expr.literal x.
 
 Definition swap_chars_over_uart: cmd :=
-  let t : varname := 1%Z in
+  let trim : varname := 1%Z in
   let prev : varname := 2%Z in
   let rx : varname := 3%Z in
   let tx : varname := 4%Z in
   let running : varname := 5%Z in
 
   let bit31 : varname := 6%Z in
+  let one : varname := 7%Z in
+  let dot : varname := 8%Z in
+  let uart_tx : varname := 9%Z in
 
   let MMIOREAD  := MMInput in (* COQBUG(9514) ... *)
   let MMIOWRITE := MMOutput in
@@ -87,31 +90,30 @@ Definition swap_chars_over_uart: cmd :=
 
   bedrock_func_body:(
     (* ring oscillator: enable, trim to 72MHZ using value from OTP, divider=0+1 *)
-    io! t = MMIOREAD (constr:(Ox"0x00021fec"));
-    output! MMIOWRITE(hfrosccfg, constr:(2^30) | (t & constr:(2^5-1)) << constr:(16));
+    io! trim = MMIOREAD (constr:(Ox"0x00021fec"));
+    output! MMIOWRITE(hfrosccfg, constr:(2^30) | (trim & constr:(2^5-1)) << constr:(16));
+    constr:(cmd.unset trim);
 
-    t = (constr:(1));
+    one = (constr:(1));
     output! MMIOWRITE(constr:(uart0_base + Ox"018"), constr:(624)); (* --baud=115200 # = 72MHz/(0+1)/(624+1) *)
-    output! MMIOWRITE(constr:(uart0_base + Ox"008"), t); (* tx enable *)
-    output! MMIOWRITE(constr:(uart0_base + Ox"00c"), t); (* rx enable *)
+    output! MMIOWRITE(constr:(uart0_base + Ox"008"), one); (* tx enable *)
+    output! MMIOWRITE(constr:(uart0_base + Ox"00c"), one); (* rx enable *)
     output! MMIOWRITE(constr:(gpio0_base + Ox"038"), constr:(2^17 + 2^16)); (* pinmux uart tx rx *)
 
-    t = (constr:(46)); prev = (t);
+    dot = (constr:(46)); prev = (dot);
     bit31 = (constr:(2^31)); running = (bit31);
     while (running) {
       rx = (bit31);
       while (rx & bit31) { io! rx = MMIOREAD(constr:(uart0_base + Ox"004")) };
 
-      t = (constr:(uart0_base + Ox"000"));
+      uart_tx = (constr:(uart0_base + Ox"000"));
       tx = (bit31);
-      while (tx & bit31) { io! tx = MMIOREAD(t) }; constr:(cmd.unset tx);
-      output! MMIOWRITE(t, prev);
+      while (tx & bit31) { io! tx = MMIOREAD(uart_tx) }; constr:(cmd.unset tx);
+      output! MMIOWRITE(uart_tx, prev); constr:(cmd.unset uart_tx);
 
       prev = (rx); constr:(cmd.unset rx);
-      t = (constr:(1));
-      running = (running - t);
-      t = (constr:(46));
-      if (prev == t) { running = (running ^ running) }
+      running = (running - one);
+      if (prev == dot) { running = (running ^ running) }
     }
   ).
 
@@ -133,20 +135,23 @@ Proof.
   cbv [WeakestPrecondition.cmd_body].
 
   refine (
-  let t : varname := 1%Z in
+  let trim : varname := 1%Z in
   let prev : varname := 2%Z in
   let rx : varname := 3%Z in
   let tx : varname := 4%Z in
   let running : varname := 5%Z in
   let bit31 : varname := 6%Z in
+  let one : varname := 7%Z in
+  let dot : varname := 8%Z in
+  let uart_tx : varname := 9%Z in
   _).
 
-  let keys := eval cbv in [running; prev; bit31; t] in
-  eexists Z, _, (fun v t _ l => exists p, map.of_list keys [word.of_Z v; p; word.of_Z(2^31); word.of_Z(46)] = Some l ).
+  let keys := eval cbv in [running; prev; bit31; one; dot] in
+  eexists Z, _, (fun v t _ l => exists p, map.of_list keys [word.of_Z v; p; word.of_Z(2^31); word.of_Z(1); word.of_Z(46)] = Some l ).
   split; [unshelve eapply (Z.lt_wf 0)|].
   split.
   { eexists. eexists. cbn. reflexivity. }
-  { intros V trace ? ? H.
+  { intros V t ? ? H.
     destruct H. cbn in H. injection H; clear H; intro H; symmetry in H.
     repeat straightline.
     split; repeat straightline.
