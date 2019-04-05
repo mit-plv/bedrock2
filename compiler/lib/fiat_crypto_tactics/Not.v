40e22bd7c21fcf77647365b67cf382588bdb183d
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-04-05 19:43:24 -0400

    delete compiler/lib

--- compiler/lib/fiat_crypto_tactics/Not.v
+++
@@ -1,4 +0,0 @@
-Require Import lib.fiat_crypto_tactics.Test.
-
-(** [not tac] is equivalent to [fail tac "succeeds"] if [tac] succeeds, and is equivalent to [idtac] if [tac] fails *)
-Tactic Notation "not" tactic3(tac) := try ((test tac); fail 1 tac "succeeds").
