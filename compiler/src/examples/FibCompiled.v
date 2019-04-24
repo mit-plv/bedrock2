168f33d2586daddc4a5cb0fd4086d5f4e405fcaf
Thomas Carotti <thomas@carotti.co.uk>
2019-04-24 19:17:39 -0400

    End to end fib demo without concrete exprimp for now

--- compiler/src/examples/FibCompiled.v
+++ compiler/src/examples/FibCompiled.v
@@ -1,6 +1,8 @@
 Require Import compiler.Pipeline.
 Require Import riscv.Platform.MetricLogging.
 Require Import bedrock2.Examples.Demos.
+Require Import bedrock2.Map.SeparationLogic.
+Require Import compiler.Simp.
 
 Section FibCompiled.
 
@@ -48,12 +50,12 @@ Section FibCompiled.
   Definition fib_ExprImp(n: Z): Syntax.cmd. Admitted.
                                   
   Axiom fib_program_logic: forall n t m l mc,
-     (Z.of_nat n) < BinInt.Z.pow_pos 2 32 ->
-     Semantics.exec map.empty (fib_ExprImp (Z.of_nat n)) t m l mc (
+     n < BinInt.Z.pow_pos 2 32 ->
+     Semantics.exec map.empty (fib_ExprImp n) t m l mc (
                       fun t' m ' l' mc' =>
-                        bedrock2.MetricLogging.instructions mc' <= bedrock2.MetricLogging.instructions mc + (Z.of_nat n) * 34 + 39 /\
+                        bedrock2.MetricLogging.instructions mc' <= bedrock2.MetricLogging.instructions mc + 34 * n + 39 /\
                         exists (result: Utility.word),
-                          word.unsigned result = (fib n) /\
+                          word.unsigned result = (fib (Z.to_nat n)) /\
                           map.get l' regb = Some result).
 
   Lemma fib_demo: forall n,
@@ -66,20 +68,36 @@ Section FibCompiled.
                              word.unsigned result = (fib (Z.to_nat n)) /\
                              finalL.(getMetrics).(instructions) <= 34 * n + 39).
   Proof.
-    pose proof Pipeline.exprImp2Riscv_correct.
     intros.
-    eapply runsToNonDet.runsTo_weaken; [eapply H|].
-    9 : {
-      eapply ExprImp.weaken_exec; [eapply fib_program_logic with (n := (Z.to_nat n))|].
-      + match goal with
-        | |- _ < ?x => let r := eval cbv in x in change x with r
-        end.
-        rewrite Z2Nat.id by blia. blia.
-      + intros.
-        simpl in H1.
-        + 
-      eapply fib_program_logic.
-      10 : {
-      intros.
-      
+    eapply runsToNonDet.runsTo_weaken.
+    - eapply Pipeline.exprImp2Riscv_correct with (sH := fib_ExprImp n)
+                                                 (mcH := bedrock2.MetricLogging.EmptyMetricLog)
+                                                 (mH := map.empty).
+      + admit.
+      + admit.
+      + reflexivity.
+      + admit.
+      + symmetry. apply SimplWordExpr.add_0_l.
+      + reflexivity.
+      + admit. (* Should be solvable using seplog. *)
+      + admit. (* Can we get this from p? *)
+      + eapply ExprImp.weaken_exec; [eapply fib_program_logic|].
+        * match goal with
+          | |- _ < ?x => let r := eval cbv in x in change x with r
+          end.
+          blia.
+        * intros.
+          match goal with
+          | H: _ ?t ?m ?l ?mc |- _ ?t ?m ?l ?mc => apply H
+          end.
+    - intros. simpl in *. simp.
+      eexists. repeat split.
+      + instantiate (1 := x2). (* modvars/extends get lemma? *) admit.
+      + eassumption.
+      + repeat unfold_MetricLog. repeat simpl_MetricLog. simp. simpl in *.
+        repeat rewrite Z.sub_0_r in *.
+        repeat rewrite Z.add_0_l in *.
+        eapply Z.le_trans; [eassumption|assumption].
+  Admitted.
+
 End FibCompiled.
