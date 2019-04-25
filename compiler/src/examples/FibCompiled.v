751f785399ff50bc4333bab38457ddfbaaf192d3
Thomas Carotti <thomas@carotti.co.uk>
2019-04-24 22:43:37 -0400

    Programming machine correctly and remove an admit

--- compiler/src/examples/FibCompiled.v
+++ compiler/src/examples/FibCompiled.v
@@ -1,8 +1,11 @@
 Require Import compiler.Pipeline.
 Require Import riscv.Platform.MetricLogging.
+Require Import riscv.Platform.MetricRiscvMachine.
 Require Import bedrock2.Examples.Demos.
 Require Import bedrock2.Map.SeparationLogic.
 Require Import compiler.Simp.
+Require Import coqutil.Word.Interface.
+Require Import coqutil.Z.HexNotation.
 
 Section FibCompiled.
 
@@ -17,17 +20,22 @@ Section FibCompiled.
     GoFlatToRiscv.mcomp_sat.
 
   Definition zeroedRiscvMachine: RiscvMachine := {|
-  getMetrics := EmptyMetricLog;
-  getMachine := {|
-    getRegs := map.empty;
-    getPc := word.of_Z 0;
-    getNextPc := word.of_Z 4;
-    getMem := map.empty;
-    getLog := nil;
-  |};
-|}.
+    getMetrics := EmptyMetricLog;
+    getMachine := {|
+      getRegs := map.empty;
+      getPc := word.of_Z 0;
+      getNextPc := word.of_Z 4;
+      getMem := map.empty;
+      getLog := nil;
+    |};
+  |}.
 
-  Definition programmedMachine (n : Z) : RiscvMachine := zeroedRiscvMachine (* putProgram (compile (fib_ExprImp n)) zeroedriscvmachine *).
+  Definition fib_ExprImp(n: Z): Syntax.cmd. Admitted.
+
+  Definition imem(n: Z) := List.map encode (ExprImp2Riscv (fib_ExprImp n)).
+
+  Definition programmedMachine (n : Z) : RiscvMachine :=
+    putProgram (imem n) (getPc zeroedRiscvMachine) zeroedRiscvMachine.
 
   Definition run1 : Pipeline.M unit := @run1 _ _ _ _ Pipeline.RVM _ iset.
 
@@ -41,13 +49,9 @@ Section FibCompiled.
       end
     end.
 
-  Eval vm_compute in (fib 35).
-
   Definition regb : Register := 2.
 
   Existing Instance Demos.Fibonacci.ZNames.Inst.
-
-  Definition fib_ExprImp(n: Z): Syntax.cmd. Admitted.
                                   
   Axiom fib_program_logic: forall n t m l mc,
      n < BinInt.Z.pow_pos 2 32 ->
@@ -70,14 +74,13 @@ Section FibCompiled.
   Proof.
     intros.
     eapply runsToNonDet.runsTo_weaken.
-    - eapply Pipeline.exprImp2Riscv_correct with (sH := fib_ExprImp n)
-                                                 (mcH := bedrock2.MetricLogging.EmptyMetricLog)
-                                                 (mH := map.empty).
+    - eapply Pipeline.exprImp2Riscv_correct with (sH := fib_ExprImp n) 
+        (mcH := bedrock2.MetricLogging.EmptyMetricLog) (mH := map.empty).
       + admit.
       + admit.
       + reflexivity.
-      + admit.
-      + symmetry. apply SimplWordExpr.add_0_l.
+      + simpl. rewrite word.unsigned_of_Z_0. reflexivity.
+      + reflexivity.
       + reflexivity.
       + admit. (* Should be solvable using seplog. *)
       + admit. (* Can we get this from p? *)
@@ -92,7 +95,10 @@ Section FibCompiled.
           end.
     - intros. simpl in *. simp.
       eexists. repeat split.
-      + instantiate (1 := x2). (* modvars/extends get lemma? *) admit.
+      + match goal with
+        | H: map.extends ?m1 _ |- map.get ?m1 _ = Some _ => unfold map.extends in H; apply H
+        end.
+        eassumption.
       + eassumption.
       + repeat unfold_MetricLog. repeat simpl_MetricLog. simp. simpl in *.
         repeat rewrite Z.sub_0_r in *.
