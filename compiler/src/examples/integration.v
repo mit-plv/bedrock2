66dafb9b08cf907d7b1391816c454a1624194275
Andres Erbsen <andreser@mit.edu>
2019-04-30 10:59:46 -0400

    fix integration to use 32-bit code, still no luck

--- compiler/src/examples/integration.v
+++ compiler/src/examples/integration.v
@@ -5,8 +5,8 @@ Require Import coqutil.Decidable.
 Require Import compiler.ExprImp.
 Require Import compiler.NameGen.
 Require Import compiler.PipelineWithRename.
-Require Import riscv.Utility.Words64Naive.
-Require Import riscv.Utility.DefaultMemImpl64.
+Require Import riscv.Utility.Words32Naive.
+Require Import riscv.Utility.DefaultMemImpl32.
 Require Import riscv.Utility.Monads.
 Require Import compiler.util.Common.
 Require Import coqutil.Decidable.
@@ -44,28 +44,29 @@ Instance flatToRiscvDef_params: FlatToRiscvDef.FlatToRiscvDef.parameters := {
   FlatToRiscvDef.FlatToRiscvDef.compile_ext_call_emits_valid _ _ := TODO;
 }.
 Notation RiscvMachine := (MetricRiscvMachine Register FlatToRiscvDef.FlatToRiscvDef.actname).
-Instance pipeline_params: Pipeline.parameters. simple refine {|
+Definition params : Pipeline.parameters. simple refine {|
   Pipeline.locals := _;
   Pipeline.Registers := _;
   Pipeline.ext_spec _ _ := TODO;
   Pipeline.ext_guarantee _ := False;
   Pipeline.PRParams := TODO;
 |}; unshelve (try exact _); apply TODO. Defined.
-Instance pipeline_assumptions: @Pipeline.assumptions pipeline_params. Admitted.
+Definition flatparams := (FlattenExpr.mk_Syntax_params (@Pipeline.FlattenExpr_parameters params)).
+Instance pipeline_assumptions: @Pipeline.assumptions params. Admitted.
 Instance mapops: RegAlloc.map.ops (SortedListString.map Z) :=
   {| RegAlloc.map.intersect (s1 s2 : SortedListString.map Z) :=
     {| value := ListLib.list_intersect (value s1) (value s2); _value_ok := TODO |} |}.
 
 Definition compile '(functions, initial, reactive) :=
-  compile_prog (RegAlloc.map.putmany_of_tuples map.empty functions) initial reactive (List.map fst functions).
+  compile_prog (p:=params) (RegAlloc.map.putmany_of_tuples map.empty functions) initial reactive (List.map fst functions).
 Definition instrencode p : list byte :=
   let word8s := List.flat_map (fun inst => HList.tuple.to_list (LittleEndian.split 4 (encode inst))) p in
   List.map (fun w => Byte.of_Z (word.unsigned w)) word8s.
 
 Definition prog := (
   [swap; swap_swap],
-  @cmd.call (FlattenExpr.mk_Syntax_params _) [] "swap_swap" [expr.literal 100; expr.literal 108],
-  @cmd.interact (FlattenExpr.mk_Syntax_params _) [] "nop" []).
+  @cmd.call flatparams [] "swap_swap" [expr.literal 100; expr.literal 108],
+  @cmd.interact flatparams [] "nop" []).
 
 
 Import riscv.Utility.InstructionNotations.
@@ -78,6 +79,5 @@ Goal True.
   Local Open Scope hexdump_scope.
 
   let x := eval cbv in (instrencode asm) in
-  idtac (* x *).
-  do 32 idtac "0a552583".
+  idtac x.
 Abort.
