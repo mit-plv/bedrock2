3922ab8ed682bfc5ed4bc01effacfbd5e6bfb86c
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-05-17 11:11:10 -0400

    all compiler OOM problems were due to admitted List.intersect
    Running the renamer with 50 registers instead of 31-2 shows by how much
    we would have to reduce register usage:
    - iot is fine
    - lightbulb max register is 36 --> need to save 5
    - recvEthernet max register is 41 --> need to save 10

--- compiler/src/examples/lightbulb.v
+++ compiler/src/examples/lightbulb.v
@@ -42,21 +42,24 @@ Instance flatToRiscvDef_params: FlatToRiscvDef.FlatToRiscvDef.parameters := {
   FlatToRiscvDef.FlatToRiscvDef.compile_ext_call_emits_valid _ _ := TODO;
 }.
 Notation RiscvMachine := MetricRiscvMachine.
-Definition params : Pipeline.parameters. simple refine {|
-  Pipeline.locals := _;
-  Pipeline.Registers := _;
-  Pipeline.ext_spec _ _ := TODO;
-  Pipeline.ext_guarantee _ := False;
-  Pipeline.PRParams := TODO;
-|}; unshelve (try exact _); apply TODO. Defined.
-Definition flatparams := (FlattenExpr.mk_Syntax_params (@Pipeline.FlattenExpr_parameters params)).
-Instance pipeline_assumptions: @Pipeline.assumptions params. Admitted.
+
 Instance mapops: RegAlloc.map.ops (SortedListString.map Z). refine (
   {| RegAlloc.map.intersect (s1 s2 : SortedListString.map Z) :=
     {| value := ListLib.list_intersect (fun '(k,v) '(k',v') => andb (_ k k') (_ v v')) (value s1) (value s2); _value_ok := TODO |} |}).
 - exact String.eqb.
 - exact Z.eqb.
 Defined.
+
+Definition params : Pipeline.parameters. simple refine {|
+  Pipeline.locals := _;
+  Pipeline.Registers := _;
+  Pipeline.ext_spec _ _ := TODO;
+  Pipeline.ext_guarantee _ := False;
+  Pipeline.PRParams := TODO;
+  Pipeline.src2imp_ops := mapops;
+|}; unshelve (try exact _); apply TODO. Defined.
+Definition flatparams := (FlattenExpr.mk_Syntax_params (@Pipeline.FlattenExpr_parameters params)).
+Instance pipeline_assumptions: @Pipeline.assumptions params. Admitted.
 
 (* stack grows from high addreses to low addresses, first stack word will be written to
    (stack_pastend-4), next stack word to (stack_pastend-8) etc *)
@@ -78,78 +81,69 @@ Definition prog := (
   @cmd.skip flatparams,
   @cmd.call flatparams [] "iot" []).
 
-(*
+Definition dummy_iot :=
+let p_addr := "p_addr" in
+let bytesWritten := "bytesWritten" in
+let recvEthernet := "recvEthernet" in
+let lightbulb := "lightbulb" in
+let r := "r" in
+("iot", ([p_addr], [r], (@cmd.set (@syntax FE310CSemantics.parameters) r (lightbulb.literal (1234))))).
+
+Definition dummy_lightbulb :=
+let packet := "packet" in
+let len := "len" in
+let ethertype := "ethertype" in
+let protocol := "protocol" in
+let port := "port" in
+let mmio_val := "mmio_val" in
+let command := "command" in
+let MMIOREAD := "MMIOREAD" in
+let MMIOWRITE := "MMIOWRITE" in
+let r := "r" in
+("lightbulb",
+([packet; len], [r], (@cmd.set (@syntax FE310CSemantics.parameters) r (lightbulb.literal (321))))).
+
+Definition dummy_recvEthernet :=
+let info := "info" in
+let rxunused := "rx_unused" in
+let rx_status := "rx_status" in
+let rx_packet := "rx_packet" in
+let c := "c" in
+let len_bytes := "len_bytes" in
+let len_words := "len_words" in
+let word := "word" in
+let lan9250_readword := "lan9250_readword" in
+let r := "r" in
+("recvEthernet",
+([rx_packet], [r], (@cmd.set (@syntax FE310CSemantics.parameters) r (lightbulb.literal (432))))).
+
+Definition dummy_prog := (
+  [dummy_iot; dummy_lightbulb; recvEthernet],
+  @cmd.skip flatparams,
+  @cmd.call flatparams [] "iot" []).
+
 Import riscv.Utility.InstructionNotations.
 Import bedrock2.Hexdump.
 Local Open Scope hexdump_scope.
 Set Printing Width 108.
-*)
 
-Goal compile prog = nil.
-  cbv [compile compile_prog functions2Riscv prog].
-  match goal with
-  | |- context [@flatten_functions ?p ?E ?Fs] =>
-    let r := eval cbv in (@flatten_functions p E Fs) in change (@flatten_functions p E Fs) with r
-  end.
-  cbv [RegAlloc.rename_functions funnames List.map fst].
-  cbv [iot lightbulb recvEthernet].
 
-  match goal with
-  | |- context [?T] =>
-    lazymatch T with
-      RegAlloc.rename_function available_registers ?E ?N =>
-      assert (T = (nil, nil, FlatImp.SSkip))
-    end
-  end.
-  {
-    unfold RegAlloc.rename_function.
-    match goal with
-    | |- context [@map.get ?k ?v ?m ?K ?M] =>
-      let r := eval cbv in (@map.get k v m K M) in change (@map.get k v m K M) with r
-    end.
-    cbv iota beta.
-    unfold RegAlloc.rename_fun.
-    progress simpl (RegAlloc.rename_binds ["rx_packet"] available_registers).
-    cbv iota beta.
-    progress simpl (RegAlloc.rename_binds ["r"]
-        [4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19; 20; 21; 22; 23; 24; 25; 26;
-        27; 28; 29; 30; 31; 32; 33; 34; 35; 36; 37; 38; 39; 40; 41; 42; 43; 44; 45; 46; 47; 48;
-        49; 50; 51; 52]).
-    cbv iota beta.
-    cbv [RegAlloc.rename_stmt].
-    match goal with
-    | |- context [RegAlloc.rename ?A ?B ?C] =>
-      let r := eval cbv in (RegAlloc.rename A B C) in
-          change (RegAlloc.rename  A B C) with r
-    end.
-    Set Printing Depth 100000.
-    cbv iota beta.
-    match goal with
-    | |- context [@RegAlloc.checker ?srcvar ?impvar ?func ?act ?src2imp ?ops ?m ?s] =>
-      set (test := ops)
-    end.
-    exfalso.
-
-    pose (m1 := map.put (map.put map.empty "a" 1) "b" 2).
-    pose (m2 := map.put (map.put map.empty "a" 1) "b" 22).
-    cbv in m1.
-    cbv in m2.
-    pose (m3 := RegAlloc.map.intersect m1 m2).
-    unfold RegAlloc.map.intersect, test, mapops in m3.
-    subst m1 m2.
-    assert (m3 = map.empty). {
-      subst test m3.
-      unfold ListLib.list_intersect.
-      simpl.
-      (* intersect now works *)
-Abort.
-
-(*
 Goal True.
   Time
   let r := eval vm_compute in (([[
                          ]] ++ compile prog)%list%Z) in
   pose r as asm.
+
+  (* searching for "addi    x2, x2, -" shows where the functions begin, and the first
+     thing they do is to save all used registers onto the stack, so we can look for the
+     max there to see how many registers a function needs *)
+
+  (* - iot is fine
+     - lightbulb's max register is 36 --> need to save 5
+     - recvEthernets' max register is 41 --> need to save 10
+  *)
+
+
   (* {| value := [("iot", 0); ("lightbulb", 112); ("recvEthernet", 132)]; _value_ok := eq_refl |} *)
   Import bedrock2.NotationsCustomEntry.
   Set Printing Width 120.
@@ -159,4 +153,3 @@ Goal True.
   let x := eval cbv in (instrencode asm) in
   idtac x.
 Abort.
-*)
