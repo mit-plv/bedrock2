8faf005d0720323e7d0a420430aa750b21d2c3d4
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-05-22 11:19:01 +0200

    FlattenExprSimulation works; we can put precondition on high-level state into simulation because it still holds at the end, so we do not need a special initial state restriction

--- compiler/src/FlattenExprSimulation.v
+++ compiler/src/FlattenExprSimulation.v
@@ -3,6 +3,7 @@ Require Import coqutil.Tactics.Tactics.
 Require Import bedrock2.MetricLogging.
 Require Import compiler.FlattenExprDef.
 Require Import compiler.FlattenExpr.
+Require Import compiler.NameGen.
 Require Import compiler.Simulation.
 Require Import compiler.Simp.
 
@@ -32,9 +33,8 @@ Section Sim.
       t1 = t2 /\
       m1 = m2 /\
       map.extends l2 l1 /\
+      map.undef_on l1 (allFreshVars (freshNameGenState (ExprImp.allVars_cmd_as_list c1))) /\
       (mc2 <= mc1)%metricsH.
-
-  Axiom TODO_restrict_initial_state: False.
 
   Lemma flattenExprSim{hyps: FlattenExpr.assumptions p}: simulation exec1 exec2 related.
   Proof.
@@ -48,6 +48,25 @@ Section Sim.
     | |- context [ fst ?x ] => destruct x as [s ngs] eqn: E
     end.
     simpl.
+    assert (PropSet.disjoint (ExprImp.allVars_cmd c1)
+                             (allFreshVars (freshNameGenState (ExprImp.allVars_cmd_as_list c1))))
+      as D. {
+      intro x.
+      pose proof (NameGen.freshNameGenState_spec (ExprImp.allVars_cmd_as_list c1) x) as P.
+      assert (varname_eq_dec: forall a b: Syntax.varname, {a = b} + {a <> b}). {
+        intros. destr (Semantics.varname_eqb a b); auto.
+      }
+      match type of P with
+      | List.In ?x ?l -> _ => destruct (List.in_dec varname_eq_dec x l) as
+            [Iyes | Ino]
+      end.
+      * auto.
+      * left. clear -Ino.
+        intro. apply Ino.
+        epose proof (ExprImp.allVars_cmd_allVars_cmd_as_list _ _) as P. destruct P as [P _].
+        apply P.
+        apply H.
+    }
     eapply FlatImp.exec.weaken.
     - eapply @flattenStmt_correct_aux.
       + typeclasses eauto.
@@ -55,26 +74,16 @@ Section Sim.
       + reflexivity.
       + exact E.
       + assumption.
-      + case TODO_restrict_initial_state.
-      + intro x.
-        pose proof (NameGen.freshNameGenState_spec (ExprImp.allVars_cmd_as_list c1) x) as P.
-        assert (varname_eq_dec: forall a b: Syntax.varname, {a = b} + {a <> b}). {
-          intros. destr (Semantics.varname_eqb a b); auto.
-        }
-        match type of P with
-        | List.In ?x ?l -> _ => destruct (List.in_dec varname_eq_dec x l) as
-              [Iyes | Ino]
-        end.
-        * auto.
-        * left. clear -Ino.
-          intro. apply Ino.
-          epose proof (ExprImp.allVars_cmd_allVars_cmd_as_list _ _) as P. destruct P as [P _].
-          apply P.
-          apply H.
+      + assumption.
+      + exact D.
     - simpl. intros. simp.
       eexists. split; [|eassumption]. simpl. rewrite E. simpl.
       repeat (split; [solve [auto]|]).
-      solve_MetricLog.
+      split.
+      + pose proof (ExprImp.modVars_subset_allVars c1).
+        simpl in *. (* PARAMRECORDS *)
+        Solver.map_solver FlattenExpr.locals_ok.
+      + solve_MetricLog.
   Qed.
 
 (*
