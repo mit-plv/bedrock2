1b26c2083190d73230bcac403da4da1d5570cf8f
Andres Erbsen <andreser@mit.edu>
2019-05-24 18:00:22 -0700

    wip

--- compiler/src/examples/integration.v
+++ compiler/src/examples/integration.v
@@ -1,6 +1,5 @@
 Require Import Coq.Lists.List.
 Import ListNotations.
-Require bedrock2.Examples.Demos.
 Require Import coqutil.Decidable.
 Require Import compiler.ExprImp.
 Require Import compiler.NameGen.
@@ -22,8 +21,9 @@ Require Import riscv.Utility.InstructionCoercions.
 Require Import riscv.Platform.MetricRiscvMachine.
 Require Import bedrock2.Byte.
 Require bedrock2.Hexdump.
-Require Import bedrock2.Examples.swap.
 Require Import bedrock2.Examples.SPI.
+Require Import bedrock2.Examples.LAN9250.
+Require Import bedrock2.Examples.lightbulb.
 
 Open Scope Z_scope.
 Open Scope string_scope.
@@ -51,17 +51,8 @@ Instance flatToRiscvDef_params: FlatToRiscvDef.FlatToRiscvDef.parameters := {
   FlatToRiscvDef.FlatToRiscvDef.compile_ext_call_length _ := TODO;
   FlatToRiscvDef.FlatToRiscvDef.compile_ext_call_emits_valid _ _ := TODO;
 }.
-
 Notation RiscvMachine := MetricRiscvMachine.
-Definition params : Pipeline.parameters. simple refine {|
-  Pipeline.locals := _;
-  Pipeline.Registers := _;
-  Pipeline.ext_spec _ _ := TODO;
-  Pipeline.ext_guarantee _ := False;
-  Pipeline.PRParams := TODO;
-|}; unshelve (try exact _); apply TODO. Defined.
-Definition flatparams := (FlattenExpr.mk_Syntax_params (@Pipeline.FlattenExpr_parameters params)).
-Instance pipeline_assumptions: @Pipeline.assumptions params. Admitted.
+
 Instance mapops: RegAlloc.map.ops (SortedListString.map Z). refine (
   {| RegAlloc.map.intersect (s1 s2 : SortedListString.map Z) :=
     {| value := ListLib.list_intersect (fun '(k,v) '(k',v') => andb (_ k k') (_ v v')) (value s1) (value s2); _value_ok := TODO |};
@@ -70,6 +61,17 @@ Instance mapops: RegAlloc.map.ops (SortedListString.map Z). refine (
 - exact String.eqb.
 - exact Z.eqb.
 Defined.
+
+Definition params : Pipeline.parameters. simple refine {|
+  Pipeline.locals := _;
+  Pipeline.Registers := _;
+  Pipeline.ext_spec _ _ := TODO;
+  Pipeline.ext_guarantee _ := False;
+  Pipeline.PRParams := TODO;
+  Pipeline.src2imp_ops := mapops;
+|}; unshelve (try exact _); apply TODO. Defined.
+Definition flatparams := (FlattenExpr.mk_Syntax_params (@Pipeline.FlattenExpr_parameters params)).
+Instance pipeline_assumptions: @Pipeline.assumptions params. Admitted.
 
 (* stack grows from high addreses to low addresses, first stack word will be written to
    (stack_pastend-4), next stack word to (stack_pastend-8) etc *)
@@ -86,29 +88,26 @@ Definition instrencode p : list byte :=
   let word8s := List.flat_map (fun inst => HList.tuple.to_list (LittleEndian.split 4 (encode inst))) p in
   List.map (fun w => Byte.of_Z (word.unsigned w)) word8s.
 
-Definition i : @varname flatparams := "i".
-Require Import coqutil.Z.HexNotation.
 Definition prog := (
-  [swap; swap_swap], (* TODO: spi_read here *)
-  @cmd.call flatparams [] "swap_swap" [expr.literal (2^9); expr.literal (2^9+4)],
-  cmd.seq (cmd.set i (expr.load access_size.word (expr.literal (2^9-2))))
-  (cmd.seq (cmd.store access_size.word (expr.literal (Ox"1001200c")) (expr.var i))
-  (cmd.store access_size.word (expr.literal (2^9-2)) (expr.op bopname.add (expr.var i) (expr.literal 1))))).
+  [spi_xchg; spi_write; spi_read],
+  @cmd.skip flatparams,
+  (@cmd.call flatparams ["x"; "x"] "spi_xchg" [expr.literal 63])).
 
 Import riscv.Utility.InstructionNotations.
 Import bedrock2.Hexdump.
 Local Open Scope hexdump_scope.
 Set Printing Width 108.
+
+
 Goal True.
-  let r := eval cbv in (([[
-                             Sw 0 0 (2^9-4);
-                             Addi 7 0 (42);
-                             Sw 0 7 (2^9);
-                             Addi 7 0 (36);
-                             Sw 0 7 (2^9+4)
+  let r := eval vm_compute in (([[
                          ]] ++ compile prog)%list%Z) in
   pose r as asm.
+  Import bedrock2.NotationsCustomEntry.
 
+  (* searching for "addi    x2, x2, -" shows where the functions begin, and the first
+     thing they do is to save all used registers onto the stack, so we can look for the
+     max there to see how many registers a function needs *)
 
   let x := eval cbv in (instrencode asm) in
   idtac x.
