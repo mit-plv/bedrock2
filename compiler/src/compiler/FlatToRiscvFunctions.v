897e87a28c230d559270c471f7eba3bfbe37a885
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-10-10 19:54:39 -0400

    move code to coqutil and compiler/SeparationLogic.v
    does not compile

--- compiler/src/compiler/FlatToRiscvFunctions.v
+++ compiler/src/compiler/FlatToRiscvFunctions.v
@@ -304,61 +304,7 @@ Section Proofs.
       repeat (simpl; rewrite ?List.app_length); erewrite ?IHs1; erewrite ?IHs2; try reflexivity.
   Qed.
 
-  Lemma f_equal2: forall {A B: Type} {f1 f2: A -> B} {a1 a2: A},
-      f1 = f2 -> a1 = a2 -> f1 a1 = f2 a2.
-  Proof. intros. congruence. Qed.
-
-  (* TODO put in coqutil, share with bsearch *)
-  Ltac is_lia P :=
-    lazymatch P with
-    | @eq Z _ _ => idtac
-    | not (@eq Z _ _) => idtac
-    | (_ < _)%Z => idtac
-    | (_ <= _)%Z => idtac
-    | (_ <= _ < _)%Z => idtac
-    | @eq nat _ _ => idtac
-    | not (@eq nat _ _) => idtac
-    | (_ < _)%nat => idtac
-    | (_ <= _)%nat => idtac
-    | (_ <= _ < _)%nat => idtac
-    | _ => fail "The term" P "is not LIA"
-    end.
-
-  (* unifies two separation logic clauses syntactically, instantiating as many evars
-     as it wants, and for subterms of type word, applies word solver instead of syntatic unify,
-     and for subterms of type Z, applies lia instead of syntactic unify *)
-  Ltac wclause_unify OK :=
-    lazymatch type of OK with
-    | word.ok ?WORD =>
-      lazymatch goal with
-      | |- @eq ?T ?x ?y =>
-        tryif first [is_evar x | is_evar y | constr_eq x y] then (
-          reflexivity
-        ) else (
-          tryif (unify T (@word.rep _ WORD)) then (
-            solve [simpl_addrs; autorewrite with rew_word_morphism; solve_word_eq OK]
-          ) else (
-            tryif (unify T Z) then (
-              solve [bomega]
-            ) else (
-              lazymatch x with
-              | ?x1 ?x2 => lazymatch y with
-                           | ?y1 ?y2 => refine (f_equal2 _ _); wclause_unify OK
-                           | _ => fail "" x "is an application while" y "is not"
-                           end
-              | _ => lazymatch y with
-                     | ?y1 ?y2 => fail "" x "is not an application while" y "is"
-                     | _ => tryif constr_eq x y then reflexivity else fail "" x "does not match" y
-                     end
-              end
-            )
-          )
-        )
-      end
-    | _ => fail 1000 "OK does not have the right type"
-    end.
-
-  Ltac tag P :=
+  Ltac tag P ::=
     let __ := lazymatch type of P with
               | @map.rep _ _ _ -> Prop => idtac
               | _ => fail 10000 P "is not a sep clause"
@@ -371,7 +317,7 @@ Section Proofs.
     | _ => fail "no recognizable tag"
     end.
 
-  Ltac addr P :=
+  Ltac addr P ::=
     let __ := lazymatch type of P with
               | @map.rep _ _ _ -> Prop => idtac
               | _ => fail 10000 P "is not a sep clause"
@@ -382,61 +328,6 @@ Section Proofs.
     | ptsto_word ?A _ => A
     | _ => fail "no recognizable address"
     end.
-
-  (* should completely solve a sepclause equality or fail *)
-  Ltac sepclause_eq :=
-    match goal with
-    | |- ?G => assert_fails (has_evar G);
-               wclause_unify (@word_ok (@W (@def_params p)))
-    | |- ?lhs = ?rhs => let tagL := tag lhs in
-                        let tagR := tag rhs in
-                        constr_eq tagL tagR;
-                        wclause_unify (@word_ok (@W (@def_params p)))
-    | |- ?lhs = ?rhs => let addrL := addr lhs in
-                        let addrR := addr rhs in
-                        assert_fails (has_evar addrL);
-                        assert_fails (has_evar addrR);
-                        replace addrL with addrR by solve_word_eq (@word_ok (@W (@def_params p)));
-                        reflexivity
-    end.
-
-  Ltac pick_nat n :=
-    multimatch n with
-    | S ?m => constr:(m)
-    | S ?m => pick_nat m
-    end.
-
-  Ltac wcancel_step := once (
-    let RHS := lazymatch goal with |- Lift1Prop.iff1 _ (seps ?RHS) => RHS end in
-    let jy := index_and_element_of RHS in (* <-- multi-success! *)
-    let j := lazymatch jy with (?i, _) => i end in
-    let y := lazymatch jy with (_, ?y) => y end in
-    assert_fails (idtac; let y := rdelta_var y in is_evar y);
-    let LHS := lazymatch goal with |- Lift1Prop.iff1 (seps ?LHS) _ => LHS end in
-    let l := eval cbv [List.length] in (List.length LHS) in
-    let i := pick_nat l in (* <-- multi-success! *)
-    cancel_seps_at_indices i j; [sepclause_eq|]).
-
-  Ltac wcancel :=
-    cancel;
-    repeat wcancel_step;
-    try solve [ecancel_done'].
-
-  (* we do use the "tag" mechanism, but as part of wcancel, not as a separate cancel method *)
-  Ltac cancel_by_tag_UNUSED :=
-    let RHS := lazymatch goal with |- Lift1Prop.iff1 _ (seps ?RHS) => RHS end in
-    let jy := index_and_element_of RHS in
-    let j := lazymatch jy with (?i, _) => i end in
-    let y := lazymatch jy with (_, ?y) => y end in
-    let tagR := tag y in
-    assert_fails (idtac; let y := rdelta_var y in is_evar y);
-    let LHS := lazymatch goal with |- Lift1Prop.iff1 (seps ?LHS) _ => LHS end in
-    let l := eval cbv [List.length] in (List.length LHS) in
-    let i := pick_nat l in
-    let x := eval cbv [List.nth] in (List.nth i LHS (emp True)) in
-    let tagL := tag x in
-    constr_eq tagL tagR;
-    cancel_seps_at_indices i j.
 
   Ltac safe_sidecond :=
     match goal with
--- compiler/src/compiler/SeparationLogic.v
+++ compiler/src/compiler/SeparationLogic.v
@@ -2,10 +2,13 @@ Require Export Coq.Lists.List. Export ListNotations.
 Require Export Coq.ZArith.BinInt. Open Scope Z_scope.
 Require Export coqutil.Word.Interface coqutil.Word.Properties.
 Require Export coqutil.Map.Interface.
+Require Import coqutil.Tactics.rdelta.
+Require Export coqutil.Z.Lia.
 Require Export bedrock2.Lift1Prop.
 Require Export bedrock2.Map.Separation.
 Require Export bedrock2.Map.SeparationLogic.
 Require Export bedrock2.Array.
+Require Export compiler.SimplWordExpr.
 
 Infix "*" := sep : sep_scope.
 
@@ -15,3 +18,127 @@ Arguments iff1 {T} (_)%sep (_)%sep.
 
 (* TODO does not get rid of %sep in printing as intended *)
 Arguments sep {key} {value} {map} (_)%sep (_)%sep.
+
+
+Lemma f_equal2: forall {A B: Type} {f1 f2: A -> B} {a1 a2: A},
+    f1 = f2 -> a1 = a2 -> f1 a1 = f2 a2.
+Proof. intros. congruence. Qed.
+
+(* unifies two separation logic clauses syntactically, instantiating as many evars
+   as it wants, but for subterms of type word, applies word solver instead of syntatic unify,
+   and for subterms of type Z, applies lia instead of syntactic unify *)
+Ltac wclause_unify OK :=
+  lazymatch type of OK with
+  | word.ok ?WORD =>
+    lazymatch goal with
+    | |- @eq ?T ?x ?y =>
+      tryif first [is_evar x | is_evar y | constr_eq x y] then (
+        reflexivity
+      ) else (
+        tryif (unify T (@word.rep _ WORD)) then (
+          solve [solve_word_eq OK]
+        ) else (
+          tryif (unify T Z) then (
+            solve [bomega]
+          ) else (
+            lazymatch x with
+            | ?x1 ?x2 => lazymatch y with
+                         | ?y1 ?y2 => refine (f_equal2 _ _); wclause_unify OK
+                         | _ => fail "" x "is an application while" y "is not"
+                         end
+            | _ => lazymatch y with
+                   | ?y1 ?y2 => fail "" x "is not an application while" y "is"
+                   | _ => tryif constr_eq x y then reflexivity else fail "" x "does not match" y
+                   end
+            end
+          )
+        )
+      )
+    end
+  | _ => fail 1000 "OK does not have the right type"
+  end.
+
+(* This can be overridden by the user.
+   The idea of a "tag" is that it's a subterm of a sepclause which is so unique that if
+   the same tag appears both on the left and on the right of an iff1, we're sure that
+   these two clauses should be matched & canceled with each other.
+   "tag" should return any Gallina term. *)
+Ltac tag P :=
+  let __ := lazymatch type of P with
+            | @map.rep _ _ _ -> Prop => idtac
+            | _ => fail 10000 P "is not a sep clause"
+            end in
+  lazymatch P with
+  | _ => fail "no recognizable tag"
+  end.
+
+(* This can be overridden by the user.
+   The idea of "addr" is that if the addresses of two sepclauses are the same,
+   we're sure that these two clauses should be matched & canceled with each other,
+   even if they still contain many evars outside of their address.
+   "addr" should return a Gallina term of type "word" *)
+Ltac addr P :=
+  let __ := lazymatch type of P with
+            | @map.rep _ _ _ -> Prop => idtac
+            | _ => fail 10000 P "is not a sep clause"
+            end in
+  lazymatch P with
+  | ptsto ?A _ => A
+  | array _ _ ?A _ => A
+  | _ => fail "no recognizable address"
+  end.
+
+(* completely solves a sepclause equality or fails *)
+Ltac sepclause_eq OK :=
+  match goal with
+  | |- ?G => assert_fails (has_evar G);
+             wclause_unify OK
+  | |- ?lhs = ?rhs => let tagL := tag lhs in
+                      let tagR := tag rhs in
+                      constr_eq tagL tagR;
+                      wclause_unify OK
+  | |- ?lhs = ?rhs => let addrL := addr lhs in
+                      let addrR := addr rhs in
+                      assert_fails (has_evar addrL);
+                      assert_fails (has_evar addrR);
+                      replace addrL with addrR by solve_word_eq OK;
+                      reflexivity
+  end.
+
+Ltac pick_nat n :=
+  multimatch n with
+  | S ?m => constr:(m)
+  | S ?m => pick_nat m
+  end.
+
+Ltac wcancel_step OK := once (
+  let RHS := lazymatch goal with |- Lift1Prop.iff1 _ (seps ?RHS) => RHS end in
+  let jy := index_and_element_of RHS in (* <-- multi-success! *)
+  let j := lazymatch jy with (?i, _) => i end in
+  let y := lazymatch jy with (_, ?y) => y end in
+  assert_fails (idtac; let y := rdelta_var y in is_evar y);
+  let LHS := lazymatch goal with |- Lift1Prop.iff1 (seps ?LHS) _ => LHS end in
+  let l := eval cbv [List.length] in (List.length LHS) in
+  let i := pick_nat l in (* <-- multi-success! *)
+  cancel_seps_at_indices i j; [sepclause_eq OK|]).
+
+Ltac wcancel OK :=
+  cancel;
+  repeat wcancel_step OK;
+  try solve [ecancel_done'].
+
+(* mostly useful for debugging, once everything works, wcancel should do all the work *)
+Ltac cancel_by_tag :=
+  let RHS := lazymatch goal with |- Lift1Prop.iff1 _ (seps ?RHS) => RHS end in
+  let jy := index_and_element_of RHS in
+  let j := lazymatch jy with (?i, _) => i end in
+  let y := lazymatch jy with (_, ?y) => y end in
+  let tagR := tag y in
+  assert_fails (idtac; let y := rdelta_var y in is_evar y);
+  let LHS := lazymatch goal with |- Lift1Prop.iff1 (seps ?LHS) _ => LHS end in
+  let l := eval cbv [List.length] in (List.length LHS) in
+  let i := pick_nat l in
+  let x := eval cbv [List.nth] in (List.nth i LHS (emp True)) in
+  let tagL := tag x in
+  constr_eq tagL tagR;
+  cancel_seps_at_indices i j.
