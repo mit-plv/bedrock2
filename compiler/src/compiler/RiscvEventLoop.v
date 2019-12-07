bd484d53a94e63a58524e77eac59e59fed4ded2b
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-12-06 23:19:24 -0500

    in RiscvEventLoop, put constraints on goodReadyState instead
    of doing `goodReadyState /\ constraints`, should be easier to
    use in Pipeline because less unfolding of goodReadyState,
    hopefully.
    
    (does not compile)

--- compiler/src/compiler/RiscvEventLoop.v
+++ compiler/src/compiler/RiscvEventLoop.v
@@ -54,50 +54,45 @@ Section EventLoop.
        constants [word_cst]).
 
   (* goodReadyState is the invariant which says that the machine is ready to execute
-     the next iteration of the infinite event loop. It should not say anything about
-     the pc, because it will be used both when the pc is at pc_start and when it is at
-     pc_end *)
-  Variable goodReadyState: RiscvMachineL -> Prop.
+     the next iteration of the infinite event loop.
+     It also gets the expected value of the PC, and has to check getPc and getNextPc. *)
+  Variable goodReadyState: word -> RiscvMachineL -> Prop.
+
+  Hypothesis goodReadyState_checks_PC: forall pc m,
+      goodReadyState pc m -> m.(getPc) = pc.
 
   Variables pc_start pc_end: word.
   Hypothesis pc_start_aligned: (word.unsigned pc_start) mod 4 = 0.
   Hypothesis start_ne_end: pc_start <> pc_end.
 
-  Hypothesis goodReadyState_ignores:
+  Hypothesis goodReadyState_preserved_by_jump_back:
     forall (state: RiscvMachineL) newMetrics,
-      goodReadyState state ->
-      state.(getPc) = pc_end ->
-      goodReadyState (withPc pc_start
-                     (withNextPc (word.add pc_start (word.of_Z 4))
-                     (withMetrics newMetrics state))).
+      goodReadyState pc_end state ->
+      goodReadyState pc_start (withPc pc_start
+                              (withNextPc (word.add pc_start (word.of_Z 4))
+                              (withMetrics newMetrics state))).
+
+  Hypothesis goodReadyState_implies_valid_machine: forall pc m,
+      goodReadyState pc m -> valid_machine m.
 
   Variable jump: Z.
   Hypothesis jump_bound: - 2 ^ 20 <= jump < 2 ^ 20.
   Hypothesis jump_aligned: jump mod 4 = 0.
   Hypothesis pc_end_def: pc_end = word.sub pc_start (word.of_Z jump).
 
+  Hypothesis goodReadyState_implies_jump_back_instr: forall m,
+      goodReadyState pc_end m ->
+      (exists R, (ptsto_instr pc_end (Jal Register0 jump) * R)%sep m.(getMem)) /\
+      subset (footpr (ptsto_instr pc_end (Jal Register0 jump)))
+             (of_list m.(getXAddrs)).
+
   (* loop body: between pc_start and pc_end *)
   Hypothesis body_correct: forall (initial: RiscvMachineL),
-      goodReadyState initial ->
-      initial.(getPc) = pc_start ->
-      valid_machine initial ->
-      subset (footpr (ptsto_instr pc_end (Jal Register0 jump)))
-             (of_list initial.(getXAddrs)) ->
-      runsTo (mcomp_sat (run1 iset)) initial (fun final =>
-          goodReadyState final /\
-          final.(getPc) = pc_end /\
-          (exists R, (ptsto_instr pc_end (Jal Register0 jump) * R)%sep final.(getMem)) /\
-          valid_machine final /\
-          subset (footpr (ptsto_instr pc_end (Jal Register0 jump)))
-                 (of_list final.(getXAddrs))).
+      goodReadyState pc_start initial ->
+      runsTo (mcomp_sat (run1 iset)) initial (goodReadyState pc_end).
 
-  Definition runsToGood_Invariant(prefinalL: RiscvMachineL): Prop :=
-    runsTo (mcomp_sat (run1 iset)) prefinalL (fun finalL =>
-       goodReadyState finalL /\
-       finalL.(getPc) = pc_start /\
-       valid_machine finalL /\
-       subset (footpr (ptsto_instr pc_end (Jal Register0 jump)))
-              (of_list finalL.(getXAddrs))).
+  Definition runsToGood_Invariant(m: RiscvMachineL): Prop :=
+    runsTo (mcomp_sat (run1 iset)) m (goodReadyState pc_start).
 
   (* "runs to a good state" is an invariant of the transition system
      (note that this does not depend on the definition of runN) *)
@@ -105,14 +100,17 @@ Section EventLoop.
       runsToGood_Invariant st -> mcomp_sat (run1 iset) st runsToGood_Invariant.
   Proof.
     eapply runsTo_safe1_inv; cycle 1.
-    - intros state (? & ? & ? & ?). eapply body_correct; assumption.
-    - intros state (? & ? & (R & ?) & ? & ?).
+    - intros. eapply body_correct; assumption.
+    - intros.
       (* this is the loop verification code: *)
       eapply runsToStep. {
-        eapply run_Jal0; try eassumption.
-        - rewrite H0. unfold program, array.
-          eapply rearrange_footpr_subset; [ eassumption | solve [ ecancel ] ].
-        - unfold program, array. rewrite H0. ecancel_assumption.
+        specialize (goodReadyState_implies_jump_back_instr _ H).
+        destruct goodReadyState_implies_jump_back_instr. simp.
+        specialize (goodReadyState_checks_PC _ _ H). subst pc_end.
+        eapply run_Jal0; try eauto.
+        - unfold program, array.
+          eapply rearrange_footpr_subset; [ eauto | solve [ ecancel ] ].
+        - unfold program, array. ecancel_assumption.
       }
       simpl. intros. simp.
       destruct_RiscvMachine state.
@@ -122,10 +120,18 @@ Section EventLoop.
       ssplit; try assumption; cbn;
         ring_simplify (word.add (word.sub pc_start (word.of_Z jump)) (word.of_Z jump));
         try reflexivity.
-      eapply goodReadyState_ignores in H.
-      + apply H.
-      + reflexivity.
-    - intros state C. simp. congruence.
+      specialize (goodReadyState_checks_PC _ _ H). simpl in *. subst state_pc.
+      eapply goodReadyState_preserved_by_jump_back in H.
+      + simpl in H.
+        match goal with
+        | |- ?G => let T := type of H in replace G with T; [exact H|]
+        end.
+        repeat f_equal.
+        all: solve_word_eq word_ok.
+    - intros state [C1 C2].
+      apply goodReadyState_checks_PC in C1.
+      apply goodReadyState_checks_PC in C2.
+      congruence.
   Qed.
 
 End EventLoop.
