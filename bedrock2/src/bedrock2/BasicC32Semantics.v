7410842ea306745553bc6e1b6b1851629033d6ed
jadep <jade.philipoom@gmail.com>
2020-04-20 11:40:55 -0400

    add BasicC32Semantics.parameters_ok by copying 64-bit proof

--- bedrock2/src/bedrock2/BasicC32Semantics.v
+++ bedrock2/src/bedrock2/BasicC32Semantics.v
@@ -27,9 +27,31 @@ Proof.
   assumption.
 Qed.
 
+Instance localsok: coqutil.Map.Interface.map.ok locals := SortedListString.ok _.
 Instance mapok: coqutil.Map.Interface.map.ok mem := SortedListWord.ok Naive.word32 _.
 Instance wordok: coqutil.Word.Interface.word.ok Semantics.word := Naive.word32_ok.
 Add Ring wring : (Properties.word.ring_theory (word := Semantics.word))
       (preprocess [autorewrite with rew_word_morphism],
        morphism (Properties.word.ring_morph (word := Semantics.word)),
        constants [Properties.word_cst]).
+
+Global Instance ext_spec_ok trace m0 act args :
+  Morphisms.Proper
+    (Morphisms.respectful
+       (Morphisms.pointwise_relation Interface.map.rep
+          (Morphisms.pointwise_relation (list Semantics.word) Basics.impl))
+       Basics.impl) (Semantics.ext_spec trace m0 act args).
+Proof.
+  cbn in *.
+  unfold Morphisms.Proper, Morphisms.respectful, Morphisms.pointwise_relation, Basics.impl.
+  intros.
+  assumption.
+Qed.
+
+Instance parameters_ok : parameters_ok parameters.
+  constructor; intros; try typeclasses eauto.
+  - cbv. left. reflexivity.
+  - unfold env, parameters. exact (SortedListString.ok _).
+  - constructor.
+    all: try typeclasses eauto; intros; cbn in *; contradiction.
+Qed.
