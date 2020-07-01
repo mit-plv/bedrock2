4b67ab5eec0181522c393c93565b2d1027436022
jadep <jade.philipoom@gmail.com>
2020-07-01 04:22:11 -0400

    improvements to linked lists

--- src/Examples/LinkedList/LinkedList.v
+++ src/Examples/LinkedList/LinkedList.v
@@ -6,37 +6,43 @@ Section Gallina.
   Definition ll_hd {A} : A -> linkedlist A -> A := hd.
 
   Definition ll_next {A} : linkedlist A -> linkedlist A := tl.
-
-  Definition ll_empty {A} : linkedlist A -> bool :=
-    fun l => (length l =? 0)%nat.
 End Gallina.
 
 Section Separation.
   Context {semantics : Semantics.parameters}
           {semantics_ok : Semantics.parameters_ok semantics}.
-  Context {NULL : address -> Semantics.mem -> Prop}.
-  Context {element : Type}
+  Context {element : Type} {element_size : access_size}
           {Element : address -> element -> Semantics.mem -> Prop}.
 
-  Local Notation word_size_in_bytes :=
-    (@Memory.bytes_per Semantics.width access_size.word).
-  Local Notation next_word :=
+  Local Notation element_size_in_bytes :=
+    (@Memory.bytes_per Semantics.width element_size).
+  Local Notation skip_element :=
     (fun p : address =>
-       word.add p (word.of_Z (Z.of_nat word_size_in_bytes))).
+       word.add p (word.of_Z (Z.of_nat element_size_in_bytes))).
 
-  Fixpoint LinkedList (pl : address) (l : list element)
+  (* To have a LinkedList with elements inline, instantiate Element with
+     something like [scalar] or [ptsto], and set the element_size to word or one
+     respectively.
+
+     To have a LinkedList that holds pointers to its elements, set the
+     element_size to word and give Element an indirection. For example, to do a
+     LinkedList of scalars but with pointer-indirection, do:
+
+     element := word
+     Element := (fun (p : address) (x : word) =>
+                  Lift1Prop.ex1
+                    (fun px : address =>
+                      (scalar p px * scalar px x)%sep)) *)
+  Fixpoint LinkedList (end_ptr pl : address) (l : list element)
     : Semantics.mem -> Prop :=
     match l with
-    | [] => NULL pl
+    | [] => emp (pl = end_ptr)
     | e :: l' =>
       Lift1Prop.ex1
-        (fun pe =>
-           Lift1Prop.ex1
-             (fun pl' =>
-                (scalar pl pe
-                 * scalar (next_word pl) pl'
-                 * LinkedList pl' l'
-                 * Element pe e)%sep))
+        (fun pl' =>
+           (Element pl e
+            * scalar (skip_element pl) pl'
+            * LinkedList end_ptr pl' l')%sep)
     end.
 End Separation.
 
@@ -44,7 +50,6 @@ Section Compile.
   Context {semantics : Semantics.parameters}
           {semantics_ok : Semantics.parameters_ok semantics}.
   (* TODO: generalize
-  Context {NULL : address -> Semantics.mem -> Prop}.
   Context {element : Type}
           {Element : address -> element -> Semantics.mem -> Prop}.
   Context {element_eqb : element -> element -> bool}
@@ -52,13 +57,10 @@ Section Compile.
              forall x y : element,
                BoolSpec (x = y) (x <> y) (element_eqb x y)}.
   Local Notation LinkedList :=
-    (@LinkedList semantics NULL element Element).
+    (@LinkedList semantics element Element).
    *)
-  Local Notation NULL :=
-    (fun p : address => emp (map:=Semantics.mem)
-                            (word.eqb p (word.of_Z 0) = true)).
   Local Notation LinkedList :=
-    (@LinkedList semantics NULL word scalar) (only parsing).
+    (@LinkedList semantics word access_size.word scalar) (only parsing).
   Local Notation word_size_in_bytes :=
     (@Memory.bytes_per Semantics.width access_size.word).
   Local Notation next_word :=
@@ -71,24 +73,23 @@ Section Compile.
     forall (locals: Semantics.locals) (mem: Semantics.mem)
            (locals_ok : Semantics.locals -> Prop)
       tr R' R functions T (pred: T -> _ -> Prop)
-      d ll ll_var ll_ptr k k_impl var,
+      d end_ptr ll ll_var ll_ptr k k_impl var,
       map.get locals ll_var = Some ll_ptr ->
-      (LinkedList ll_ptr ll * R')%sep mem ->
+      (LinkedList end_ptr ll_ptr ll * R')%sep mem ->
       ll <> nil ->
       let v := ll_hd d ll in
       (let head := v in
-       forall m ll' px pll',
-         ll = head :: ll' ->
-         (scalar ll_ptr px
-          * scalar px head
+       forall m pll',
+         let ll' := ll_next ll in
+         (scalar ll_ptr head
           * scalar (next_word ll_ptr) pll'
-          * LinkedList pll' ll'
+          * LinkedList end_ptr pll' ll'
           * R')%sep m ->
          find k_impl
          implementing (pred (k head))
          and-locals-post locals_ok
-         with-locals (map.put locals var px)
-         and-memory mem and-trace tr and-rest R
+         with-locals (map.put locals var head)
+         and-memory m and-trace tr and-rest R
          and-functions functions) ->
       (let head := v in
        find (cmd.seq
@@ -112,24 +113,32 @@ Section Compile.
   Lemma compile_ll_next :
     forall (locals: Semantics.locals) (mem: Semantics.mem)
            (locals_ok : Semantics.locals -> Prop)
-      tr R functions T (pred: T -> _ -> Prop)
-      (ll : linkedlist word) ll_var ll_ptr k k_impl var,
+      tr R' R functions T (pred: T -> _ -> Prop)
+      end_ptr (ll ll' : linkedlist word) ll_var ll_ptr ll'_ptr
+      dummy k k_impl var,
       map.get locals ll_var = Some ll_ptr ->
+      ll <> nil ->
+      (scalar ll_ptr (ll_hd dummy ll)
+       * scalar (next_word ll_ptr) ll'_ptr
+       * LinkedList end_ptr ll'_ptr (ll_next ll)
+       * R')%sep mem ->
       let v := ll_next ll in
       (let head := v in
        find k_impl
        implementing (pred (k head))
        and-locals-post locals_ok
-       with-locals (map.put locals var (next_word ll_ptr))
+       with-locals (map.put locals var ll'_ptr)
        and-memory mem and-trace tr and-rest R
        and-functions functions) ->
       (let head := v in
        find (cmd.seq
                (cmd.set var
-                        (expr.op bopname.add
-                                 (expr.var ll_var)
-                                 (expr.literal
-                                    (Z.of_nat word_size_in_bytes))))
+                        (expr.load
+                           access_size.word
+                           (expr.op bopname.add
+                                    (expr.var ll_var)
+                                    (expr.literal
+                                       (Z.of_nat word_size_in_bytes)))))
                k_impl)
        implementing (pred (dlet head k))
        and-locals-post locals_ok
