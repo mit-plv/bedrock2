c0f040eb2966c57bc1e650c003cfad0c35b24f1a
Samuel Gruetter <samuel.gruetter@epfl.ch>
2020-09-22 19:27:17 -0400

    wip softmul

--- compiler/src/compiler/Softmul.v
+++ compiler/src/compiler/Softmul.v
@@ -12,6 +12,7 @@ Require Import riscv.Utility.StringRecords.
 Import RecordNotations. (* Warnings are spurious, COQBUG https://github.com/coq/coq/issues/13058 *)
 Require Import coqutil.Z.Lia.
 Require Import coqutil.Map.Interface.
+Require Import coqutil.Tactics.Tactics.
 Require Import riscv.Utility.runsToNonDet.
 Require Import compiler.SeparationLogic.
 Require Import compiler.Simp.
@@ -41,6 +42,16 @@ Section Riscv.
    *)
   Definition mcomp_sat(m: M unit)(initial: State)(post: State -> Prop): Prop :=
     free.interpret run_primitive m initial (fun tt => post) (fun _ => False).
+
+  Lemma weaken_mcomp_sat: forall m initial (post1 post2: State -> Prop),
+      (forall s, post1 s -> post2 s) ->
+      mcomp_sat m initial post1 ->
+      mcomp_sat m initial post2.
+  Proof.
+    unfold mcomp_sat. intros.
+    eapply free.interpret_weaken_post with (postA1 := fun _ => False); eauto; simpl;
+      eauto using weaken_run_primitive.
+  Qed.
 
   Lemma invert_fetch: forall initial post k,
       mcomp_sat (pc <- Machine.getPC; i <- Machine.loadWord Fetch pc; k i)
@@ -87,12 +98,113 @@ Section Riscv.
     r1#"pc" = r2#"pc" /\
     r1#"nextPc" = r2#"nextPc" /\
     r1#"log" = r2#"log" /\
+    r1#"csrs" = map.empty /\
     exists mtvec_base mscratch stacktrash,
       map.get r2#"csrs" CSRField.MTVecBase = Some mtvec_base /\
       map.get r2#"csrs" CSRField.MScratch = Some mscratch /\
       List.length stacktrash = 32%nat /\
       (eq r1#"mem" * word_array (word.of_Z mscratch) stacktrash *
        program RV32I (word.of_Z (mtvec_base * 4)) handler_insts)%sep r2#"mem".
+
+  Ltac paramrecords :=
+    change (@Utility.word Words32) with word in *;
+    change (@SortedList.rep CSRFile_map_params) with (@map.rep CSRField.CSRField Z CSRFile) in *;
+    change (@width Words32) with 32 in *.
+
+  Lemma related_preserves_load_bytes: forall n sH sL a w,
+      related sH sL ->
+      load_bytes n sH#"mem" a = Some w ->
+      load_bytes n sL#"mem" a = Some w.
+  Proof.
+  Admitted.
+
+  Lemma load_preserves_related: forall n c a initialH initialL postH,
+      related initialH initialL ->
+      load n c a initialH postH ->
+      load n c a initialL
+           (fun res finalL => exists finalH, related finalH finalL /\ postH res finalH).
+  Proof.
+    unfold load.
+    cbn. intros.
+    destruct_one_match_hyp. 2: contradiction.
+    erewrite related_preserves_load_bytes; eauto.
+  Qed.
+
+  Lemma store_preserves_related: forall n c a v initialH initialL postH,
+      related initialH initialL ->
+      store n c a v initialH postH ->
+      store n c a v initialL
+            (fun finalL => exists finalH, related finalH finalL /\ postH finalH).
+  Proof.
+    unfold store.
+    cbn. intros.
+    destruct_one_match_hyp. 2: contradiction.
+    (* TODO separation logic/memory stuff *)
+  Admitted.
+
+  Lemma run_primitive_preserves_related: forall a initialH initialL postH,
+      related initialH initialL ->
+      run_primitive a initialH postH (fun _ => False) ->
+      run_primitive a initialL
+                    (fun res finalL => exists finalH, related finalH finalL /\ postH res finalH)
+                    (fun _ => False).
+  Proof.
+    intros. pose proof H as R.
+    unfold related in H|-*.
+    simp.
+    destruct a; cbn [run_primitive] in *.
+    - exists initialH.
+      intuition (congruence || eauto 10).
+      replace initialL#"regs" with initialH#"regs" by congruence.
+      assumption.
+    - eexists. ssplit; cycle -1. 1: eassumption. all: record.simp; try congruence. 2: eauto 10.
+      destruct_one_match; congruence.
+    - eapply load_preserves_related; eauto.
+    - eapply load_preserves_related; eauto.
+    - eapply load_preserves_related; eauto.
+    - eapply load_preserves_related; eauto.
+    - eapply store_preserves_related; eauto.
+    - eapply store_preserves_related; eauto.
+    - eapply store_preserves_related; eauto.
+    - eapply store_preserves_related; eauto.
+    - contradiction.
+    - contradiction.
+    - contradiction.
+    - simp. rewrite Hp4 in E. rewrite map.get_empty in E. discriminate E.
+    - simp. rewrite Hp4 in E. rewrite map.get_empty in E. discriminate E.
+    - eauto 10.
+    - simp. eauto 10.
+    - exists initialH.
+      intuition (congruence || eauto 10).
+    - eexists. ssplit; cycle -1. 1: eassumption. all: record.simp; try congruence. eauto 10.
+    - eexists. unfold updatePc in *. ssplit; cycle -1. 1: eassumption.
+      all: record.simp; try congruence. eauto 10.
+    - contradiction.
+  Qed.
+
+  (* If we're running the same primitives on two related states, they remain related.
+     (Note: decoding using RV32I vs RV32IM does not result in the same primitives). *)
+  Lemma mcomp_sat_preserves_related: forall m initialL initialH postH,
+      related initialH initialL ->
+      mcomp_sat m initialH postH ->
+      mcomp_sat m initialL (fun finalL => exists finalH, related finalH finalL /\ postH finalH).
+  Proof.
+    induction m; intros. 2: {
+      eapply weaken_mcomp_sat. 2: eassumption. eauto.
+    }
+    unfold mcomp_sat in *.
+    cbn in *.
+    eapply weaken_run_primitive with (postA1 := fun _ => False). 3: {
+      eapply run_primitive_preserves_related; eassumption.
+    }
+    2: auto.
+    cbn.
+    intros. simp.
+    eapply H. 1: eassumption.
+    eassumption.
+  Qed.
+
+  Axiom TODO: False.
 
   Lemma softmul_correct: forall initialH initialL post,
       runsTo (mcomp_sat (run1 RV32IM)) initialH post ->
@@ -104,6 +216,7 @@ Section Riscv.
       apply runsToDone. eauto.
     }
     unfold run1 in H |- *.
+    pose proof H2 as R.
     unfold related in H2.
     eapply invert_fetch in H. simp.
     pose proof (decode_verify_iset RV32IM (LittleEndian.combine 4 w)) as V.
@@ -114,24 +227,22 @@ Section Riscv.
     - (* IInstruction *)
       eapply runsToStep with (midset0 := fun midL => exists midH, related midH midL /\ midset midH).
       + eapply build_fetch with (w := w). {
-          admit. (* separation logic *)
+          case TODO. (* separation logic *)
         }
         apply decode_I_to_IM in E. rewrite E.
-        (* lower Hr *)
-        admit.
+        eapply mcomp_sat_preserves_related; eassumption.
       + intros midL. intros. simp. eapply H1; eassumption.
     - (* MInstruction *)
-      admit.
+      case TODO.
     - (* CSRInstruction *)
       eapply runsToStep with (midset0 := fun midL => exists midH, related midH midL /\ midset midH).
       + eapply build_fetch with (w := w). {
-          admit. (* separation logic *)
+          case TODO. (* separation logic *)
         }
         apply decode_CSR_to_IM in E. rewrite E.
         (* lower Hr *)
-        admit.
+        eapply mcomp_sat_preserves_related; eassumption.
       + intros midL. intros. simp. eapply H1; eassumption.
-
-  Abort.
+  Qed.
 
 End Riscv.
