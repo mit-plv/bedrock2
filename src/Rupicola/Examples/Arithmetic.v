3642c8206da9be77f0ec48d7f5c8a89de722bc2a
Clément Pit-Claudel <clement.pitclaudel@live.com>
2021-02-17 04:48:45 -0500

    examples/arithmetic: Add Murmur 3 body

--- src/Rupicola/Examples/Arithmetic.v
+++ src/Rupicola/Examples/Arithmetic.v
@@ -52,6 +52,36 @@ End FNV1A64_params.
 
 Module FNV1A64 := FNV1A FNV1A64_params.
 
+Module Murmur3.
+  Import BasicC32Semantics.
+
+  Definition scramble (k : word) :=
+    let/n m1 := word.of_Z 513432918353 in
+    let/n k := word.mul k m1 in
+    let/n ln := word.of_Z 15 in
+    let/n l := word.slu k ln in
+    let/n lr := word.of_Z 17 in
+    let/n r := word.sru k lr in
+    let/n k := word.or l r in
+    let/n m2 := word.of_Z 461845907 in
+    let/n k := word.mul k m2 in
+    k.
+
+  Implicit Type R : Semantics.mem -> Prop.
+  Instance spec_of_scramble : spec_of "scramble" :=
+    fnspec "scramble" (k: word) ~> k',
+    { requires tr mem := True;
+      ensures tr' mem' := tr = tr' /\ mem = mem' /\ k' = scramble k }.
+
+  Derive scramble_body SuchThat
+         (defn! "scramble"("k") ~> "k" { scramble_body },
+          implements scramble)
+         As scramble_body_correct.
+  Proof.
+    compile.
+  Qed.
+End Murmur3.
+
 Require Import bedrock2.NotationsCustomEntry.
 Require Import bedrock2.NotationsInConstr.
 Eval cbv in FNV1A32.fnv1a_update_body.
