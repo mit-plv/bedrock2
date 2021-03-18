bd125eb1ba1d3e4767bbbeae69ac572f804e4fed
Samuel Gruetter <samuel.gruetter@epfl.ch>
2021-03-18 15:09:13 -0400

    move safe_simpl to coqutil, but
    current implementation reverts and re-intros, which is not good

--- compiler/src/compiler/FlatToRiscvCommon.v
+++ compiler/src/compiler/FlatToRiscvCommon.v
@@ -838,7 +838,16 @@ Section FlatToRiscv1.
                pose proof word.unsigned_range (word.sub k addr). blia.
              }
              apply (f_equal word.of_Z) in F.
-             safe_simpl. (* PARAMRECORDS *)
+
+Ltac safe_simpl ::=
+  match goal with
+  | |- ?G => let G' := safe_simpl_term G in change G'
+  end;
+  repeat match goal with
+         | H: ?T |- _ => let T' := safe_simpl_term T in progress change T' in H
+         end.
+
+             safe_simpl. (* simpl in *. *) (* PARAMRECORDS *)
              rewrite (word.of_Z_unsigned (word.sub k addr)) in F.
              rewrite <- add_0_r at 1. change (Z.of_nat 0) with 0 in F. rewrite <- F.
              ring.
@@ -848,7 +857,7 @@ Section FlatToRiscv1.
              apply (f_equal Z.of_nat) in F.
              rewrite Z2Nat.id in F by blia.
              apply (f_equal word.of_Z) in F.
-             safe_simpl.
+             safe_simpl. (* PARAMRECORDS *)
              rewrite (word.of_Z_unsigned (word.sub k addr)) in F.
              ring_simplify (word.sub k (word.add addr (word.of_Z 1))).
              rewrite F.
