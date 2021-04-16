3fb9c93c9a52fef067d86632c36090868cf2c662
Andres Erbsen <andreser@mit.edu>
2021-04-16 13:48:00 -0400

    use coqutil.Tactics.eplace in bedrock2.Array

--- bedrock2/src/bedrock2/Array.v
+++ bedrock2/src/bedrock2/Array.v
@@ -3,6 +3,7 @@ Require Import Coq.Lists.List Coq.ZArith.BinInt. Local Open Scope Z_scope.
 Require Import coqutil.Word.Interface coqutil.Word.Properties.
 Require Import coqutil.Z.Lia.
 Require Import coqutil.Byte.
+Require Import coqutil.Tactics.eplace.
 
 Section Array.
   Context {width : Z} {word : Word.Interface.word width} {word_ok : word.ok word}.
@@ -120,7 +121,7 @@ Section Array.
       repeat (rewrite ?word.unsigned_sub, ?Zdiv.Zminus_mod_idemp_r, ?Zdiv.Zminus_mod_idemp_l, ?Zdiv.Zplus_mod_idemp_r, ?Zdiv.Zplus_mod_idemp_l || unfold word.wrap).
       replace (word.unsigned start + (word.unsigned a - word.unsigned start)) with (word.unsigned a) by blia.
       rewrite Z.mod_small by assumption; trivial. }
-    replace (word.mul (word.of_Z (Z.of_nat n)) size) with (word.of_Z (width:=width) (word.unsigned size * Z.of_nat n)); cycle 1.
+    eplace (word.mul (word.of_Z (Z.of_nat n)) size) with (word.of_Z (word.unsigned size * Z.of_nat n)).
     { eapply word.unsigned_inj.
       repeat (rewrite ?word.unsigned_of_Z, ?word.unsigned_mul, ?Zdiv.Zmult_mod_idemp_r, ?Zdiv.Zmult_mod_idemp_l || unfold word.wrap).
       f_equal. blia. }
