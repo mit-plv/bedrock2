09d441c38020b400f8fc602b1de19413de882ba3
Samuel Gruetter <saemi472@gmail.com>
2021-04-16 00:11:03 -0400

    move records tactics and wip bytesToEthernetARPPacket

--- bedrock2/src/bedrock2Examples/ARPResponder_live.v
+++ bedrock2/src/bedrock2Examples/ARPResponder_live.v
@@ -17,6 +17,7 @@ Require Import bedrock2.ptsto_bytes bedrock2.Scalars.
 Require Import bedrock2.WeakestPrecondition bedrock2.ProgramLogic.
 Require Import bedrock2.ZnWords.
 Require Import coqutil.Word.SimplWordExpr.
+Require Import coqutil.Tactics.Records.
 Require Import bedrock2.footpr.
 
 Local Set Nested Proofs Allowed.
@@ -123,161 +124,21 @@ Module List.
     Qed.
 
     Definition generate(len: nat)(f: nat -> A): list A := List.map f (List.seq 0 len).
+
+    Lemma map_with_shifted_start_index{B: Type}: forall (f1 f2: A -> nat -> B) vs start1 start2,
+        (forall a i, f1 a i = f2 a (i + start2 - start1)) ->
+        map_with_start_index f1 start1 vs = map_with_start_index f2 start2 vs.
+    Proof.
+      induction vs; intros.
+      - reflexivity.
+      - simpl. f_equal.
+        + rewrite H. f_equal. blia.
+        + eapply IHvs. intros. rewrite H.
+          f_equal. blia.
+    Qed.
+
   End WithA.
 End List.
-
-Require Import Ltac2.Ltac2.
-Set Default Proof Mode "Classic".
-
-(* TODO 8.14 replace by Constr.is_evar *)
-Ltac2 is_evar(c: constr) :=
-  match Constr.Unsafe.kind c with
-  | Constr.Unsafe.Evar _ _ => true
-  | _ => false
-  end.
-
-(* TODO 8.14 replace by Constr.is_ind *)
-Ltac2 is_ind(c: constr) :=
-  match Constr.Unsafe.kind c with
-  | Constr.Unsafe.Ind _ _ => true
-  | _ => false
-  end.
-
-(* TODO 8.14 replace by Constr.is_constructor *)
-Ltac2 is_constructor(c: constr) :=
-  match Constr.Unsafe.kind c with
-  | Constr.Unsafe.Constructor _ _ => true
-  | _ => false
-  end.
-
-Ltac2 sfail(s: string) := Control.zero (Tactic_failure (Some (Message.of_string s))).
-Ltac2 ufail () := Control.zero (Tactic_failure None).
-
-Ltac2 head(c: constr) :=
-  match Constr.Unsafe.kind c with
-  | Constr.Unsafe.App h _ => h
-  | _ => c
-  end.
-
-Ltac2 splitApp(c: constr) :=
-  match Constr.Unsafe.kind c with
-  | Constr.Unsafe.App f args => (f, args)
-  | _ => (c, Array.empty ())
-  end.
-
-Ltac2 mkApp(c: constr)(args: constr array) :=
-  Constr.Unsafe.make (Constr.Unsafe.App c args).
-
-Ltac2 rec strip_foralls(t: constr) :=
-  match Constr.Unsafe.kind t with
-  | Constr.Unsafe.Prod _ u => strip_foralls u
-  | _ => t
-  end.
-
-Ltac2 rec strip_lambdas(t: constr) :=
-  match Constr.Unsafe.kind t with
-  | Constr.Unsafe.Lambda _ u => strip_lambdas u
-  | _ => t
-  end.
-
-Ltac2 rec count_lambdas(t: constr) :=
-  match Constr.Unsafe.kind t with
-  | Constr.Unsafe.Lambda _ u => Int.add 1 (count_lambdas u)
-  | _ => 0
-  end.
-
-Ltac2 unfold_if_getter(g: constr) :=
-  let (h, args) := splitApp g in
-  match Constr.Unsafe.kind h with
-  | Constr.Unsafe.Constant r _ =>
-    let getter_lambda := Std.eval_unfold [(Std.ConstRef r, Std.AllOccurrences)] h in
-    match Constr.Unsafe.kind (strip_lambdas getter_lambda) with
-    | Constr.Unsafe.Proj _ v =>
-      match Constr.Unsafe.kind v with
-      | Constr.Unsafe.Rel i => if Int.equal i 1 then () else sfail "not a getter because not proj from Rel 1"
-      | _ => sfail "not a getter because not proj from Rel"
-      end
-    | Constr.Unsafe.Case _ _ _ _ branches =>
-      if Int.equal (Array.length branches) 1 then
-        let b := Array.get branches 0 in
-        match Constr.Unsafe.kind (strip_lambdas b) with
-        | Constr.Unsafe.Rel i => (* note: de Brujin indices are 1-based *)
-          if Int.le i (count_lambdas b) then ()
-          else sfail "not a getter because branch returns a variable bound further out than in branch pattern"
-        | _ => sfail "not a getter because branch does not return a variable"
-        end
-      else sfail "not a getter because not exactly 1 branch in match"
-    | _ => sfail "not a getter because unfolding did not yield a primitive projection or match"
-    end;
-    mkApp getter_lambda args
-  | _ => sfail "not a getter because head is not a constant"
-  end.
-
-Ltac is_getter :=
-  ltac2:(g |- let _ := unfold_if_getter (Option.get (Ltac1.to_constr g)) in ()).
-
-Definition TestIsAGetter(p: nat * nat) :=
-  match p with
-  | pair a b => a
-  end.
-Definition TestIsntAGetter(p: nat * nat) :=
-  match p with
-  | pair a b => a + 1
-  end.
-Goal False.
-  assert_succeeds (is_getter TestIsAGetter).
-  assert_fails (is_getter TestIsntAGetter).
-Abort.
-
-(* ltac1 needed because of COQBUG https://github.com/coq/coq/issues/11641 *)
-Ltac2 change_x_with_y(x : constr)(y : constr) :=
-  ltac1:(a b |- change a with b) (Ltac1.of_constr x) (Ltac1.of_constr y).
-
-Ltac2 simpl_getter_applied_to_constructor(a: constr) :=
-  lazy_match! a with
-  | ?g ?v =>
-    if is_constructor (head v) then
-      if is_ind (head (Constr.type v)) then
-        let ug := unfold_if_getter g in
-        let r := eval cbv beta iota in ($ug $v) in
-        (* Note: if `change` has no effect, it might be due to COQBUG https://github.com/coq/coq/issues/14084 *)
-        change_x_with_y a r
-      else
-        sfail "constructor is not fully applied"
-    else
-      sfail "not a construction"
-  end.
-
-Ltac simpl_getter_applied_to_constructor :=
-  ltac2:(a0 |- Control.enter (fun () => simpl_getter_applied_to_constructor (Option.get (Ltac1.to_constr a0)))).
-
-Ltac simpl_getters_applied_to_constructors :=
-  repeat match goal with
-         | |- context[?a] => simpl_getter_applied_to_constructor a
-         end.
-
-Record TestRecord{A: Type}{n: nat} := {
-  OtherTestRecordField: unit;
-  TestRecordField: tuple nat n;
-}.
-Arguments TestRecord: clear implicits.
-
-Goal forall (x y: nat),
-    PrimitivePair.pair._1 (PrimitivePair.pair.mk (B := fun _ => nat) x y) = x /\
-    fst (x, y) = x /\
-    TestRecordField (Build_TestRecord unit 0 tt tt) = tt.
-Proof.
-  intros. simpl_getters_applied_to_constructors.
-  match goal with
-  | |- ?G => constr_eq G constr:(x = x /\ x = x /\ (@eq (tuple nat O) tt tt))
-  end.
-Abort.
-
-Ltac instantiate_evar_with_econstructor e :=
-  let T := type of e in
-  let y := fresh in
-  unshelve refine (let y: T := _ in _);
-  [econstructor; shelve| unify e y; subst y].
 
 Definition TODO{T: Type}: T. Admitted.
 
@@ -345,6 +206,28 @@ Section WithParameters.
     erewrite load_bounded_Z_of_sep by eassumption.
     reflexivity.
   Qed.
+
+  Lemma bytearray_index_split_iff1: forall a (l: list byte) n,
+      word.unsigned n <= Z.of_nat (List.length l) ->
+      iff1 (array ptsto /[1] a l)
+           (sep (array ptsto /[1] a (List.firstn (Z.to_nat (word.unsigned n)) l))
+                (array ptsto /[1] (a ^+ n) (List.skipn (Z.to_nat (word.unsigned n)) l))).
+  Proof.
+    intros.
+    etransitivity.
+    2: {
+      symmetry. eapply bytearray_index_merge. ZnWords.
+    }
+    rewrite List.firstn_skipn.
+    reflexivity.
+  Qed.
+
+  Lemma bytearray_index_split: forall a (l: list byte) n,
+      word.unsigned n <= Z.of_nat (List.length l) ->
+      array ptsto /[1] a l =
+      sep (array ptsto /[1] a (List.firstn (Z.to_nat (word.unsigned n)) l))
+          (array ptsto /[1] (a ^+ n) (List.skipn (Z.to_nat (word.unsigned n)) l)).
+  Proof. intros. eapply iff1ToEq. eapply bytearray_index_split_iff1. assumption. Qed.
 
   Definition BEBytesToWord{n: nat}(bs: tuple byte n): word := word.of_Z (BigEndian.combine n bs).
 
@@ -423,15 +306,17 @@ Section WithParameters.
 
     Definition arrayAt{E: Type}(elemSize: Z)(elem: TypeSpec E)(start: word): list E -> list (mem -> Prop) :=
       List.map_with_index (fun e i => dataAt elem (start ^+ /[i * elemSize]) e).
+
+    Definition dataAt_body{R: Type}(sp: TypeSpec R): word -> R -> mem -> Prop :=
+      match sp with
+      | TByte => ptsto
+      | @TStruct R fields => fun base r => seps (fieldsAt fields base r)
+      | @TArray E elemSize elem => fun base l => seps (arrayAt elemSize elem base l)
+      | @TTuple E n elemSize elem => fun base t => seps (arrayAt elemSize elem base (tuple.to_list t))
+      end.
   End dataAt_recursion_helpers.
 
-  Fixpoint dataAt{R: Type}(sp: TypeSpec R){struct sp}: word -> R -> mem -> Prop :=
-    match sp with
-    | TByte => ptsto
-    | @TStruct R fields => fun base r => seps (fieldsAt (@dataAt) fields base r)
-    | @TArray E elemSize elem => fun base l => seps (arrayAt (@dataAt) elemSize elem base l)
-    | @TTuple E n elemSize elem => fun base t => seps (arrayAt (@dataAt) elemSize elem base (tuple.to_list t))
-    end.
+  Fixpoint dataAt{R: Type}(sp: TypeSpec R){struct sp}: word -> R -> mem -> Prop := dataAt_body (@dataAt) sp.
 
   (* ** Packet Formats *)
 
@@ -524,6 +409,49 @@ Section WithParameters.
        |}
     |}.
 
+  Fixpoint firstn_as_tuple{A: Type}(default: A)(n: nat)(l: list A): tuple A n :=
+    match n as n return tuple A n with
+    | O => tt
+    | S m => PrimitivePair.pair.mk (List.hd default l) (firstn_as_tuple default m (List.tl l))
+    end.
+
+  Lemma to_list_firstn_as_tuple: forall A (default: A) n (l: list A),
+      (n <= List.length l)%nat ->
+      tuple.to_list (firstn_as_tuple default n l) = List.firstn n l.
+  Proof.
+    induction n; intros.
+    - reflexivity.
+    - destruct l. 1: cbv in H; exfalso; blia.
+      simpl in *.
+      f_equal. eapply IHn. blia.
+  Qed.
+
+  Ltac unfold1dataAt :=
+    lazymatch goal with
+    | |- context[@dataAt ?R ?sp] => change (@dataAt R sp) with (dataAt_body (@dataAt) sp)
+    end;
+    cbv beta iota delta [dataAt_body].
+
+  Lemma array_arrayAt: forall {R: Type} sz sp (vs: list R) addr,
+      seps (arrayAt (@dataAt) sz sp addr vs) = array (dataAt sp) /[sz] addr vs.
+  Proof.
+    induction vs; intros.
+    - simpl. reflexivity.
+    - etransitivity. {
+        eapply iff1ToEq. symmetry. eapply seps'_iff1_seps.
+      }
+      simpl.
+      simpl_word_exprs word_ok.
+      eapply iff1ToEq.
+      cancel.
+      cbv [seps].
+      rewrite seps'_iff1_seps.
+      rewrite <- IHvs.
+      unfold arrayAt, List.map_with_index.
+      erewrite (List.map_with_shifted_start_index _ _ _ 0). 1: reflexivity.
+      intros. simpl. f_equal. ZnWords.
+  Qed.
+
   Lemma bytesToARPPacket: forall a bs,
       28 = len bs ->
       exists p: ARPPacket,
@@ -540,7 +468,75 @@ Section WithParameters.
         iff1 (array ptsto /[1] a bs) (dataAt (EthernetPacket_spec ARPPacket_spec) a p).
   Proof.
     intros.
-    eexists {| payload := _ |}. cbn -[tuple.to_list].
+    eexists {| payload := _ |}.
+    unfold1dataAt.
+    unfold EthernetPacket_spec.
+    unfold fieldsAt, List.map_with_index, List.map_with_start_index.
+    unfold fieldAt.
+    simpl_getters_applied_to_constructors.
+    unfold offset, List.fold_right, List.firstn.
+    unfold FieldSpec_size.
+    unfold seps.
+    simpl_word_exprs word_ok.
+    cancel.
+
+    ParamRecords.simpl_param_projections.
+
+    lazymatch goal with
+    | |- iff1 (seps [array ptsto /[1] ?addr ?vs]) (seps (dataAt ?sp ?addr ?v :: _)) =>
+      let s := eval cbv in (TypeSpec_size sp v) in idtac s;
+      rewrite (bytearray_index_split addr vs /[s]) by ZnWords
+    end.
+    cbn [seps]. cancel.
+    cancel_seps_at_indices O O. {
+      unfold1dataAt.
+      rewrite array_arrayAt.
+      f_equal.
+      replace (Z.to_nat \[/[6]]) with 6%nat by ZnWords.
+      symmetry.
+      apply to_list_firstn_as_tuple.
+      ZnWords.
+    }
+
+    lazymatch goal with
+    | |- iff1 (seps [array ptsto /[1] ?addr ?vs]) (seps (dataAt ?sp ?addr ?v :: _)) =>
+      let s := eval cbv in (TypeSpec_size sp v) in idtac s;
+      rewrite (bytearray_index_split addr vs /[s]) by ZnWords
+    end.
+    cbn [seps]. cancel.
+    cancel_seps_at_indices O O. {
+      unfold1dataAt.
+      rewrite array_arrayAt.
+      f_equal.
+      replace (Z.to_nat \[/[6]]) with 6%nat by ZnWords.
+      symmetry.
+      apply to_list_firstn_as_tuple.
+      ZnWords.
+    }
+
+    replace (a ^+ /[6] ^+ /[6]) with (a ^+ /[12]) by ZnWords.
+
+    lazymatch goal with
+    | |- iff1 (seps [array ptsto /[1] ?addr ?vs]) (seps (dataAt ?sp ?addr ?v :: _)) =>
+      let s := eval cbv in (TypeSpec_size sp v) in idtac s;
+      rewrite (bytearray_index_split addr vs /[s]) by ZnWords
+    end.
+    cbn [seps]. cancel.
+    cancel_seps_at_indices O O. {
+      unfold1dataAt.
+      rewrite array_arrayAt.
+      f_equal.
+      replace (Z.to_nat \[/[2]]) with 2%nat by ZnWords.
+      symmetry.
+      apply to_list_firstn_as_tuple.
+      ZnWords.
+    }
+
+    rewrite ?List.skipn_skipn.
+    cbn [seps].
+
+    (* existential is annoying, maybe define a function filling a TypeSpec starting from a list of
+       bytes? *)
 
     (* TODO messy *)
   Admitted.
@@ -648,6 +644,10 @@ Section WithParameters.
 
   Lemma record_simplification: forall (P Q: Prop), P = Q -> P -> Q. Proof. intros. subst P. assumption. Qed.
   Arguments record_simplification: clear implicits.
+
+Require Import Ltac2.Ltac2.
+Require Ltac2.Option.
+Set Default Proof Mode "Classic".
 
 Ltac2 Type exn ::= [ Succeeds ].
 
