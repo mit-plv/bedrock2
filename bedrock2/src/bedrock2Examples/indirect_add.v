c167c2f66f3d519417622cf110973d7836c52229
Andres Erbsen <andreser@mit.edu>
2021-04-29 17:09:58 -0400

    indirect_add.v: add aliasing challenge

--- bedrock2/src/bedrock2Examples/indirect_add.v
+++ bedrock2/src/bedrock2Examples/indirect_add.v
@@ -58,12 +58,14 @@ Section WithParameters.
   (* Print Assumptions link_swap_swap_swap_swap. *)
   (* SortedList.* SortedListString.* *)
 
+  (*
   From bedrock2 Require Import ToCString Bytedump.
   Local Open Scope bytedump_scope.
   Goal True.
     let c_code := eval cbv in (byte_list_of_string (c_module (indirect_add_twice::indirect_add::nil))) in
     idtac c_code.
   Abort.
+  *)
 
   Definition indirect_add_three : Syntax.func := let a := "a" in let b := "b" in let c := "c" in
     ("indirect_add_three", ([a;b;c], [], bedrock_func_body:(
@@ -88,4 +90,126 @@ Section WithParameters.
     repeat straightline.
     { split; trivial. split; trivial. cbv [g]. ecancel_assumption. }
   Qed.
+
+  Definition indirect_add_three' : Syntax.func := let a := "a" in let b := "b" in let c := "c" in let out := "out" in let v := "v" in
+    ("indirect_add_three'", ([out;a;b;c], [], bedrock_func_body:(
+    stackalloc 4 as v {
+      indirect_add(v, a, b);
+      indirect_add(out, v, c)
+    }
+  ))).
+
+  Instance spec_of_indirect_add_three' : spec_of "indirect_add_three'" :=
+    fnspec! "indirect_add_three'" out a b c / vout va vb vc Ra Rb Rc R,
+    { requires t m :=
+        m =* scalar out vout * R /\
+        m =* scalar a va * Ra /\
+        m =* scalar b vb * Rb /\
+        m =* scalar c vc * Rc;
+      ensures t' m' := t=t' /\ m' =* scalar a (g va vb vc) * R }.
+
+  Lemma indirect_add_three'_ok : program_logic_goal_for_function! indirect_add_three'.
+  Proof.
+    repeat straightline.
+    (* note: we want to introduce only one variable for stack contents 
+     * and use it in a all separation-logic facts in the symbolic state *)
+
+    repeat match goal with
+           | H : _ |- _ =>
+               (* WHY do I need @ in front of the lemma? PARAMRECORDS? *)
+               seprewrite_in_by @scalar_of_bytes H
+                 ltac:(change width with 32; Lia.lia);
+                 let x := fresh "x" in
+                 set (word.of_Z _) as x in H; clearbody x; move x at top
+           end.
+    clear dependent mStack.
+
+    (*
+H1 : (scalar a0 x ⋆ (scalar out vout ⋆ R))%sep m2
+H2 : (scalar a0 x0 ⋆ (scalar a va ⋆ Ra))%sep m2
+H3 : (scalar a0 x1 ⋆ (scalar b vb ⋆ Rb))%sep m2
+H4 : (scalar a0 x2 ⋆ (scalar c vc ⋆ Rc))%sep m2
+     *)
+
+    straightline_call.
+    { split; [exact H1|split]; ecancel_assumption. }
+
+    repeat straightline.
+    (*
+H15 : (scalar a0 (word.add va vb) ⋆ (scalar out vout ⋆ R))%sep a2
+     *)
+    (* H15 is an updated version of H1,
+       but we really wanted to carry over H2,H3, and H4 as well *)
+  Abort.
+
+  (* let's see how this would look like with an alternate spec of [indirect_add] *)
+
+  Remove Hints spec_of_indirect_add : typeclass_instances.
+  Instance spec_of_indirect_add_gen : spec_of "indirect_add" :=
+    fnspec! "indirect_add" a b c / va Ra vb Rb vc Rc,
+    { requires t m := m =* scalar a va * Ra /\ m =* scalar b vb * Rb /\ m =* scalar c vc * Rc;
+      ensures t' m' := t=t' /\
+        forall va Ra, m =* scalar a va * Ra -> m' =* scalar a (word.add vb vc) * Ra }.
+
+  Lemma indirect_add_gen_ok : program_logic_goal_for_function! indirect_add.
+  Proof.
+    repeat straightline; []; split; [>|split]; trivial; [].
+    (* This goal is unprovable as shown.
+       It could be made provable by revealing how the memory was updated
+         by modifying the straightline rule for store.
+       I don't know how I would do this systematically for non-leaf functions, though. *)
+ Abort.
+
+  (* an potential alternative to changing this spec would be to
+     - prove  wp.call (post:=p) /\ wp.call(post=q) -> wp.call (post:=p/\q)
+     - instantiate the spec of indirect_add multiple times at the call site
+       (existentials in postcondition are duplicated for each instantiation)
+     but for now, let's prototype with changed spec:
+  *)
+
+  Lemma indirect_add_three'_ok : program_logic_goal_for_function! indirect_add_three'.
+  Proof.
+    repeat straightline.
+    (* note: we want to introduce only one variable for stack contents 
+     * and use it in a all separation-logic facts in the symbolic state.
+     * here we get away with doing it wrong anyway. *)
+
+    repeat match goal with
+           | H : _ |- _ =>
+               (* WHY do I need @ in front of the lemma? PARAMRECORDS? *)
+               seprewrite_in_by @scalar_of_bytes H
+                 ltac:(change width with 32; Lia.lia);
+                 let x := fresh "x" in
+                 set (word.of_Z _) as x in H; clearbody x; move x at top
+           end.
+    clear dependent mStack.
+
+    straightline_call.
+    { split; [exact H1|split]; ecancel_assumption. }
+    repeat straightline.
+    rename a2 into m.
+    (*
+H15 : forall (va0 : word) (Ra : mem -> Prop),
+      (scalar a0 va0 ⋆ Ra)%sep m2 -> (scalar a0 (word.add va vb) ⋆ Ra)%sep m
+     *)
+    eapply H15 in H1.
+    eapply H15 in H2.
+    eapply H15 in H3.
+    eapply H15 in H4.
+    clear H15.
+
+    straightline_call.
+    { split; [>|split]; try ecancel_assumption. }
+    repeat straightline.
+    rename a3 into m'.
+    (*
+H15 : forall (va0 : word) (Ra : mem -> Prop),
+      (scalar out va0 ⋆ Ra)%sep m ->
+      (scalar out (word.add (word.add va vb) vc) ⋆ Ra)%sep m'
+     *)
+    specialize (H15 _ _ ltac:(ecancel_assumption)).
+
+    (* unrelated: stack deallocation proof, would need scalar-to-bytes lemma *)
+  Abort.
+
 End WithParameters.
