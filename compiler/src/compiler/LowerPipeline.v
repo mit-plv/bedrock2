9f22dfb31a17f28fcc26e615487d93daa0181ad0
Samuel Gruetter <saemi472@gmail.com>
2021-05-15 12:23:50 -0400

    remove unnecessary parameters and hyps from LowerPipeline

--- compiler/src/compiler/LowerPipeline.v
+++ compiler/src/compiler/LowerPipeline.v
@@ -1,58 +1,31 @@
-Require Export Coq.Lists.List.
-Require Import Coq.ZArith.ZArith.
-Export ListNotations.
-Require Export coqutil.Decidable.
-Require Import coqutil.Tactics.ParamRecords.
-Require        compiler.ExprImp.
-Require Export compiler.FlattenExprDef.
-Require Export compiler.FlattenExpr.
-Require        compiler.FlatImp.
-Require Export riscv.Spec.Machine.
-Require Export riscv.Platform.Run.
-Require Export riscv.Platform.RiscvMachine.
-Require Export riscv.Platform.MetricLogging.
-Require Export riscv.Utility.Monads.
-Require Import riscv.Utility.runsToNonDet.
-Require Export riscv.Platform.MetricRiscvMachine.
-Require Import coqutil.Z.Lia.
-Require Import compiler.NameGen.
-Require Import compiler.StringNameGen.
-Require Export compiler.util.Common.
-Require Export coqutil.Decidable.
-Require Export riscv.Utility.Encode.
-Require Import riscv.Spec.Decode.
-Require Export riscv.Spec.Primitives.
-Require Export riscv.Spec.MetricPrimitives.
-Require Import compiler.GoFlatToRiscv.
-Require Import riscv.Utility.MkMachineWidth.
-Require Export riscv.Proofs.DecodeEncode.
-Require Export riscv.Proofs.EncodeBound.
-Require Import riscv.Utility.Utility.
-Require Export riscv.Platform.Memory.
-Require Export riscv.Utility.InstructionCoercions.
-Require Import compiler.SeparationLogic.
-Require Import coqutil.Tactics.Simp.
-Require Import compiler.FlattenExprSimulation.
-Require Import compiler.RegRename.
-Require Import compiler.FlatToRiscvSimulation.
+Require Import Coq.Logic.FunctionalExtensionality.
+Require Import coqutil.Map.Interface.
+Require Import coqutil.Tactics.Tactics.
 Require Import compiler.Simulation.
-Require Import compiler.RiscvEventLoop.
-Require Coq.Init.Byte.
-Require Import bedrock2.MetricLogging.
+Require Import coqutil.Tactics.Simp.
+Require Import riscv.Spec.Decode.
+Require Import riscv.Spec.Primitives.
+Require Import riscv.Platform.RiscvMachine.
+Require Import riscv.Platform.MetricRiscvMachine.
+Require Import riscv.Platform.MetricLogging.
+Require Import riscv.Utility.runsToNonDet.
+Require Import compiler.util.Common.
+Require Import compiler.SeparationLogic.
+Require Export coqutil.Word.SimplWordExpr.
+Require Import compiler.GoFlatToRiscv.
+Require Import compiler.DivisibleBy4.
+Require Import compiler.MetricsToRiscv.
+Require Import compiler.FlatImp.
+Require Import compiler.RunInstruction.
+Require Import compiler.FlatToRiscvDef.
 Require Import compiler.FlatToRiscvCommon.
 Require Import compiler.FlatToRiscvFunctions.
-Require Import compiler.DivisibleBy4.
-Require Export coqutil.Word.SimplWordExpr.
-Require Import compiler.ForeverSafe.
-Require Export compiler.MemoryLayout.
-Require Import FunctionalExtensionality.
-Require Import coqutil.Tactics.autoforward.
+Require Import compiler.MemoryLayout.
+Require Import bedrock2.MetricLogging.
 Require Import compiler.FitsStack.
-Import Utility.
+Require Import riscv.Utility.InstructionCoercions.
+Require Import riscv.Utility.Utility.
 
-Existing Instance riscv.Spec.Machine.DefaultRiscvState.
-
-Open Scope Z_scope.
 
 Section WithWordAndMem.
   Context {width: Z} {word: word.word width} {mem: map.map word byte}.
@@ -66,100 +39,30 @@ Section WithWordAndMem.
       (ptsto_bytes start anybytes))%sep.
 End WithWordAndMem.
 
-Module Import Pipeline.
+Section LowerPipeline.
+  Context {p: FlatToRiscvCommon.parameters}.
 
-  Class parameters := {
-    W :> Words;
-    mem :> map.map word byte;
-    Registers :> map.map Z word;
-    string_keyed_map :> forall T: Type, map.map string T; (* abstract T for better reusability *)
-    trace := list (mem * string * list word * (mem * list word));
-    ExtSpec := trace -> mem -> string -> list word -> (mem -> list word -> Prop) -> Prop;
-    ext_spec : ExtSpec;
-    compile_ext_call : string_keyed_map Z -> Z -> Z -> FlatImp.stmt Z -> list Instruction;
-    M: Type -> Type;
-    MM :> Monad M;
-    RVM :> RiscvProgram M word;
-    PRParams :> PrimitivesParams M MetricRiscvMachine;
-  }.
+  Local Open Scope ilist_scope.
 
-  Instance FlatToRiscvDef_parameters{p: parameters}: FlatToRiscvDef.FlatToRiscvDef.parameters := {|
-    iset := if Utility.width =? 32 then RV32I else RV64I;
-    FlatToRiscvDef.FlatToRiscvDef.compile_ext_call := compile_ext_call;
-  |}.
-
-  Instance FlattenExpr_parameters{p: parameters}: FlattenExpr.parameters := {
-    FlattenExpr.W := _;
-    FlattenExpr.locals := _;
-    FlattenExpr.mem := mem;
-    FlattenExpr.ext_spec := ext_spec;
-    FlattenExpr.NGstate := N;
-  }.
-
-  Instance FlatToRiscv_params{p: parameters}: FlatToRiscvCommon.parameters := {|
-    FlatToRiscvCommon.ext_spec := ext_spec;
-  |}.
-
-  Class assumptions{p: parameters}: Prop := {
-    word_riscv_ok :> RiscvWordProperties.word.riscv_ok word;
-    string_keyed_map_ok :> forall T, map.ok (string_keyed_map T);
-    Registers_ok :> map.ok Registers;
-    PR :> MetricPrimitives PRParams;
-    FlatToRiscv_hyps :> FlatToRiscvCommon.assumptions;
-    ext_spec_ok :> Semantics.ext_spec.ok (FlattenExpr.mk_Semantics_params FlattenExpr_parameters);
-    compile_ext_call_correct: forall resvars extcall argvars,
-        compiles_FlatToRiscv_correctly
-          compile_ext_call (FlatImp.SInteract resvars extcall argvars);
-    compile_ext_call_length_ignores_positions: forall stackoffset posmap1 posmap2 c pos1 pos2,
-      List.length (compile_ext_call posmap1 pos1 stackoffset c) =
-      List.length (compile_ext_call posmap2 pos2 stackoffset c);
-  }.
-
-End Pipeline.
-
-Section Pipeline1.
-
-  Context {p: parameters}.
-  Context {h: assumptions}.
-
-  Add Ring wring : (word.ring_theory (word := word))
+  Add Ring wring : (word.ring_theory (word := Utility.word))
       (preprocess [autorewrite with rew_word_morphism],
-       morphism (word.ring_morph (word := word)),
+       morphism (word.ring_morph (word := Utility.word)),
        constants [word_cst]).
-
-  Instance mk_FlatImp_ext_spec_ok:
-    FlatImp.ext_spec.ok  string (FlattenExpr.mk_FlatImp_params FlattenExpr_parameters).
-  Proof.
-    destruct h. destruct ext_spec_ok0.
-    constructor.
-    all: intros; eauto.
-    eapply intersect; eassumption.
-  Qed.
-
-  Instance FlattenExpr_hyps: FlattenExpr.assumptions FlattenExpr_parameters.
-  Proof.
-    constructor.
-    - apply (string_keyed_map_ok (p := p) (@word (@FlattenExpr.W (@FlattenExpr_parameters p)))).
-    - exact mem_ok.
-    - apply (string_keyed_map_ok (p := p) (list string * list string * Syntax.cmd.cmd)).
-    - apply (string_keyed_map_ok (p := p) (list string * list string * FlatImp.stmt string)).
-    - apply mk_FlatImp_ext_spec_ok.
-  Qed.
-
-  Local Notation renamed_env := (@string_keyed_map p (list Z * list Z * FlatImp.stmt Z)).
 
   (* Note: we could also track code size from the source program all the way to the target
      program, and a lot of infrastructure is already there, will do once/if we want to get
      a total compiler.
      Returns the fun_pos_env so that users know where to jump to call the compiled functions. *)
-  Definition riscvPhase(prog: renamed_env):
-    option (list Instruction * funname_env Z * Z) :=
-    bind_opt stack_words_needed <- stack_usage prog;
-    let positions := FlatToRiscvDef.build_fun_pos_env prog in
-    let '(i, _) := FlatToRiscvDef.compile_funs positions prog in
-    Some (i, positions, stack_words_needed).
+  Definition riscvPhase(prog: env): option (list Instruction * funname_env Z * Z) :=
+    match stack_usage prog with
+    | Some stack_words_needed =>
+      let positions := FlatToRiscvDef.build_fun_pos_env prog in
+      let '(i, _) := FlatToRiscvDef.compile_funs positions prog in
+      Some (i, positions, stack_words_needed)
+    | None => None
+    end.
 
-  Local Instance map_ok': @map.ok (@word (@W p)) Init.Byte.byte (@mem p) := mem_ok.
+  Context {hyps: @FlatToRiscvCommon.assumptions p}.
 
   Lemma get_build_fun_pos_env: forall e f,
       map.get e f <> None ->
@@ -191,9 +94,11 @@ Section Pipeline1.
       + auto.
   Qed.
 
+(*
   Local Definition FlatImp__word_eq : FlatImp.word -> FlatImp.word -> bool := word.eqb.
   Local Instance  EqDecider_FlatImp__word_eq : EqDecider FlatImp__word_eq.
   Proof. eapply word.eqb_spec. Unshelve. all: exact word_ok. Qed.
+*)
 
   Lemma mem_available_to_exists: forall start pastend (m: mem) P,
       (mem_available start pastend * P)%sep m ->
@@ -312,12 +217,15 @@ Section Pipeline1.
            | |- (_ + _)%nat = (_ + _)%nat => f_equal
            end.
 
+  Hypothesis compile_ext_call_length_ignores_positions: forall stackoffset posmap1 posmap2 c pos1 pos2,
+      List.length (compile_ext_call posmap1 pos1 stackoffset c) =
+      List.length (compile_ext_call posmap2 pos2 stackoffset c).
+
   Lemma compile_stmt_length_ignores_positions: forall posmap1 posmap2 c stackoffset pos1 pos2,
       List.length (FlatToRiscvDef.compile_stmt posmap1 pos1 stackoffset c) =
       List.length (FlatToRiscvDef.compile_stmt posmap2 pos2 stackoffset c).
   Proof.
     induction c; intros; ignore_positions.
-    apply compile_ext_call_length_ignores_positions.
   Qed.
 
   Lemma compile_function_length_ignores_positions: forall posmap1 posmap2 pos1 pos2 impl,
@@ -443,6 +351,8 @@ Section Pipeline1.
     f_equal.
     f_equal.
     solve_word_eq word_ok.
+    Unshelve.
+    all: (simpl; typeclasses eauto).
   Qed.
 
   Open Scope ilist_scope.
@@ -471,17 +381,20 @@ Section Pipeline1.
       (* configured by PrimitivesParams, can contain invariants needed for external calls *)
       valid_machine mach.
 
+  Hypothesis compile_ext_call_correct: forall resvars extcall argvars,
+      compiles_FlatToRiscv_correctly compile_ext_call (FlatImp.SInteract resvars extcall argvars).
+
   Lemma flat_to_riscv_correct: forall
       (ml: MemoryLayout)
       (mlOk: MemoryLayoutOk ml)
       (f_entry_name : string) fbody (f_entry_rel_pos req_stack_size: Z)
       (p_call p_functions: word)
       (Rdata Rexec : mem -> Prop)
-      (functions: renamed_env)
+      (functions: env)
       (instrs: list Instruction)
       (pos_map: funname_env Z)
       (mH: mem) (mc: MetricLog)
-      (postH: Semantics.trace -> Semantics.mem -> Prop)
+      (postH: trace -> mem -> Prop)
       (initial: MetricRiscvMachine),
       (forall f fun_impl, map.get functions f = Some fun_impl -> FlatToRiscvDef.valid_FlatImp_fun fun_impl) ->
       riscvPhase functions = Some (instrs, pos_map, req_stack_size) ->
@@ -516,16 +429,15 @@ Section Pipeline1.
       congruence.
     }
     eapply runsTo_weaken.
-    - specialize compile_stmt_correct with (1 := @compile_ext_call_correct _ h).
+    - specialize compile_stmt_correct with (1 := compile_ext_call_correct).
       unfold compiles_FlatToRiscv_correctly. intros compile_stmt_correct.
       eapply compile_stmt_correct with (g := {|
         e_pos := FlatToRiscvDef.build_fun_pos_env functions;
-        (* PARAMRECORDS requires annotation *)
-        program_base := p_functions : (@word (@FlatToRiscvCommon.W (@FlatToRiscv_params p)));
+        program_base := p_functions;
         e_impl := functions;
         rem_stackwords := word.unsigned (word.sub (stack_pastend ml) (stack_start ml)) / bytes_per_word;
         rem_framewords := 0;
-        p_insts := p_call : (@word (@FlatToRiscvCommon.W (@FlatToRiscv_params p)));
+        p_insts := p_call;
         insts := [[Jal RegisterNames.ra
                       (f_entry_rel_pos + word.signed (word.sub p_functions (getPc initial)))]];
         xframe := Rexec;
@@ -548,7 +460,8 @@ Section Pipeline1.
         - eapply H. eassumption.
         - unfold FlatToRiscvDef.build_fun_pos_env, FlatToRiscvDef.build_fun_pos_env.
           pose proof (get_compile_funs_pos functions) as P.
-          destruct (FlatToRiscvDef.compile_funs map.empty functions) as [ insts posmap ] eqn: F.
+          ParamRecords.simpl_param_projections.
+          destruct (compile_funs map.empty functions) as [ insts posmap ] eqn: F.
           eapply P.
           eassumption. }
       { eapply fits_stack_call. 2: eassumption. 1: reflexivity.
@@ -608,7 +521,7 @@ Section Pipeline1.
           simpl in P|-*. unfold program in P.
           seprewrite P. clear P.
           rewrite <- Z_div_exact_2; cycle 1. {
-            unfold bytes_per_word. clear -h.
+            unfold bytes_per_word. clear.
             destruct width_cases as [E | E]; rewrite E; reflexivity.
           }
           {
@@ -653,7 +566,7 @@ Section Pipeline1.
           intros w.
           rewrite HList.tuple.length_to_list; trivial. }
         use_sep_assumption.
-        cbn [dframe xframe ghostConsts program_base ghostConsts e_pos e_impl p_insts insts].
+        cbn [dframe xframe program_base e_pos e_impl p_insts insts].
         change (if width =? 32 then RV32I else RV64I) with iset.
         wwcancel.
         case TODO.
@@ -697,7 +610,7 @@ Section Pipeline1.
         end.
         symmetry in P.
         rewrite P. clear P.
-        cbn [dframe xframe ghostConsts program_base ghostConsts e_pos e_impl p_insts insts program].
+        cbn [dframe xframe program_base e_pos e_impl p_insts insts program].
         simpl.
         unfold ptsto_bytes, ptsto_instr, truncated_scalar, littleendian, ptsto_bytes.ptsto_bytes.
         simpl.
@@ -707,4 +620,4 @@ Section Pipeline1.
     all: try (simpl; typeclasses eauto).
   Qed.
 
-End Pipeline1.
+End LowerPipeline.
