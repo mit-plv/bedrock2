f2f0b027f3aa1c21e821a5fc09d0d2456f7e20e6
Clément Pit-Claudel <clement.pitclaudel@live.com>
2021-10-02 00:44:15 -0400

    lib/api: Export Lib.Alloc

--- src/Rupicola/Lib/Api.v
+++ src/Rupicola/Lib/Api.v
@@ -2,4 +2,5 @@ Require Export Rupicola.Lib.Core.
 Require Export Rupicola.Lib.Tactics.
 Require Export Rupicola.Lib.Notations.
 Require Export Rupicola.Lib.Compiler.
+Require Export Rupicola.Lib.Alloc.
 Require Export Coq.derive.Derive.
