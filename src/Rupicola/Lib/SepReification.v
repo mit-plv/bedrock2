2453691ee947746f926611cedd690fbda27aa452
Clément Pit-Claudel <clement.pitclaudel@live.com>
2021-10-02 23:01:54 -0400

    lib: Document SepReification

--- src/Rupicola/Lib/SepReification.v
+++ src/Rupicola/Lib/SepReification.v
@@ -162,5 +162,58 @@ End Tactics.
 
 Export Tactics.
 
+(** The hint below is only a heuristic: it works well for stack allocation, for
+    example, but it does not work in all cases.
+
+    The separation logic problems that Rupicola needs to solve fall into two
+    categories: *in* parameters and *(in-)out* parameters.  Consider this
+    function and its spec::
+
+        void add(bignum* p1, bignum* p2, bignum* pout)
+
+        requires := p1 ↦ o1 * R1 &&& p2 ↦ o2 * R2 &&& pout ↦ out * Rout
+
+        ensures := pout ↦ sum(op1, op2) * Rout
+
+    Here `p1` and `p2` are *in* parameters: they (the pointers) are only read.
+    `pout` is an out parameter: it is used for writing.  The spec of the
+    function shows that:
+
+    This peculiar spec allows `p1`, `p2`, and `out` to overlap in arbitrary
+    ways: for example, calling `add(x, y, x)` will increment `x` by `y` in
+    place. Assuming a memory `x ↦ X * y ↦ Y`, this corresponds to invoking `add`
+    with `p1 == pout == x`, `p2 == y`, `R1 == Rout == y ↦ Y`, and `R2 == x ↦ X`.
+
+
+    To infer the values of `R1`, `R2`, and `Rout`, Rupicola uses Bedrock2's
+    cancellation.  This works well for formulae with a single non-separating
+    conjunct, but there is a subtle issue when there is more than one such
+    parameter.  Consider a call to `add(x, y, z)`:
+
+    - If memory is described by `z ↦ Z * (x ↦ X &&& y ↦ Y)`, then traditional
+      cancellation will infer `Rout == x ↦ X &&& y ↦ Y`, which is good.
+    - But if we have `(z ↦ Z * x ↦ X) &&& (z ↦ Z * y ↦ Y)` instead (this is
+      the same formula, written differently), then naive cancellation will yield
+      either `Rout == x ↦ X` or `Rout == y ↦ Y`, and the postcondition will lose
+      information about one of the two input variables… which is bad.
+
+    Note that in both cases the problem is only with *out* parameters: for *in*
+    parameters we only need to show that there is some pointer pointing to the
+    right object.
+
+
+    The heuristic that Rupicola implements is to first try unification against
+    the original formula, and then if this fails to expand to the second form by
+    normalizing and try canceling again.
+
+    Concretely, the unification would succeed for `z` in the first case but fail
+    for `x` and `y`, and subsequently succeed for `x` and `y` against the
+    expanded, normalized form.
+
+    But since Rupicola only ever expands, and never factors, starting with the
+    second form will cause the wrong thing to be inferred for `z` in the example
+    above.
+
+    Stepping through IndirectAdd.v can also be very informative. **)
 #[export] Hint Extern 1 ((sep _ _) _) =>
   normalize_decompose_relevant_sep; shelve : compiler_side_conditions.
