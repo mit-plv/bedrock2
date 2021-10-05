33b6e9120260fa795ace4fb629dafe14ddb7f7e1
Clément Pit-Claudel <clement.pitclaudel@live.com>
2021-10-05 14:54:37 -0400

    examples/expr: Make example larger to facilitate benchmarking

--- src/Rupicola/Examples/Expr.v
+++ src/Rupicola/Examples/Expr.v
@@ -38,16 +38,16 @@ Section with_parameters.
 
   Definition exZ (x: Z) (y: Z) :=
     let t := y + x in
-    let t1 := (1 - x) * (Z.land y 1) in
-    let/n t1 := t1 - (Z.lxor t 1) in
-    let t2 := (1 - y) * (y + 3) in
+    let t1 := ((1 - x) * (Z.land y 1) - x) * (Z.land y 1) in
+    let/n t1 := t1 - (Z.lxor (Z.lxor 0 1) (Z.lxor 0 t)) in
+    let t2 := ((y + 3) - (1 - y)) * (y + 3) in
     let/n t2 := Z.lor x (t * 3 + 1) in
-    let t3 := t - (Z.lxor t 1) in
-    let t3 := (1 - t3) * (y + t3) in
-    let/n t3 := Z.lor x (t3 * 3 + 1) in
+    let t3 := t - (Z.lxor (Z.lxor t 1) 1) in
+    let t3 := (1 - t3) * (((1 - t3) * (y + t3)) + t3) in
+    let/n t3 := Z.lor (Z.lor x (t3 * 3 + 1)) (Z.lxor 0 (t3 * 3 + 1)) in
     let t4 := (Z.land (y + 1) (Z.lxor (t - y + t) 0)) in
-    let/n t4 := Z.lor t4 (1 + t4) in
-    let t5 := t + (t - 8) in
+    let/n t4 := Z.lor (1 + t4) (1 - t4) in
+    let t5 := 3 * ((t + 8) + (t - 8)) in
     let/n x := t1 + t2 - t3 + t4 - t5 in
     x.
 
