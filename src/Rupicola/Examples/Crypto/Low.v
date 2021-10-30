6eee932da4bde1b0541a96c5912bca5bb65b4e45
Clément Pit-Claudel <clement.pitclaudel@live.com>
2021-10-30 01:40:11 -0400

    examples/crypto: Complete definition of lowered Gallina poly1305

--- src/Rupicola/Examples/Crypto/Low.v
+++ src/Rupicola/Examples/Crypto/Low.v
@@ -1,36 +1,88 @@
 (* Rewritten versions of poly1305 and chacha20 that you can compile with Rupicola *)
+Require Import Rupicola.Lib.Api.
 Require Import Rupicola.Examples.ChaCha20Poly1305.Spec.
+Require Import bedrock2.BasicC32Semantics.
 
-Definition poly1305 (p:=2^130-5) (k : list byte) (m : list byte) : list byte :=
-  let/n f16, l16 := split 16 k in
-  let/n scratch := make_buffer felem_size in
+(* TODO array_split should record a special fact in the context to make it trivial to re-split. *)
+Open Scope Z_scope.
+
+Definition array_t := List.list.
+Definition array_len {T} (a: array_t T) := List.length a.
+Definition array_split_at {T} (n: nat) (a: array_t T) :=
+  (List.firstn n a, List.skipn n a).
+Definition array_unsplit {T} (a1 a2: array_t T) := a1 ++ a2.
+Definition array_slice_at {T} (start len: nat) (a: array_t T) :=
+  let (before, rest) := array_split_at start a in
+  let (middle, after) := array_split_at len rest in
+  (before, middle, after).
+Definition array_unslice {T} (a1 a2 a3: array_t T) := a1 ++ a2 ++ a3.
+
+Definition buffer_t := List.list.
+Definition buf_make T (n: nat) : buffer_t T := [].
+Definition buf_push {T} (buf: buffer_t T) (t: T) := buf ++ [t].
+Definition buf_append {T} (buf: buffer_t T) (arr: array_t T) := buf ++ arr.
+Definition buf_split {T} (buf: buffer_t T) : array_t T * buffer_t T := (buf, []).
+Definition buf_unsplit {T} (arr: array_t T) (buf: buffer_t T) : buffer_t T := arr.
+
+Definition p : Z := 2^130 - 5.
+Definition felem_init_zero := 0.
+Definition felem_add (z1 z2: Z) : Z := z1 + z2 mod p.
+Definition felem_mul (z1 z2: Z) : Z := z1 * z2 mod p.
+Definition bytes_as_felem_inplace (bs: list byte) : Z :=
+  le_combine bs.
+Definition felem_as_uint128 z : Z := z mod 2 ^ 128.
+
+Definition uint128_add z1 z2 : Z :=
+  (z1 + z2) mod 2 ^ 128.
+Definition uint128_as_bytes (z: Z) :=
+  le_split 16 z.
+Definition bytes_as_uint128 (bs: list byte) :=
+  le_combine bs.
+
+Definition w32s_of_bytes (bs: list byte) : list word :=
+  List.map word.of_Z (List.map le_combine (chunk 4 bs)).
+
+Definition bytes_of_w32s (w32s: list word) : list byte :=
+  List.flat_map (le_split 4) (List.map word.unsigned w32s).
+
+Definition array_fold_chunked {A T}
+           (a: array_t T) (size: nat)
+           (f: A -> list T -> A)
+           a0 :=
+  List.fold_left (fun acc c => f acc c) (chunk size a) a0.
+
+Axiom felem_size : nat.
+
+Definition byte_land (b1 b2: byte) :=
+  byte.of_Z (Z.land (byte.unsigned b1) (byte.unsigned b2)).
+
+Definition poly1305 (k : list byte) (m : list byte) (output: Z): list byte :=
+  let/n (f16, l16) := array_split_at 16 k in
+  let/n scratch := buf_make byte felem_size in
   let/n scratch := buf_append scratch f16 in
-  let/n f16, lone_byte_and_felem_spare_space := split_buffer scratch in
-  let/n f16 := as_word_list f16 in
-  let/n f16 := List.map (fun w =>
-                        let/n w := word.and w 0x0ffffffc in
-                        w)
-                     f16 in
-  let/n f16 := as_byte_list f16 in
-  let/n scratch := reassemble_buffer f16 lone_byte_and_felem_spare_space in
-  let/n scratch := buffer_push scratch 0 in
-  let/n scratch := buffer_as_array scratch in
-  let/n scratch := as_felem_inplace scratch in (* B2 primitive reads first 17 bytes of longer array *)
-  let/n a := stack (felem_init_zero ()) in
-  let/n nchunks := Nat.div_up m_length 16 in
-  let/n a := List.iter  (fun idx a ck =>
-                       let/n nscratch := make_buffer felem_size in
-                       let/n ck_start := idx * 16 in
-                       let/n ck_end := min (ck_start + 16) m_length in
-                       let/n before, ck, after := slice m ck_start ck_end in
-                       let/n nscratch := buf_append nscratch ck in (* len = 16 *)
-                       let/n m := reassemble before ck after in
-                       let/n nscratch := buf_push nscratch x01 in (* len = 17 *)
-                       let/n nscratch := as_felem_inplace nscratch in
-                       let/n a := felem.add a nscratch in
-                       let/n a := felem.mul a scratch in
-                       a)
-                    a in
-  let/n output := felem_as_bytes a in
-  let/n output := andres_uint128_add output l16 in (* FIXME reassemble inputs *)
+  let/n (scratch, lone_byte_and_felem_spare_space) := buf_split scratch in
+  let/n scratch := List.map (fun '(w1, w2) => let/n w := byte_land w1 w2 in w)
+                           (combine scratch (le_split 16 0x0ffffffc0ffffffc0ffffffc0fffffff)) in
+  let/n scratch := buf_unsplit scratch lone_byte_and_felem_spare_space in
+  let/n scratch := buf_push scratch x00 in
+  let/n scratch := bytes_as_felem_inplace scratch in (* B2 primitive reads first 17 bytes of longer array *)
+  let output := felem_init_zero in
+  let/n output :=
+      array_fold_chunked
+        m 16
+        (fun output ck =>
+           let/n nscratch := buf_make byte felem_size in
+           let/n nscratch := buf_append nscratch ck in (* len = 16 *)
+           let/n nscratch := buf_push nscratch x01 in (* len = 17 *)
+           let/n nscratch := bytes_as_felem_inplace nscratch in
+           let/n output := felem_add output nscratch in
+           let/n output := felem_mul output scratch in
+           output)
+        output in
+  let/n output := felem_as_uint128 output in
+  let/n l16 := bytes_as_uint128 l16 in
+  let/n output := uint128_add output l16 in
+  let/n l16 := uint128_as_bytes l16 in
+  let/n k := array_unsplit f16 l16 in
+  let/n output := uint128_as_bytes output in
   output.
