c8b5b71586bbaf87405b98d9bf4cecf459b3c3b2
Clément Pit-Claudel <cpitclaudel@users.noreply.github.com>
2021-11-18 07:53:08 -0600

    ToCString: further appease compiler optimizations for memory access (#203)

--- bedrock2/src/bedrock2/ToCString.v
+++ bedrock2/src/bedrock2/ToCString.v
@@ -4,46 +4,30 @@ Require Import Coq.ZArith.BinIntDef Coq.Numbers.BinNums Coq.Numbers.DecimalStrin
 Require Import Coq.Strings.String. Local Open Scope string_scope.
 
 Definition prelude := "#include <stdint.h>
+#include <string.h>
 
-// We use bytewise access to work around -fstrict-aliasing.
-// On clang 10, these functions could be replaced with memcpy but gcc 11
-// seems to fail to infer the bounds on an integer loaded by memcpy.
+// We use memcpy to work around -fstrict-aliasing.
+// A plain memcpy is enough on clang 10, but not on gcc 10, which fails
+// to infer the bounds on an integer loaded by memcpy.
 // Adding a range mask after memcpy in turn makes slower code in clang.
-// The little-endian memory access idioms below are fast in both.
-static inline uintptr_t _br2_load(uintptr_t a, uintptr_t sz) {
-  uint8_t* p = (uint8_t*) a;
-  uintptr_t r = 0;
+// Loading individual bytes, shifting them together, and or-ing is fast
+// on clang and sometimes on GCC, but other times GCC inlines individual
+// byte operations without reconstructing wider accesses.
+// The little-endian idiom below seems fast in gcc 9+ and clang 10.
+static __attribute__((always_inline)) inline uintptr_t
+_br2_load(uintptr_t a, uintptr_t sz) {
   switch (sz) {
-    case 8: r |= (uintptr_t)p[7] << 56;
-            r |= (uintptr_t)p[6] << 48;
-            r |= (uintptr_t)p[5] << 40;
-            r |= (uintptr_t)p[4] << 32;
-            // fall through
-    case 4: r |= (uintptr_t)p[3] << 24;
-            r |= (uintptr_t)p[2] << 16;
-            // fall through
-    case 2: r |= (uintptr_t)p[1] << 8;
-            // fall through
-    case 1: r |= (uintptr_t)p[0];
+  case 1: { uint8_t  r = 0; memcpy(&r, (void*)a, 1); return r; }
+  case 2: { uint16_t r = 0; memcpy(&r, (void*)a, 2); return r; }
+  case 4: { uint32_t r = 0; memcpy(&r, (void*)a, 4); return r; }
+  case 8: { uint64_t r = 0; memcpy(&r, (void*)a, 8); return r; }
+  default: __builtin_unreachable();
   }
-  return r;
 }
 
-static inline void _br2_store(uintptr_t a, uintptr_t v, uintptr_t sz) {
-  uint8_t* p = (uint8_t*) a;
-  switch (sz) {
-    case 8: p[7] = v >> 56;
-            p[6] = v >> 48;
-            p[5] = v >> 40;
-            p[4] = v >> 32;
-            // fall through
-    case 4: p[3] = v >> 24;
-            p[2] = v >> 16;
-            // fall through
-    case 2: p[1] = v >> 8;
-            // fall through
-    case 1: p[0] = v;
-  }
+static __attribute__((always_inline)) inline void
+_br2_store(uintptr_t a, uintptr_t v, uintptr_t sz) {
+  memcpy((void*)a, &v, sz);
 }
 ".
 
