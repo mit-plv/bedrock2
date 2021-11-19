478d8db4e7e691ff5b5af3cf40a672e9c7a6843c
Andres Erbsen <andreser@mit.edu>
2021-11-19 13:57:06 -0500

    record cpu info during benchmarking

--- src/Rupicola/Examples/benchmark.sh
+++ src/Rupicola/Examples/benchmark.sh
@@ -2,8 +2,8 @@
 set -eu
 cd "$(dirname "$0")"
 
-printf "# %s\n" "$(grep 'processor\W*2' -A5 /proc/cpuinfo | tr '\n' ';' |  sed 's/;/; /g; s/\s\+/ /g; s/ \?: \?/=/g')"
 {
+	printf "# %s\n" "$(grep 'processor\W*2' -A5 /proc/cpuinfo | tr '\n' ';' |  sed 's/;/; /g; s/\s\+/ /g; s/ \?: \?/=/g')"
 	printf 'data=[\n'
 	for CC in clang gcc gcc-10; do
 		find . -name ubench.sh | xargs -n1 env CC="$CC" CFLAGS="-O3 -march=native" sh
--- src/Rupicola/Examples/benchmark_ocaml.sh
+++ src/Rupicola/Examples/benchmark_ocaml.sh
@@ -2,8 +2,8 @@
 set -eu
 cd "$(dirname "$0")"
 
-printf "# %s\n" "$(grep 'processor\W*2' -A5 /proc/cpuinfo | tr '\n' ';' |  sed 's/;/; /g; s/\s\+/ /g; s/ \?: \?/=/g')"
 {
+	printf "# %s\n" "$(grep 'processor\W*2' -A5 /proc/cpuinfo | tr '\n' ';' |  sed 's/;/; /g; s/\s\+/ /g; s/ \?: \?/=/g')"
 	printf 'data=[\n'
 	find Net/IPChecksum -name 'ubench_ocaml.sh' | xargs -n1 env CC="clang" CFLAGS="-O3 -march=native" sh
 	for CC in clang gcc gcc-10; do
