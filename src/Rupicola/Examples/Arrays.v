a613f2b940a2aa2687788475cf954dff03ce2aef
Clément Pit-Claudel <clement.pitclaudel@live.com>
2021-12-01 00:13:58 -0500

    examples/arrays: Add an example of a pointer cast

--- src/Rupicola/Examples/Arrays.v
+++ src/Rupicola/Examples/Arrays.v
@@ -1,6 +1,8 @@
 Require Import Rupicola.Lib.Api.
 Require Import Rupicola.Lib.Arrays.
 Require Import Rupicola.Lib.Loops.
+Require Import Rupicola.Lib.WordNotations.
+Require Import coqutil.Word.LittleEndianList.
 
 Section with_parameters.
   Context {width: Z} {BW: Bitwidth width} {word: word.word width} {mem: map.map word Byte.byte}.
@@ -158,6 +160,107 @@ Section with_parameters.
       Time compile.
     Qed.
   End Loops.
+
+  Section Casts.
+    Notation bytes := (listarray_value (memT := mem) (word := word) access_size.one).
+    Notation words := (listarray_value (memT := mem) (word := word) access_size.word).
+
+    Notation of_nat n := (word.of_Z (Z.of_nat n)).
+    Notation of_Z := word.of_Z.
+    Notation unsigned := word.unsigned.
+
+    Notation bs2ws := (bs2ws (word := word) (Memory.bytes_per (width := width) access_size.word)).
+    Notation ws2bs := (ws2bs (word := word) (Memory.bytes_per (width := width) access_size.word)).
+    Notation bytes_per_word := (Memory.bytes_per (width := width) access_size.word).
+
+(*|
+A simple program that takes as input an array of bytes, reinterprets it as an array of little-endian machine words, and counts the number of matches for a given search term in the resulting array.
+|*)
+
+    Definition count_ws (data: ListArray.t byte) (needle: word) :=
+      let/n r := 0 in
+      let/n data := bs2ws data in
+      let/n r := ListArray.fold_left (fun r w64 =>
+           let/n hit := word.eqb w64 needle in
+           let/n r := r + Z.b2z hit in
+           r)
+        data 0 in
+      let/n data := ws2bs data in
+      r.
+
+(*|
+Instead of applying the mask byte by byte, the program starts by casting its input into a list of 64-bit words, processes these words, and finally casts the result back to a lists of bytes.  Without loading appropriate libraries, Rupicola will not recognize these patterns:
+|*)
+
+    Instance spec_of_count_ws : spec_of "count_ws" :=
+      fnspec! "count_ws" ptr wlen needle / (bs: list byte) R ~> r, {
+        requires tr mem :=
+          wlen = of_nat (length bs) /\
+          Z.of_nat (length bs) < 2 ^ width /\
+          (Datatypes.length bs mod bytes_per_word = 0)%nat /\
+          (bytes ptr bs ⋆ R) mem;
+        ensures tr' mem' :=
+          tr' = tr /\ r = of_Z (count_ws bs needle) /\
+          (bytes ptr bs ⋆ R) mem'
+      }.
+
+    Lemma bytes_as_words ptr bs:
+      (length bs mod bytes_per_word = 0)%nat ->
+      Lift1Prop.iff1 (bytes ptr bs) (words ptr (bs2ws bs)).
+    Proof. apply words_of_bytes. Qed.
+
+    Lemma words_as_bytes ptr bs:
+      (length bs mod bytes_per_word = 0)%nat ->
+      Lift1Prop.iff1 (words ptr (bs2ws bs)) (bytes ptr bs).
+    Proof. symmetry; apply words_of_bytes; assumption. Qed.
+
+    Lemma compile_bs2ws [t m l σ] (bs: list byte):
+      let v := bs2ws bs in
+      forall {P} {pred: P v -> _} {k: nlet_eq_k P v} K r bs_var ptr,
+        (bytes ptr bs ⋆ r) m ->
+        (length bs mod bytes_per_word = 0)%nat ->
+        (forall m' : mem,
+          (words ptr v ⋆ r) m' ->
+          <{ Trace := t; Memory := m'; Locals := l; Functions := σ }> K <{ pred (k v eq_refl) }>) ->
+        <{ Trace := t; Memory := m; Locals := l; Functions := σ }>
+          K
+        <{ pred (let/n x as bs_var eq:Heq := v in k x Heq) }>.
+    Proof. intros; seprewrite_in bytes_as_words H; eauto. Qed.
+
+    Lemma compile_bs2ws_rev [t m l σ] (bs: list byte):
+      let v := ws2bs (bs2ws bs) in
+      forall {P} {pred: P v -> _} {k: nlet_eq_k P v} K r bs_var ptr,
+
+        (words ptr (bs2ws bs) ⋆ r) m ->
+        (length bs mod bytes_per_word = 0)%nat ->
+
+        (forall m' : mem,
+            (bytes ptr bs ⋆ r) m' ->
+            <{ Trace := t; Memory := m'; Locals := l; Functions := σ }>
+              K
+            <{ pred (k v eq_refl) }>) ->
+
+        <{ Trace := t; Memory := m; Locals := l; Functions := σ }>
+          K
+        <{ pred (let/n x as bs_var eq:Heq := v in k x Heq) }>.
+    Proof. intros; seprewrite_in (words_as_bytes) H; eauto. Qed.
+
+    Import UnsizedListArrayCompiler.
+
+    Hint Extern 1 => simple eapply compile_bs2ws; shelve : compiler.
+    Hint Extern 1 => simple eapply compile_bs2ws_rev; shelve : compiler.
+    Hint Extern 1 => (pose proof bytes_per_range access_size.word) : nia.
+    Hint Rewrite @bs2ws_length using solve[eauto with nia] : compiler_side_conditions.
+    Hint Rewrite Nat2Z_inj_div : compiler_side_conditions.
+    Hint Extern 10 => eauto with nia : compiler_side_conditions.
+
+    Derive count_ws_br2fn SuchThat
+      (defn! "count_ws"("data", "len", "needle") ~> "r" { count_ws_br2fn },
+       implements count_ws) As count_ws_br2fn_ok.
+    Proof.
+      compile.
+    Qed.
+  End Casts.
 End with_parameters.
 
 From bedrock2 Require Import BasicC64Semantics NotationsInConstr.
@@ -166,3 +269,4 @@ Compute mask_bytes_br2fn (width := 64).
 Compute xor_bytes_br2fn (word := word).
 Compute incr_words_br2fn (word := word).
 Compute sum_words_br2fn (word := word).
+Compute count_ws_br2fn (word := word).
