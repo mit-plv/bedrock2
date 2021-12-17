950c255b4faac497d4564baa8109a9078342acc8
Andres Erbsen <andreser@mit.edu>
2021-12-17 11:52:27 -0500

    return 0 from stackloop
    closes #220
    
    As noted this is still not actually within C spec, but putting
    (uintptr_t)0 in the return-value register should in practice ensure that
    (int)0 will be read out of it...

--- bedrock2/src/bedrock2/ToCStringStackallocLoopTest.v
+++ bedrock2/src/bedrock2/ToCStringStackallocLoopTest.v
@@ -3,12 +3,13 @@ Import Syntax.Coercions BinInt String List.ListNotations.
 Local Open Scope string_scope. Local Open Scope Z_scope. Local Open Scope list_scope.
 
 Definition main : Syntax.func :=
-  ("main", ([], [], bedrock_func_body:(
+  ("main", ([], ["r"], bedrock_func_body:(
     i = $0; while (i < $(2^20)) {
       stackalloc 2^10 as t;
       store1(t+i>>$10, i);
       i = i+$1
-    }
+    };
+    r = $0
 ))).
 
 Definition main_cbytes := Eval vm_compute in
