a55c99fb9e5f976b34893b43f6c39a507d6097d1
Ashley Lin <ashley.lin.ca@gmail.com>
2022-01-27 23:36:26 -0500

    quarter compilation lemma

--- src/Rupicola/Examples/Crypto/Derive.v
+++ src/Rupicola/Examples/Crypto/Derive.v
@@ -27,4 +27,41 @@ Section Bedrock2.
     compile.
   Qed.
 
+  Lemma compile_quarter : forall {tr mem locals functions} a b c d,
+      let v := quarter a b c d in
+
+      forall {P} {pred: P v -> predicate} {k: nlet_eq_k P v} {k_impl}
+             a_var b_var c_var d_var,
+
+        spec_of_quarter functions ->
+        
+        map.get locals a_var = Some a ->
+        map.get locals b_var = Some b ->
+        map.get locals c_var = Some c ->
+        map.get locals d_var = Some d ->
+
+        (let v := v in
+         forall m,
+           let '\<w, x, y, z\> := v in
+        (<{ Trace := tr;
+            Memory := m;
+            Locals := map.put (map.put (map.put (map.put locals a_var w) b_var x) c_var y) d_var z;
+            Functions := functions }>
+         k_impl
+         <{ pred (k v eq_refl) }>)) ->
+        <{ Trace := tr;
+           Memory := mem;
+           Locals := locals;
+           Functions := functions }>
+        
+        cmd.seq (cmd.call [a_var; b_var; c_var; d_var] "quarter" [expr.var a_var; expr.var b_var; expr.var c_var; expr.var d_var])
+                k_impl
+                
+        <{ pred (nlet_eq [a_var; b_var; c_var; d_var] v k) }>.
+  Proof.
+    repeat straightline.
+    repeat (eexists; split; eauto).
+    handle_call; eauto.
+  Qed.
+
 End Bedrock2.
