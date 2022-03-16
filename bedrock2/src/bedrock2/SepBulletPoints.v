6aacc3f943f2c85eb2ea9bcbfbe84f985e31189c
Samuel Gruetter <saemi472@gmail.com>
2022-03-16 16:14:31 -0400

    bullet point notations for impl1/iff1

--- bedrock2/src/bedrock2/SepBulletPoints.v
+++ bedrock2/src/bedrock2/SepBulletPoints.v
@@ -2,7 +2,7 @@
 
 Require Import coqutil.Map.Interface coqutil.Word.Interface.
 Require Import bedrock2.Lift1Prop.
-Require Import bedrock2.Map.Separation.
+Require Import bedrock2.Map.Separation bedrock2.Map.SeparationLogic.
 
 Declare Scope sep_bullets_scope.
 (* No scope delimiting key so that closing the scope deactivates the notation instead
@@ -13,6 +13,26 @@ Notation "<{ * x * .. * y * z }>" :=
   (at level 0, x at level 39, y at level 39, z at level 39,
    format "<{ '[v '  *  '[' x ']' '//' *  ..  '//' *  '[' y ']' '//' *  '[' z ']'  ']' }>")
   : sep_bullets_scope.
+
+Declare Custom Entry seps_bullet_list.
+
+Notation "* x * y * .. * z" :=
+  (cons x (cons y .. (cons z nil) ..))
+  (in custom seps_bullet_list at level 0,
+   x constr at level 39, y constr at level 39, z constr at level 39,
+   format "'[v' *  '[' x ']' '//' *  '[' y ']' '//' *  ..  '//' *  '[' z ']'  ']'").
+
+Notation "LHS '=========' 'seps' 'impl' '=========' RHS" := (impl1 (seps LHS) (seps RHS))
+  (at level 200, no associativity,
+   LHS custom seps_bullet_list, RHS custom seps_bullet_list,
+   (* leading space to prevent ( * from becoming a comment *)
+   format " '[' LHS '//' '========='  'seps'  'impl'  '=========' '//' RHS ']'").
+
+Notation "LHS '=========' 'seps' 'iff' '=========' RHS" := (iff1 (seps LHS) (seps RHS))
+  (at level 200, no associativity,
+   LHS custom seps_bullet_list, RHS custom seps_bullet_list,
+   (* leading space to prevent ( * from becoming a comment *)
+   format " '[' LHS '//' '========='  'seps'  'iff'  '=========' '//' RHS ']'").
 
 Section NotationTests.
   Context {width : BinInt.Z} {word : Word.Interface.word width}
@@ -60,8 +80,6 @@ Section NotationTests.
     intros. subst manyseps. match goal with |- ?G => enough G end.
   Abort.
 
-  (* TODO reactivate notations for impl1/iff1, which should use seps *)
-
   Goal forall (a b: word) (v: word),
     impl1 <{ * a |-> scalar v
        * b |-> scalar v
@@ -70,20 +88,23 @@ Section NotationTests.
        * b |-> scalar v
        * a |-> scalar v
     }>.
-  Proof. intros. match goal with |- ?G => enough G end. Abort.
+  Proof. intros. reify_goal. match goal with |- ?G => enough G end. Abort.
 
   Goal forall (a b: word) (v: word),
     impl1 manyseps <{
        * b |-> scalar v
        * a |-> scalar v
     }>.
-  Proof. intros. subst manyseps. match goal with |- ?G => enough G end. Abort.
+  Proof.
+    intros. subst manyseps. reify_goal. refine (proj1 _).
+    match goal with |- ?G => enough G end.
+  Abort.
 
   Goal forall (a b: word) (v: word),
     iff1 manyseps <{
        * b |-> scalar v
        * a |-> scalar v
     }>.
-  Proof. intros. subst manyseps. match goal with |- ?G => enough G end. Abort.
+  Proof. intros. subst manyseps. reify_goal. match goal with |- ?G => enough G end. Abort.
 
 End NotationTests.
