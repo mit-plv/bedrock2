2329016b97888c71b6a1cfc7dd93e41cbe74fda0
Andres Erbsen <andreser@mit.edu>
2022-09-06 17:36:41 -0400

    use python instead of sed in bytedump.sh

--- etc/bytedump.sh
+++ etc/bytedump.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 set -eu
-ulimit -s unlimited
+ulimit -s unlimited || true
 
 {
 coqtop -q -quiet $COQFLAGS 2>/dev/null << EOF
@@ -12,4 +12,19 @@ Goal True.
   let bs := eval cbv in ${1} in idtac bs.
 Abort.
 EOF
-} | sed '0,/^COQTOP_CRAP_ENDS_HERE$/d' | sed -z '$ s/\n$//'
+} | python -c '
+import os, sys
+needle = b"\nCOQTOP_CRAP_ENDS_HERE\n"
+waiting_on_coqbug_15373 = True
+b = b""
+while True:
+	r = os.read(0, 1)
+	if not r:
+		sys.exit(waiting_on_coqbug_15373 + 2*(not b.endswith(b"\n")))
+	b = b[-(len(needle)-len(r)):] + r
+	if b == needle:
+		waiting_on_coqbug_15373 = 0
+		b = b""
+	if not waiting_on_coqbug_15373:
+		os.write(1, b[-2:-1])
+'
