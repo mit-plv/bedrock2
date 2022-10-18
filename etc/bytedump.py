50ac26af2d3e1f9d8f3d27682ac765c886c267e2
Jason Gross <jgross@mit.edu>
2022-10-18 11:37:14 +0530

    Integrate some suggestions from Andres

--- etc/bytedump.py
+++ etc/bytedump.py
@@ -7,53 +7,41 @@ try:
 except Exception as e:
     print(e, file=sys.stderr)
 
-is_help = any(f in sys.argv[1:] for f in ('-h', '--help'))
-CONSTANT_NAME = "GALLINA_CONSTANT"
-USAGE = f"Usage: COQFLAGS=... {sys.argv[0]} {CONSTANT_NAME}"
-if len(sys.argv) < 2 or is_help:
-    print(USAGE, file=sys.stderr)
-    sys.exit(0 if is_help else 1)
+if len(sys.argv) != 2 or sys.argv[1] in ("-h", "--help"):
+    print(f"Usage: COQFLAGS=... {sys.argv[0]} GALLINA_CONSTANT", file=sys.stderr)
+    sys.exit(1)
 
+constant = sys.argv[1]
+modpath, _ = constant.rsplit(".", 1)
+COQFLAGS = shlex.split(os.getenv("COQFLAGS", default=""))
 # we need stdin to be bytes in order to get stdout as bytes
 # use os.fsencode to get bytes, as per https://stackoverflow.com/a/27185688/377022
-constant = os.fsencode(sys.argv[1])
-modpath = b".".join(constant.split(b".")[:-1])
-COQFLAGS = shlex.split(os.getenv("COQFLAGS", default=""))
-p = subprocess.run(["coqtop", "-q", "-quiet"] + COQFLAGS, capture_output=True, input=b"""
-Require bedrock2.PrintListByte """ + modpath + b""".
+p = subprocess.run(["coqtop", "-q", "-quiet"] + COQFLAGS, capture_output=True, input=os.fsencode(f"""
+Require bedrock2.PrintListByte {modpath}.
 Local Set Printing Width 2147483647.
 Goal True.
   idtac "COQBUG(15373)".
   idtac "LINE_SEPARATOR_LOTTERY".
-  PrintListByte.print_list_byte """ + constant + b""".
+  PrintListByte.print_list_byte {constant}.
 Abort.
-""")
+"""))
+# coqtop only exits with nonzero on the case of segfaults, etc.
+# Normal coq errors do not interrupt coqtop (unlike coqc), so all we
+# can go on is the error message
 if p.returncode != 0 or b"\nError:" in p.stderr:
     sys.stderr.buffer.write(p.stderr)
-    sys.exit(p.returncode or 5)
+    sys.exit(p.returncode or 2)
 
-#  strip header, detect \r\n or \n, convert to \n, strip last \n
+# strip header, detect \r\n or \n, convert to \n, strip last \n
 # os.linesep is \n on cygwin but \r\n in cygwin coq on github ci
-stdout = p.stdout
-def read(n):
-    global stdout
-    r, stdout = stdout[:n], stdout[n:]
-    return r
-
-b = b""
-while not b.endswith(b"COQBUG(15373)"):
-	r = read(1); b += r
-	if not r: print(f"{b!r}"); sys.exit(4)
-b = b""
-while not b.endswith(b"LINE_SEPARATOR_LOTTERY"):
-	r = read(1); b += r
-	if not r: print(f"{b!r}"); sys.exit(3)
-linesep = b[:-len(b"LINE_SEPARATOR_LOTTERY")]
-read(len(linesep)) == linesep or sys.exit(2)
-b = b""
-while True:
-	r = read(1); b += r
-	if not r: sys.exit(not b.endswith(b"\n"))
-	if b == linesep: b = b"\n"
-	os.write(1, b[-2:-1])
-	b = b[-1:]
+output = p.stdout
+try:
+    _header, output = output.split(b"COQBUG(15373)", 1)
+    linesep, output = output.split(b"LINE_SEPARATOR_LOTTERY", 1)
+    empty  , output = output.split(linesep, 1)
+    if empty != b"": print(f"Non-empty after LINE_SEPARATOR_LOTTERY: {empty!r}", file=sys.stderr); sys.exit(3)
+    output , end = output[:-len(linesep)], output[-len(linesep):]
+    sys.stdout.buffer.write(output)
+    if end != linesep: print(f"Non-linesep at end: {end!r}", file=sys.stderr); sys.exit(4)
+except ValueError as e:
+    print(f"Failure to split: {output!r}\n{e}", file=sys.stderr); sys.exit(5)
