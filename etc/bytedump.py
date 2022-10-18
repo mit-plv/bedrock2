910ca805c68f396460d8213b759bacda51c7d9f7
Jason Gross <jgross@mit.edu>
2022-10-18 12:04:14 +0530

    Integrate more suggestions, do newline replacement

--- etc/bytedump.py
+++ etc/bytedump.py
@@ -2,7 +2,6 @@
 import os, sys, shlex, subprocess
 try:
     from resource import RLIMIT_STACK, RLIM_INFINITY, setrlimit
-    # tuple is (soft, hard)
     setrlimit(RLIMIT_STACK, (RLIM_INFINITY, RLIM_INFINITY))
 except Exception as e:
     print(e, file=sys.stderr)
@@ -11,18 +10,16 @@ if len(sys.argv) != 2 or sys.argv[1] in ("-h", "--help"):
     print(f"Usage: COQFLAGS=... {sys.argv[0]} GALLINA_CONSTANT", file=sys.stderr)
     sys.exit(1)
 
-constant = sys.argv[1]
-modpath, _ = constant.rsplit(".", 1)
 COQFLAGS = shlex.split(os.getenv("COQFLAGS", default=""))
 # we need stdin to be bytes in order to get stdout as bytes
 # use os.fsencode to get bytes, as per https://stackoverflow.com/a/27185688/377022
 p = subprocess.run(["coqtop", "-q", "-quiet"] + COQFLAGS, capture_output=True, input=os.fsencode(f"""
-Require bedrock2.PrintListByte {modpath}.
+Require bedrock2.PrintListByte {sys.argv[1].rsplit(".", 1)[0]}.
 Local Set Printing Width 2147483647.
 Goal True.
   idtac "COQBUG(15373)".
   idtac "LINE_SEPARATOR_LOTTERY".
-  PrintListByte.print_list_byte {constant}.
+  PrintListByte.print_list_byte {sys.argv[1]}.
 Abort.
 """))
 # coqtop only exits with nonzero on the case of segfaults, etc.
@@ -38,10 +35,11 @@ output = p.stdout
 try:
     _header, output = output.split(b"COQBUG(15373)", 1)
     linesep, output = output.split(b"LINE_SEPARATOR_LOTTERY", 1)
-    empty  , output = output.split(linesep, 1)
-    if empty != b"": print(f"Non-empty after LINE_SEPARATOR_LOTTERY: {empty!r}", file=sys.stderr); sys.exit(3)
-    output , end = output[:-len(linesep)], output[-len(linesep):]
+    output = output.replace(linesep, b"\n")
+    begin  , output = output[:1], output[1:]
+    if begin != b"\n": print(f"Non-linesep after LINE_SEPARATOR_LOTTERY: {begin!r}", file=sys.stderr); sys.exit(3)
+    output , end    = output[:-1], output[-1:]
     sys.stdout.buffer.write(output)
-    if end != linesep: print(f"Non-linesep at end: {end!r}", file=sys.stderr); sys.exit(4)
+    if end   != b"\n": print(f"Non-linesep at end: {end!r}", file=sys.stderr); sys.exit(4)
 except ValueError as e:
     print(f"Failure to split: {output!r}\n{e}", file=sys.stderr); sys.exit(5)
