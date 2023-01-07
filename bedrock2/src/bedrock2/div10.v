f8899a8356b6a50bfd70d42d791b2cb56dac7f52
Samuel Gruetter <saemi472@gmail.com>
2023-01-07 13:21:32 -0500

    delete some old unused files

--- bedrock2/src/bedrock2/div10.v
+++
@@ -1,3 +0,0 @@
-Require Import Coq.ZArith.ZArith coqutil.Z.Lia coqutil.Z.div_mod_to_equations.
-Lemma div10_by_mul a : (0 <= a < 2^32 -> a/10 = (a*3435973837)/2^35)%Z.
-Proof. intros. Z.div_mod_to_equations. blia. Qed.
