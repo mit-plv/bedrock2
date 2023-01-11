f7b96b7f432765cf37f5305e7c7e928f1a8513ce
Pratyush Venkatakrishnan <contact@psvenk.com>
2023-01-11 15:32:20 -0500

    Changed records, started filling out elaborate
    From mhulse, 2023-01-10

--- PyLevelLang/src/PyLevelLang/language.v
+++ PyLevelLang/src/PyLevelLang/language.v
@@ -1,20 +1,27 @@
 (* First attempt at translating high-level language specification into Coq
    (psvenk, 2023-01-09) *)
+(* Changed records, started filling out elaborate 
+   (mhulse, 2023-01-10) *)
 
 Require Import String.
 Require Import ZArith.
-(* Require Import List. *)
+Require Import List.
 
 Inductive type : Type :=
   | TInt
   | TBool
   | TString
-  | TProd (t1 t2 : type)
+  | TPair (t1 t2 : type)
   | TList (t : type)
-  | TRecord (_ : list type).
-  (* TODO: This could be written as [TRecord (_ : list (string * type))], but I
-     decided to write out definitions with unnamed tuples for the time being for
-     convenience *)
+  | TEmpty. (* "Empty" type: its only value should be the empty tuple () *)
+
+(* Construct a "record" type using a list of types *)
+Fixpoint TRecord (l : list type) : type :=
+  match l with
+  (* This creates the issue of having the empty type as the last element *)
+  | nil => TEmpty
+  | t :: ts => TPair t (TRecord ts)
+  end.
 
 (* "Pre-expression": untyped expressions from surface-level parsing. *)
 Inductive pexpr : Type :=
@@ -23,10 +30,10 @@ Inductive pexpr : Type :=
   | PEInt (n : Z)
   | PEBool (b : bool)
   | PEString (s : string)
-  | PEProd (e1 e2 : pexpr)
+  | PEPair (e1 e2 : pexpr)
+  | PEFst (p: pexpr)
+  | PESnd (p: pexpr)
   | PEList (es : list pexpr)
-  | PERecord (xs : list (string * pexpr))
-  | PEProj (rec : pexpr) (i : Z)
   | PERange (lo hi : pexpr)
   | PEFlatmap (e1 : pexpr) (x : string) (e2 : pexpr)
   | PEIf (e1 e2 e3 : pexpr)
@@ -44,15 +51,10 @@ Inductive expr : type -> Type :=
      Bedrock *)
   | EBool (b : bool) : expr TBool
   | EString (s : string) : expr TString
-  | EProd (t1 t2 : type) (e1 : expr t1) (e2 : expr t2) : expr (TProd t1 t2)
+  | EPair (t1 t2 : type) (e1 : expr t1) (e2 : expr t2) : expr (TPair t1 t2)
+  | EFst (t1 t2 : type) (e : expr (TPair t1 t2)) : expr t1
+  | ESnd (t1 t2 : type) (e : expr (TPair t1 t2)) : expr t2
   | EList (t : type) (es : list (expr t)) : expr (TList t)
-  | ERecord (ts : list type) (xs : list {t & expr t}) : expr (TRecord ts)
-  (* TODO: In this case, [ts = map (@projT1 type expr) xs] should be satisfied
-     (that is, the types in [xs] should correspond to [ts], and [xs] and [ts]
-     should have the same length); however; I can't figure out a way to enforce
-     this constraint in the GADT without violating the strict positivity
-     requirement for inductive definitions. *)
-  | EProj (t_rec t_field : type) (rec : expr t_rec) (i : Z) : expr t_field
   | ERange (lo hi : expr TInt) : expr (TList TInt)
   | ELet (t1 t2 : type) (x : string) (e1 : expr t1) (e2 : expr t2) : expr t2
   | EIf (t : type) (e1 : expr TBool) (e2 e3 : expr t) : expr t
@@ -60,16 +62,90 @@ Inductive expr : type -> Type :=
       : expr (TList t).
 
 Inductive command : Type :=
-  | CNop
-  | CChain (c1 c2 : command)
+  | CSkip
+  | CSeq (c1 c2 : command)
   | CLet (t: type) (x : string) (e : expr t) (c : command)
   | CLetMut (t : type) (l : string) (e : expr t) (c : command)
   | CIf (e : expr TBool) (c1 c2 : command)
   | CForeach (t: type) (x : string) (e : expr (TList t)) (c : command).
 
+
+(* A few helper functions *)
+(* Takes an integer n and a natural number c' and returns a 
+   list of TInt expressions corresponding to [n, n + c) *)
+Fixpoint EIntRange (s : Z)  (c: nat) : list (expr TInt) :=
+  match ct with
+  | 0 => nil
+  | S c' => EInt s :: EIntRange (s + 1) c'
+  end.
+
+(* clips an integer to be positive, then turns it into a natural *)
+Definition znatclip (n : Z) : nat := Z.to_nat (Z.max n 0).
+
 (* Type checks a [pexpr] and possibly emits a typed expression
    Checks scoping and field names/indices (for records) *)
+(* TODO: take context into account for variables, locations, and let expressions *)   
 Theorem elaborate : pexpr -> option {t & expr t}.
-Proof. Admitted.
-(* TODO: implement this function. This will probably have to take a store
-   (assigning variable names to types and values) as an argument. *)
+Proof. 
+  intro p.
+  induction p.
+
+  (* PEVar x *)
+  - admit.
+
+  (* PELoc l *)
+  - admit.
+
+  (* PEInt n *)
+  - exact (Some (existT expr TInt (EInt n))).
+
+  (* PEBool n *)
+  - exact (Some (existT expr TBool (EBool b))).
+
+  (* PEString s *)
+  - exact (Some (existT expr TString (EString s))).
+
+  (* PEPair e1 e2 *)
+  - exact (match IHp1, IHp2 with
+           | Some (existT _ t1 e1'), Some (existT _ t2 e2') 
+               => Some (existT expr (TPair t1 t2) (EPair t1 t2 e1' e2'))
+           | _, _ => None
+           end).
+
+  (* PEFst p *)
+  - exact (match IHp with
+           | Some (existT _ tp (EPair t1 t2 e1 e2)) => Some (existT expr t1 e1)
+           | _ => None
+           end).
+
+  (* PESnd p *)
+  - exact (match IHp with
+           | Some (existT _ tp (EPair t1 t2 e1 e2)) => Some (existT expr t2 e2)
+           | _ => None
+           end).
+
+  (* PEList es *)
+  - induction
+
+  (* PERange lo hi *)
+  - exact (match IHp1, IHp2 with
+           | Some (existT _ TInt (EInt lo)), Some (existT _ TInt (EInt hi))
+               => Some (existT expr (TList TInt) (EList TInt (EIntRange lo (znatclip (hi - lo)))))
+           | _, _ => None
+           end).
+
+  (* PEFlatmap e1 x e2 *)
+  - admit.
+
+  (* PEIf e1 e2 e3 *)
+  - exact (match IHp1 with
+           | Some (existT _ TBool (EBool b)) => match b with 
+                                                | true => IHp2 
+                                                | false => IHp3
+                                                end
+           | _ => None
+           end).
+
+  (* PELet x e1 e2 *)
+  - admit.
+Admitted.
