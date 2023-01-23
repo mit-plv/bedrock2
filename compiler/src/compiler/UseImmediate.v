2ca24cafe4c20b6b23b1fe963746528354c8eaab
Arthur De Belen <debelen.arthurreiner@gmail.com>
2023-01-23 03:56:09 -0500

    more progress on UseImmediate impl / proof

--- compiler/src/compiler/UseImmediate.v
+++ compiler/src/compiler/UseImmediate.v
@@ -22,11 +22,12 @@ Section WithArguments.
   Proof.
     intros.
     induction H; simpl; eauto.
-
-    destr (useImmediate is5BitImmediate is12BitImmediate s1); destr (useImmediate is5BitImmediate is12BitImmediate s2); try eauto.
-    destr (map.get l y); destr z; destr op; progress inversion IHexec.
-    { destr (is12BitImmediate v).
-      { destr (x =? v0)%string.
-        { specialize H1 with (t' :=  t) (m' := m) (l' := (map.put l v0 (word.of_Z v))) (mc' := (MetricLogging.addMetricLoads 8 (MetricLogging.addMetricInstructions 8 mc))). clear H2 H3 H4 H5 H6 H7 H8. apply H0 in H9 as H2. apply H1 in H9 as H3.
+    destr s1; destr s2; try eauto; simpl in *; clear H H1.
+    destr z.
+    (* 1: { destr op.
+      { destr (is12BitImmediate v).
+        { destr ((x =? v0)%string).
+          { inversion IHexec. clear IHexec H H1 H2 H3 H4 H5 H6.  specialize H0 with (t' := t) (m' := m) (l' := (map.put l v0 (word.of_Z v))) (mc' := (MetricLogging.addMetricLoads 8 (MetricLogging.addMetricInstructions 8 mc))). apply H0 in H7. clear H0.*)
+    (* 2: { inversion IHexec. clear H H1 H2 H3 H4 H5 H6 IHexec.  specialize H0 with (t' := t) (m' := m) (l' := (map.put l x (word.of_Z v))) (mc' := (MetricLogging.addMetricLoads 8 (MetricLogging.addMetricInstructions 8 mc))). apply H0 in H7. clear H0. *)
 
 End WithArguments.
