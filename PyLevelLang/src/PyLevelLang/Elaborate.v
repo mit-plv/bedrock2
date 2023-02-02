af48c6478387b6e055e8f65a80814284fbc5bb89
Pratyush Venkatakrishnan <contact@psvenk.com>
2023-02-02 11:35:01 -0500

    Revert "Remove elaborate dependency for elaborate_record" and "Finish elaborate_record_wf"
    This reverts commits 0835e40cfe600af587d9d1edd1fcbd2c11348915 and
    13889e881de3a76a4618473131002712291cfff4.

--- PyLevelLang/src/PyLevelLang/Elaborate.v
+++ PyLevelLang/src/PyLevelLang/Elaborate.v
@@ -1,6 +1,6 @@
 Require Import PyLevelLang.Language.
 Require Import coqutil.Map.Interface coqutil.Map.SortedListString.
-Require Import coqutil.Datatypes.Result coqutil.Datatypes.List.
+Require Import coqutil.Datatypes.Result.
 Import ResultMonadNotations.
 
 (* Casts an expression `e` from `expr t2` to `expr t1`, if the two types are
@@ -63,23 +63,6 @@ Section WithMap.
   Proof.
     intros G H He.
     now apply enforce_type_pred with (e := e).
-  Qed.
-
-  Lemma existT_wf {t t'} (e : expr t) (e' : expr t') : forall G,
-    (existT expr t e = existT expr t' e') -> (wf G e -> wf G e').
-  Proof.
-    intros G H He.
-    destruct He; inversion H.
-    - apply wf_EVar.
-      now rewrite <- H2.
-    - apply wf_ELoc.
-      now rewrite <- H2.
-    - apply wf_EConst.
-    - now apply wf_EUnop.
-    - now apply wf_EBinop.
-    - now apply wf_EFlatmap.
-    - now apply wf_EIf.
-    - now apply wf_ELet.
   Qed.
 
   Definition elaborate_const (pc : pconst) : result {t & expr t} :=
@@ -276,31 +259,6 @@ Section WithMap.
       apply wf_EBinop; now apply enforce_type_wf with (G := G) in H2'.
   Qed.
 
-  Fixpoint elaborate_record (xs : list (string * {t & expr t})) :
-    result {t & expr t} :=
-    match xs with
-    | nil =>
-        Success (existT _ _ (EConst CEmpty))
-    | (s, (existT _ _ e1)) :: xs =>
-        '(existT _ _ e2) <- elaborate_record xs;;
-        Success (existT _ _ (EBinop (OPair s _ _) e1 e2))
-    end.
-
-  Lemma elaborate_record_wf (xs : list (string * {t & expr t})) : forall G t' e',
-    Forall (fun '(s, existT _ t e) => wf G e) xs ->
-    elaborate_record xs = Success (existT expr t' e') -> wf G e'.
-  Proof.
-    induction xs as [| [s [t1 e1]]]; intros G t' e' Hxs H.
-    - inversion H. apply wf_EConst.
-    - inversion H.
-      destruct elaborate_record as [[t2 e2] |]; try easy.
-      injection H1 as [= H2 H1].
-      apply existT_wf with (G := G) in H1; [easy | apply wf_EBinop].
-      + now apply Forall_inv in Hxs.
-      + apply Forall_inv_tail in Hxs.
-        now apply IHxs.
-  Qed.
-
   Fixpoint elaborate_proj {t : type} (e : expr t) (s : string) :
     result {t & expr t} :=
     match t as t' return expr t' -> _ with
@@ -327,6 +285,21 @@ Section WithMap.
       + exact H.
       + now apply wf_EUnop.
   Qed.
+
+  Section ElaborateRecord.
+    Context (elaborate : tenv -> pexpr -> result {t & expr t}).
+
+    Fixpoint elaborate_record (G : tenv) (xs : list (string * pexpr)) :
+      result {t & expr t} :=
+      match xs with
+      | nil =>
+          Success (existT _ _ (EConst CEmpty))
+      | (s, p) :: xs =>
+          '(existT _ _ e1) <- elaborate G p;;
+          '(existT _ _ e2) <- elaborate_record G xs;;
+          Success (existT _ _ (EBinop (OPair s _ _) e1 e2))
+      end.
+  End ElaborateRecord.
 
   (* Type checks a `pexpr` and possibly emits a typed expression
      Checks scoping for variables/locations *)
@@ -374,13 +347,8 @@ Section WithMap.
         let G' := map.put G x (t1, false) in
         '(existT _ t2 e2) <- elaborate G' p2;;
         Success (existT _ _ (ELet x e1 e2))
-    | PERecord xs =>
-        let f '(s, p) :=
-          e <- elaborate G p;;
-          Success (s, e)
-        in
-        xs' <- List.all_success (List.map f xs);;
-        elaborate_record xs'
+    | PERecord ps =>
+        elaborate_record elaborate G ps
     | PEProj p s =>
       '(existT _ t e) <- elaborate G p;;
         elaborate_proj e s
@@ -525,21 +493,35 @@ Section WithMap.
       + now apply IHp1.
       + apply IHp2, H2.
     - (* PERecord xs *)
-      destruct List.all_success as [xs' |] eqn : H'; try easy.
-      apply elaborate_record_wf with (xs := xs'); try easy.
-      clear H. revert xs H'.
-      induction xs' as [| [s' [t' e']]]; try easy. intros xs H'.
-      apply Forall_cons.
-      all:
-        destruct xs as [| [s p]]; try easy;
-        simpl in H';
-        destruct (elaborate G p) as [x |] eqn : H1; try easy;
-        destruct List.all_success as [xs0 |] eqn : H2; try easy;
-        injection H' as [= _ H3 H4].
-      + rewrite H3 in H1.
-        now apply elaborate_wf in H1.
-      + rewrite H4 in H2.
-        now apply IHxs' in H2.
+      revert G t e H.
+      induction xs as [| [s p]]; intros G t e H.
+      + (* nil *)
+        inversion H. apply wf_EConst.
+      + (* (s, p) :: xs *)
+        unfold elaborate_record in H.
+        destruct (elaborate G p) as [[t1 e1] |] eqn : H1; try easy.
+        assert (elaborate_record elaborate G xs =
+          (fix elaborate_record
+             (G0 : tenv) (xs0 : list (string * pexpr)) {struct xs0} :
+               result {t0 : type & expr t0} :=
+             match xs0 with
+             | nil =>
+                 Success (existT (fun t0 : type => expr t0) TEmpty (EConst CEmpty))
+             | (s0, p1) :: xs1 =>
+                 ' (existT _ x e0) <- elaborate G0 p1;;
+                 ' (existT _ x0 e2) <- elaborate_record G0 xs1;;
+                 Success
+                   (existT (fun t0 : type => expr t0) (TPair s0 x x0)
+                      (EBinop (OPair s0 x x0) e0 e2))
+             end) G xs
+        ).
+        { easy. }
+        rewrite <- H0 in H. clear H0.
+        destruct (elaborate_record elaborate G xs) as [[t2 e2] |] eqn : H2; try easy.
+        inversion H.
+        apply wf_EBinop.
+        * apply elaborate_wf in H1. exact H1.
+        * apply IHxs. exact H2.
     - (* PEProj p s *)
       unfold elaborate_proj in H.
       destruct (elaborate G p) as [[t0 e0] |] eqn : H0; try easy.
