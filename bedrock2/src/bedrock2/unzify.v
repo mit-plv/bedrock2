e2df9bfaed04d71552020d9aacc730d308bb6a72
Samuel Gruetter <saemi472@gmail.com>
2023-02-15 22:48:52 -0500

    wip unzify

--- bedrock2/src/bedrock2/unzify.v
+++ bedrock2/src/bedrock2/unzify.v
@@ -2,7 +2,19 @@
 
 Require Import Coq.ZArith.ZArith Coq.micromega.Lia.
 Require Import coqutil.Word.Interface coqutil.Word.Properties.
+Require Import coqutil.Tactics.foreach_hyp.
 Local Open Scope Z_scope.
+
+(* returns the name of the newly added or already existing hyp *)
+Ltac unique_pose_proof_name pf :=
+  let t := type of pf in
+  lazymatch goal with
+  | H: t |- _ => H
+  | |- _ => let H := fresh in
+            let __ := match constr:(Set) with
+                      | _ => pose proof pf as H
+                      end in H
+  end.
 
 Module Import word.
   Section WithWord.
@@ -84,8 +96,32 @@ Module Import word.
   End WithWord.
 End word.
 
-Lemma f_equal_impl: forall P P' Q Q': Prop, P = P' -> Q = Q' -> (P -> Q) = (P' -> Q').
-Proof. congruence. Qed.
+Section EmbedNatInZ.
+  Lemma Z_of_nat_range_eq{z}{x: nat}: Z.of_nat x = z -> 0 <= z.
+  Proof. intros. subst. apply Nat2Z.is_nonneg. Qed.
+
+  Lemma Z_of_nat_add_eq: forall (a b: nat) (az bz: Z),
+      Z.of_nat a = az ->
+      Z.of_nat b = bz ->
+      Z.of_nat (Nat.add a b) = az + bz.
+  Proof. intros. subst. apply Nat2Z.inj_add. Qed.
+
+  Lemma Z_of_nat_sub_eq: forall (a b: nat) (az bz: Z),
+      Z.of_nat a = az ->
+      Z.of_nat b = bz ->
+      Z.of_nat (Nat.sub a b) = az - bz. (* TODO needs Z.max 0 *)
+  Abort.
+
+  Lemma Z_of_nat_mul_eq: forall (a b: nat) (az bz: Z),
+      Z.of_nat a = az ->
+      Z.of_nat b = bz ->
+      Z.of_nat (Nat.mul a b) = az * bz.
+  Proof. intros. subst. apply Nat2Z.inj_mul. Qed.
+
+  (* Not using eq_refl to make sure it is not taken as "no change was made" *)
+  Lemma Z_of_nat_const n z: Z.of_nat n = z -> Z.of_nat n = z.
+  Proof. exact id. Qed.
+End EmbedNatInZ.
 
 Ltac zify_unsigned wok e :=
   lazymatch e with
@@ -101,26 +137,61 @@ Ltac zify_unsigned wok e :=
 with zify_unsigned_app2 wok lem x y :=
   let px := zify_unsigned wok x in
   let py := zify_unsigned wok y in
-  constr:(lem _ _ _ _ px py).
+  unique_pose_proof_name (lem _ _ _ _ px py).
+
+Ltac zify_of_nat e :=
+  lazymatch isnatcst e with
+  | false =>
+      lazymatch e with
+      | ?f ?x ?y =>
+          lazymatch f with
+          | Nat.add => zify_of_nat_app2 Z_of_nat_add_eq x y
+          | Nat.mul => zify_of_nat_app2 Z_of_nat_mul_eq x y
+          | _ => constr:(@eq_refl Z (Z.of_nat e))
+          end
+      | _ => constr:(@eq_refl Z (Z.of_nat e))
+      end
+  | true => let z := eval cbv in (Z.of_nat e) in constr:(Z_of_nat_const e z eq_refl)
+  end
+with zify_of_nat_app2 lem x y :=
+  let px := zify_of_nat x in
+  let py := zify_of_nat y in
+  unique_pose_proof_name (lem _ _ _ _ px py).
+
+Lemma f_equal_impl: forall P P' Q Q': Prop, P = P' -> Q = Q' -> (P -> Q) = (P' -> Q').
+Proof. congruence. Qed.
 
 Ltac zify_term wok e :=
+  (*
+  let __ := match constr:(Set) with
+            | _ => idtac "zify_term" e "{"
+            end in
+  let res :=
+  *)
   lazymatch e with
-  | ?f ?x ?y =>
-      lazymatch f with
-      | Logic.and => zify_app2 wok e f x y
-      | Logic.or => zify_app2 wok e f x y
-      | Logic.eq => zify_app2 wok e f x y
-      | _ => constr:(@eq_refl _ e)
-      end
-  | ?f ?x =>
-      lazymatch f with
-      | Logic.not => zify_app1 wok e f x
-      | @word.unsigned _ _ => zify_unsigned wok x
+  | ?f1 ?a0 =>
+      lazymatch f1 with
+      | Logic.not => zify_app1 wok e f1 a0
+      | @word.unsigned _ _ => zify_unsigned wok a0
+      | Z.of_nat => zify_of_nat a0
+      | ?f2 ?a1 =>
+          lazymatch f2 with
+          | Logic.and => zify_app2 wok e f2 a1 a0
+          | Logic.or  => zify_app2 wok e f2 a1 a0
+          | Logic.eq  => zify_app2 wok e f2 a1 a0
+          | Z.add     => zify_app2 wok e f2 a1 a0
+          | Z.sub     => zify_app2 wok e f2 a1 a0
+          | Z.mul     => zify_app2 wok e f2 a1 a0
+          | _ => constr:(@eq_refl _ e)
+          end
       | _ => constr:(@eq_refl _ e)
       end
   | ?P -> ?Q => zify_impl wok e P Q
   | _ => constr:(@eq_refl _ e)
   end
+  (*
+  in let __ := match constr:(Set) with _ => idtac "} =" res end in res
+  *)
 with zify_app1 wok e f x :=
   let px := zify_term wok x in
   lazymatch px with
@@ -150,46 +221,35 @@ with zify_impl wok e x y :=
   | _ => constr:(f_equal_impl px py)
   end.
 
-(*
-Ltac zify_hyp wok h tp :=
-  let pf := zify_term wok e in
-  lazymatch pf with
-  | eq_refl => idtac
-  | _ => lazymatch type of pf
-*)
-
-(* TODO: need to pose unsigned ... for lia equations, not only return,
-   so that we can pose modulo bounds later and save redundant proofs *)
-
-(* TODO: matching should look into rhs (:= and =) of variables to detect patterns like
-   (word.unsigned x) with H: x = word.add a b *)
-
-(* TODO: also pose the right unsigned_range lemmas to get full Euclidean equations *)
-
-(* TODO: Z division/modulo also needs to be turned into Euclidean equations *)
-
-(* structure:
-   rzify recurses into logical connectives, =, <> and word operations, posing a
-   zified hypothesis for each interesting hypothesis.
-   Goal is not yet zified, but each goal will be zified using the same rewrites,
-   so that the goal's subterms match the terms appearing in the zified hypotheses. *)
+Lemma rew_Prop_hyp: forall (P1 P2: Prop) (pf: P1 = P2), P1 -> P2.
+Proof. intros. subst. assumption. Qed.
 
 Inductive zified_hyps_start_here: Prop := mk_zified_hyps_start_here.
 
-Ltac unzify :=
+Ltac zify_hyp wok h tp :=
+  let pf := zify_term wok tp in
+  lazymatch pf with
+  | eq_refl => idtac
+  | _ => let hf := fresh h "Z" in pose proof (rew_Prop_hyp _ _ pf h) as hf
+  end.
+
+Ltac get_word_instance :=
   lazymatch goal with
-  | H: zified_hyps_start_here |- _ =>
-      repeat lazymatch goal with
-        | A: ?T |- _ => lazymatch T with
-                        | zified_hyps_start_here => fail (* done *)
-                        | _ => clear A
-                        end
-        end;
-      clear H
-  | |- _ => fail "no zified_hyps_start_here marker found"
+  | inst: word.word _ |- _ => inst
+  | _: context[@word.unsigned ?wi ?inst] |- _ => inst
+  | _: context[@word.signed ?wi ?inst] |- _ => inst
+  | |- context[@word.unsigned ?wi ?inst] => inst
+  | |- context[@word.signed ?wi ?inst] => inst
+  end.
+
+Ltac get_word_ok :=
+  let wo := get_word_instance in
+  lazymatch constr:(_ : word.ok wo) with
+  | ?wok => wok
   end.
 
 Require Import Ltac2.Ltac2.
+Set Default Proof Mode "Classic".
 
 (* Same signatures as in https://ocaml.org/p/batteries/3.6.0/doc/BatList/index.html *)
 (* TODO: upstream to Coq *)
@@ -214,23 +274,85 @@ Module List.
     end.
 End List.
 
-Ltac2 apply_unsigned_range_in_eqs () :=
+Ltac2 apply_range_bounding_lemma_in_eqs () :=
   match List.drop_while
           (fun p => let (h, rhs, tp) := p in
                     Bool.neg (Constr.equal tp 'zified_hyps_start_here))
           (Control.hyps ())
   with
   | marker :: hs =>
-      List.iter (fun p => let (h, rhs, tp) := p in eapply word.unsigned_range_eq in $h) hs
+      List.iter (fun p => let (h, rhs, tp) := p in
+                          lazy_match! tp with
+                          | word.unsigned _ = _ => eapply word.unsigned_range_eq in $h
+                          | Z.of_nat _ = _ => eapply Z_of_nat_range_eq in $h
+                          | _ => ()
+                          end) hs
   | [] => Control.throw_invalid_argument "No zified_hyps_start_here marker found"
   end.
 
-Ltac apply_unsigned_range_in_eqs := ltac2:(apply_unsigned_range_in_eqs ()).
+Ltac apply_range_bounding_lemma_in_eqs := ltac2:(apply_range_bounding_lemma_in_eqs ()).
 
-Set Default Proof Mode "Classic".
+Ltac zify_hyps :=
+  pose proof mk_zified_hyps_start_here;
+  let wok := get_word_ok in
+  foreach_hyp (zify_hyp wok);
+  apply_range_bounding_lemma_in_eqs.
 
-(*
-Goal forall a b: Z, a = b -> zified_hyps_start_here -> 1 = 1 -> b = a -> True.
+(* TODO: matching should look into rhs (:= and =) of variables to detect patterns like
+   (word.unsigned x) with H: x = word.add a b *)
+
+(* TODO: also pose the right unsigned_range lemmas to get full Euclidean equations *)
+
+(* TODO: Z division/modulo also needs to be turned into Euclidean equations *)
+
+(* structure:
+   rzify recurses into logical connectives, =, <> and word operations, posing a
+   zified hypothesis for each interesting hypothesis.
+   Goal is not yet zified, but each goal will be zified using the same rewrites,
+   so that the goal's subterms match the terms appearing in the zified hypotheses. *)
+
+Ltac unzify :=
+  lazymatch goal with
+  | H: zified_hyps_start_here |- _ =>
+      repeat lazymatch goal with
+        | A: ?T |- _ => lazymatch T with
+                        | zified_hyps_start_here => fail (* done *)
+                        | _ => clear A
+                        end
+        end;
+      clear H
+  | |- _ => fail "no zified_hyps_start_here marker found"
+  end.
+
+Goal forall (a b: nat), Z.of_nat (a + b) = Z.of_nat (a + 0) + Z.of_nat (0 + b).
+Proof.
   intros.
-  apply_unsigned_range_in_eqs.
-*)
+  zify.
+  xlia zchecker.
+Abort.
+
+Section Tests.
+  Context {word: word.word 32} {word_ok: word.ok word}.
+  Local Hint Mode Word.Interface.word - : typeclass_instances.
+
+  Goal forall (left0 right : word) (xs : list word),
+    word.unsigned (word.sub right left0) = 8 * Z.of_nat (Datatypes.length xs + 0) ->
+    forall (x : list word) (x1 x2 : word),
+    word.unsigned (word.sub x2 x1) = 8 * Z.of_nat (Datatypes.length x) ->
+    word.unsigned (word.sub x2 x1) <> 0 ->
+    word.unsigned
+      (word.sub x2
+         (word.add
+            (word.add x1 (word.slu (word.sru (word.sub x2 x1) (word.of_Z 4)) (word.of_Z 3)))
+            (word.of_Z 8))) =
+    8 *
+    Z.of_nat
+      (Datatypes.length x -
+       S (Z.to_nat (word.unsigned (word.sub (word.add x1 (word.slu (word.sru (word.sub x2 x1)
+           (word.of_Z 4)) (word.of_Z 3))) x1) / word.unsigned (word.of_Z 8)))).
+  Proof.
+    intros.
+    zify_hyps.
+    unzify.
+  Abort.
+End Tests.
