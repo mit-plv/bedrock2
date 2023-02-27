95c605a92a3128428d79699d385e2c17e7e706b1
Thanadol Chomphoochan <tcpc@mit.edu>
2023-02-26 23:25:01 -0500

    Current linked list WIP

--- LiveVerif/src/LiveVerifExamples/linked_list.v
+++ LiveVerif/src/LiveVerifExamples/linked_list.v
@@ -18,57 +18,49 @@ uintptr_t malloc(uintptr_t size)                                     /**#
   ghost_args := (R: mem -> Prop);
   requires t m := sep dummy R m;
   ensures t' m' retPtr := t' = t /\ exists anyData,
-       <{ * array uintptr \[size] anyData retPtr
+       <{ * dummy
+          * array uintptr \[size] anyData retPtr
           * R }> m'                                                   #**/ /**.
 Parameter malloc : function_with_callees.
 Parameter malloc_ok : program_logic_goal_for "malloc" malloc.
 
 
 #[export] Instance spec_of_malloc_node: fnspec :=                    .**/
-uintptr_t malloc_node(uintptr_t x, uintptr_t y)                                 /**#
+uintptr_t malloc_node(uintptr_t anything)                                 /**#
   ghost_args := (R: mem -> Prop);
   requires t m := sep dummy R m;
   (* ensures t' m' retPtr := t' = t /\
                           <{ * ex1 (fun x => node x retPtr)
                              * R }> m                               #**/ /**. *)
   ensures t' m' retPtr := t' = t /\ exists x,
-                          <{ * node x retPtr
+                          <{ * dummy
+                             * node x retPtr
                              * R }> m'                               #**/ /**.
 Derive malloc_node SuchThat (fun_correct! malloc_node) As malloc_node_ok. .**/
 { /**. .**/
   uintptr_t r = malloc(2);   /**.
+  assert (len anyData = 2) as Hlen by (purify_heapletwise_hyps; assumption).
 
-  unfold array in *.
-  unfold with_mem in *.  (* TODO: can't this be automatically done in step? *)
-  step.
-  step.
-  bottom_up_simpl_in_hyps.
-  match goal with
-  | H: len ?l = ?n |- _ =>
-      let H' := fresh "Hlen" in
-      rename H into H'
-  end.
   destruct anyData; [discriminate Hlen | idtac].
   destruct anyData; [discriminate Hlen | idtac].
   destruct anyData; [idtac | simpl in Hlen; lia].
-  simpl in H4.
-  unfold with_mem in *.
-  step.
-  step.
-  change (m2 |= R) in H2.
+
   .**/ return r; /**.
+.**/ } /**.
 
   unfold node.
   unfold sepapps.
   simpl.
   unfold sepapp.
- .**/
-} /**.
-  instantiate (1 := {| data := _; next := _ |}).
-  all: reflexivity.
+  (* TODO: automated memory cast *)
+  instantiate (1 := {| data := r0; next := r1 |}).
+  unfold array.
+  simpl.
+  eapply iff1ToEq. cancel.
+  unfold seps. unfold iff1. unfold emp. intuition.
 Qed.
 
-(*
+
 Fixpoint sll (L : list word) (p : word): mem -> Prop :=
   match L with
   | nil => emp (p = /[0])
@@ -90,21 +82,29 @@ Hint Resolve purify_sll : purify.
 
 uintptr_t sll_prepend(uintptr_t p, uintptr_t val) /**#
   ghost_args := (L : list word) (R: mem -> Prop);
-  requires t m := <{ * sll L p
+  requires t m := <{ * dummy
+                     * sll L p
                      * R }> m;
   ensures t' m' res := t' = t /\
-       <{ * sll (val::L) res
+       <{ * dummy
+          * sll (val::L) res
           * R }> m' #**/ /**.
 Derive sll_prepend SuchThat (fun_correct! sll_prepend) As sll_prepend_ok. .**/
-{ /**. .**/
-  uintptr_t r = 0; /**. .**/
-  if (p == 0) { /**. .**/
-    r = malloc_node();           /**. .**/
-  } else { /**. .**/
-    r = 0; /**. .**/
-  } /**. .**/
-  return r; /**. .**/
-} /**.
-Qed. *)
+{ /**.
+  (* TODO: a lot of dummys *)
+  (* TODO: should support empty arguments *)
+  .**/ uintptr_t r = malloc_node(-123); /**.
+  (* set r.data = val *)
+  .**/ store(r, val); /**.
+  .**/ store(r+4, p); /**.
+  .**/ return r; /**.
+  (* TODO: all of this should probably be automated *)
+  replace ((m4 \*/ (m2 \*/ m3)) \*/ m) with (m \*/ m2 \*/ m3 \*/ m4) in D by admit.
+  assert (exists (mm2:mem), m \*/ m2 = mm2) as Hex by admit.
+  destruct Hex as [mm2 Hex]. rewrite Hex in D. clear Hex.
+  assert (mm2 |= sll (val :: L) r) by admit.
+  clear H0 H5 m m2.
+.**/ } /**.
+Abort.
 
 End LiveVerif. Comments .**/ //.
