e2243bc336f5befce94bf60a63e16ebcde62d2f4
Samuel Gruetter <saemi472@gmail.com>
2023-05-11 17:32:27 -0400

    a malloc implementation that satisfies the spec, but is useless :P

--- LiveVerif/src/LiveVerifExamples/onesize_malloc.v
+++ LiveVerif/src/LiveVerifExamples/onesize_malloc.v
@@ -6,14 +6,39 @@ Class malloc_constants := {
   malloc_block_size: Z;
 }.
 
+Section TODO_move.
+  Context {width} {BW: Bitwidth width}
+    {word: word width}{word_ok: word.ok word}
+    {mem: map.map word Byte.byte}{mem_ok: map.ok mem}.
+
+  Definition anybytes(sz: Z)(p: word): mem -> Prop :=
+    ex1 (fun bs => array (uint 8) sz bs p).
+
+End TODO_move.
+
 Load LiveVerif.
 
 Context {consts: malloc_constants}.
 
-(*Fixpoint fixed_size_free_list(a: word): word -> Prop :=*)
+Definition fixed_size_free_list(sz: Z): nat -> word -> mem -> Prop :=
+  fix rec n p :=
+    match n with
+    | O => emp (p = /[0])
+    | S n' => ex1 (fun q => <{ * uintptr q p
+                               * anybytes sz (p ^+ /[4])
+                               * rec n' q }>)
+    end.
 
 Definition allocator: mem -> Prop. Admitted.
 Definition freeable(sz: Z)(a: word): mem -> Prop. Admitted.
+
+Lemma purify_allocator: purify allocator True.
+Proof. unfold purify. intros. constructor. Qed.
+Hint Resolve purify_allocator : purify.
+
+Lemma purify_freeable: forall sz a, purify (freeable sz a) True.
+Proof. unfold purify. intros. constructor. Qed.
+Hint Resolve purify_freeable : purify.
 
 #[export] Instance spec_of_malloc: fnspec :=                                    .**/
 
@@ -25,21 +50,32 @@ uintptr_t malloc (uintptr_t n) /**#
      (p = /[0] /\
       <{ * allocator
          * R }> m') \/
-     (p <> /[0] /\ exists bs,
+     (p <> /[0] /\
       <{ * allocator
-         * array (uint 8) \[n] bs p
+         * anybytes \[n] p
          * freeable \[n] p
          * R }> m') #**/                                                   /**.
-Derive malloc SuchThat (fun_correct! malloc) As malloc_ok.                  (*  .**/
+Derive malloc SuchThat (fun_correct! malloc) As malloc_ok.                      .**/
 {                                                                          /**. .**/
-  uintptr_t x = malloc_base + b;                                           /**. .**/
-
-  uintptr_t r = u_min(a, b);                                               /**. .**/
-  uintptr_t s = u_min(r, c);                                               /**. .**/
-  return s;                                                                /**. .**/
-}                                                                          /**.
+  uintptr_t x = malloc_base + n;                                           /**. .**/
+  x = 0;                                                                   /**. .**/
+  return x;                                                                /**. .**/
+}                                                                          /*?.
+eexists. step. step. step. step. step. step. step. step. left.
+steps.
 Qed.
-*)
-Abort.
+
+#[export] Instance spec_of_free: fnspec :=                                      .**/
+
+void free (uintptr_t p) /**#
+  ghost_args := n (R: mem -> Prop);
+  requires t m := <{ * allocator
+                     * anybytes \[n] p
+                     * freeable \[n] p
+                     * R }> m;
+  ensures t' m' := t' = t /\
+                   <{ * allocator
+                      * R }> m' #**/                                       /**.
+Derive free SuchThat (fun_correct! free) As free_ok. Abort.
 
 End LiveVerif. Comments .**/ //.
