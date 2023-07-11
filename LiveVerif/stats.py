23de42053b7c3523e813a7095c2c41ba1cd0cf1f
Samuel Gruetter <saemi472@gmail.com>
2023-07-11 01:06:56 -0400

    stats: loop counts

--- LiveVerif/stats.py
+++ LiveVerif/stats.py
@@ -2,8 +2,8 @@ import sys
 import os
 import re
 
-def print_table_line(current_file, func_count, snippet_count, linecount_of_file, step_count, secs):
-    print(f"{current_file:<35} & {func_count:>5} & {snippet_count:>8} & {linecount_of_file:>5} & {step_count:>5} & {secs:>8} \\\\")
+def print_table_line(current_file, func_count, snippet_count, linecount_of_file, step_count, secs, loop_count):
+    print(f"{current_file:<35} & {func_count:>5} & {snippet_count:>8} & {linecount_of_file:>5} & {step_count:>5} & {secs:>7} & {loop_count:>5} \\\\")
 
 ignored_funcs = {
     'ring_buf_enq',
@@ -37,6 +37,23 @@ counted_funcs = {
     'sort3_separate_args',
 }
 
+zero_loops = '-'
+
+loop_counts = {
+    'memset': '1+0',
+    'convert_bytes_to_record': zero_loops,
+    'swap': zero_loops,
+    'swap_subarrays': zero_loops,
+    'min': zero_loops,
+    'sort3_separate_args': zero_loops,
+    'swap_record_fields': zero_loops,
+    'sort3': zero_loops,
+    'fibonacci': '1+0',
+    'onesize_malloc': zero_loops,
+    'linked_list': '1+1',
+    'tree_set': '0+1',
+}
+
 def linecount(path):
     with open(path, 'r') as fp:
         return len(fp.readlines())
@@ -56,8 +73,9 @@ def main():
     snippet_count = 0
     secs = 0
     unclassified_funcs = []
+    unknown_loop_counts = []
 
-    print_table_line("File", "Funcs", "Snippets", "Lines", "Steps", "Time[s]")
+    print_table_line("File", "Funcs", "Snippets", "Lines", "Steps", "Time[s]", "Loops")
     print("\\hline")
 
     with open(filepath) as fp:
@@ -85,8 +103,13 @@ def main():
                         secs = m.group(1)
                         if func_count > 0:
                             name = f"\\lstinline|{current_file}|"
+                            loop_count = loop_counts.get(current_file)
+                            if loop_count == None:
+                                loop_count = zero_loops
+                                unknown_loop_counts.append(current_file)
                             print_table_line(name, func_count, snippet_count,
-                                             linecount_of_file, step_count, secs)
+                                             linecount_of_file, step_count, secs,
+                                             '$' + loop_count + '$')
                         current_file = ""
             else:
                 m = re.match("^COQC (.*)", line)
