53e60da87ab1ca06bb44f2c5d48487e432a0e652
Samuel Gruetter <saemi472@gmail.com>
2024-04-18 15:35:53 -0400

    make reversed list notations left-associative

--- bedrock2/src/bedrock2/ReversedListNotations.v
+++ bedrock2/src/bedrock2/ReversedListNotations.v
@@ -1,4 +1,6 @@
 Require Coq.Lists.List.
 
-Notation "xs ;+ x" := (cons x xs) (at level 60, format "xs ;+ x") : list_scope.
-Notation "xs ;++ ys" := (app ys xs) (at level 60, format "xs  ;++  ys") : list_scope.
+Notation "xs ;+ x" := (cons x xs)
+  (at level 59, format "xs  ;+  x", left associativity) : list_scope.
+Notation "xs ;++ ys" := (app ys xs)
+  (at level 59, format "xs  ;++  ys", left associativity) : list_scope.
