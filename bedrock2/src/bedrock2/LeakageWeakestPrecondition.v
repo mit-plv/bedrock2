e888a142b5952ff8a37c753e26fca3e5346a6e74
Owen Conoly <otconoly@hotmail.com>
2024-09-05 21:16:20 -0400

    create LWP

---
+++ bedrock2/src/bedrock2/LeakageWeakestPrecondition.v
@@ -0,0 +1,296 @@
+Require Import coqutil.Macros.subst coqutil.Macros.unique coqutil.Map.Interface coqutil.Map.OfListWord.
+Require Import Coq.ZArith.BinIntDef coqutil.Word.Interface coqutil.Word.Bitwidth.
+Require Import coqutil.dlet bedrock2.Syntax bedrock2.Semantics.
+
+Section WeakestPrecondition.
+  Context {width: Z} {BW: Bitwidth width} {word: word.word width} {mem: map.map word Byte.byte}.
+  Context {locals: map.map String.string word}.
+  Context {ext_spec: ExtSpec}.
+  Implicit Types (t : trace) (m : mem) (l : locals).
+
+  Definition literal v (post : word -> Prop) : Prop :=
+    dlet! v := word.of_Z v in post v.
+  Definition get (l : locals) (x : String.string) (post : word -> Prop) : Prop :=
+    exists v, map.get l x = Some v /\ post v.
+  Definition load s m a (post : _ -> Prop) : Prop :=
+    exists v, load s m a = Some v /\ post v.
+  Definition store sz m a v post :=
+    exists m', store sz m a v = Some m' /\ post m'.
+
+  Section WithMemAndLocals.
+    Context (m : mem) (l : locals).
+    Definition expr_body rec (e : Syntax.expr) (post : word -> Prop) : Prop :=
+      match e with
+      | expr.literal v =>
+        literal v post
+      | expr.var x =>
+        get l x post
+      | expr.op op e1 e2 =>
+        rec e1 (fun v1 =>
+        rec e2 (fun v2 =>
+        post (interp_binop op v1 v2)))
+      | expr.load s e =>
+        rec e (fun a =>
+        load s m a post)
+      | expr.inlinetable s t e =>
+        rec e (fun a =>
+        load s (map.of_list_word t) a post)
+      | expr.ite c e1 e2 =>
+        rec c (fun b => rec (if word.eqb b (word.of_Z 0) then e2 else e1) post)
+    end.
+    Fixpoint expr e := expr_body expr e.
+  End WithMemAndLocals.
+
+  Section WithF.
+    Context {A B} (f: A -> (B -> Prop) -> Prop).
+    Definition list_map_body rec (xs : list A) (post : list B -> Prop) : Prop :=
+      match xs with
+      | nil => post nil
+      | cons x xs' =>
+        f x (fun y =>
+        rec xs' (fun ys' =>
+        post (cons y ys')))
+      end.
+    Fixpoint list_map xs := list_map_body list_map xs.
+  End WithF.
+
+  Section WithFunctions.
+    Context (e: env).
+    Definition dexpr m l e v := expr m l e (eq v).
+    Definition dexprs m l es vs := list_map (expr m l) es (eq vs).
+    (* All cases except cmd.while and cmd.call can be denoted by structural recursion
+       over the syntax.
+       For cmd.while and cmd.call, we fall back to the operational semantics *)
+    Definition cmd_body (rec:_->_->_->_->_->Prop) (c : cmd) (t : trace) (m : mem) (l : locals)
+             (post : trace -> mem -> locals -> Prop) : Prop :=
+      (* give value of each pure expression when stating its subproof *)
+      match c with
+      | cmd.skip => post t m l
+      | cmd.set x ev =>
+        exists v, dexpr m l ev v /\
+        dlet! l := map.put l x v in
+        post t m l
+      | cmd.unset x =>
+        dlet! l := map.remove l x in
+        post t m l
+      | cmd.store sz ea ev =>
+        exists a, dexpr m l ea a /\
+        exists v, dexpr m l ev v /\
+        store sz m a v (fun m =>
+        post t m l)
+      | cmd.stackalloc x n c =>
+        Z.modulo n (bytes_per_word width) = 0 /\
+        forall a mStack mCombined,
+          anybytes a n mStack -> map.split mCombined m mStack ->
+          dlet! l := map.put l x a in
+          rec c t mCombined l (fun t' mCombined' l' =>
+          exists m' mStack',
+          anybytes a n mStack' /\ map.split mCombined' m' mStack' /\
+          post t' m' l')
+      | cmd.cond br ct cf =>
+        exists v, dexpr m l br v /\
+        (word.unsigned v <> 0%Z -> rec ct t m l post) /\
+        (word.unsigned v = 0%Z -> rec cf t m l post)
+      | cmd.seq c1 c2 =>
+        rec c1 t m l (fun t m l => rec c2 t m l post)
+      | cmd.while _ _ => Semantics.exec e c t m l post
+      | cmd.call binds fname arges =>
+        exists args, dexprs m l arges args /\
+        Semantics.call e fname t m args (fun t m rets =>
+          exists l', map.putmany_of_list_zip binds rets l = Some l' /\
+          post t m l')
+      | cmd.interact binds action arges =>
+        exists args, dexprs m l arges args /\
+        exists mKeep mGive, map.split m mKeep mGive /\
+        ext_spec t mGive action args (fun mReceive rets =>
+          exists l', map.putmany_of_list_zip binds rets l = Some l' /\
+          forall m', map.split m' mKeep mReceive ->
+          post (cons ((mGive, action, args), (mReceive, rets)) t) m' l')
+      end.
+    Fixpoint cmd c := cmd_body cmd c.
+  End WithFunctions.
+
+  Definition func call '(innames, outnames, c) (t : trace) (m : mem) (args : list word) (post : trace -> mem -> list word -> Prop) :=
+      exists l, map.of_list_zip innames args = Some l /\
+      cmd call c t m l (fun t m l =>
+        list_map (get l) outnames (fun rets =>
+        post t m rets)).
+
+  Definition program := cmd.
+End WeakestPrecondition.
+Notation call := Semantics.call (only parsing).
+
+Ltac unfold1_cmd e :=
+  lazymatch e with
+    @cmd ?width ?BW ?word ?mem ?locals ?ext_spec ?CA ?c ?t ?m ?l ?post =>
+    let c := eval hnf in c in
+    constr:(@cmd_body width BW word mem locals ext_spec CA
+                      (@cmd width BW word mem locals ext_spec CA) c t m l post)
+  end.
+Ltac unfold1_cmd_goal :=
+  let G := lazymatch goal with |- ?G => G end in
+  let G := unfold1_cmd G in
+  change G.
+
+Ltac unfold1_expr e :=
+  lazymatch e with
+    @expr ?width ?word ?mem ?locals ?m ?l ?arg ?post =>
+    let arg := eval hnf in arg in
+    constr:(@expr_body width word mem locals m l (@expr width word mem locals m l) arg post)
+  end.
+Ltac unfold1_expr_goal :=
+  let G := lazymatch goal with |- ?G => G end in
+  let G := unfold1_expr G in
+  change G.
+
+Ltac unfold1_list_map e :=
+  lazymatch e with
+    @list_map ?A ?B ?P ?arg ?post =>
+    let arg := eval hnf in arg in
+    constr:(@list_map_body A B P (@list_map A B P) arg post)
+  end.
+Ltac unfold1_list_map_goal :=
+  let G := lazymatch goal with |- ?G => G end in
+  let G := unfold1_list_map G in
+  change G.
+
+Import Coq.ZArith.ZArith.
+
+Notation "'fnspec!' name a0 .. an '/' g0 .. gn '~>' r0 .. rn ',' '{' 'requires' tr mem := pre ';' 'ensures' tr' mem' ':=' post '}'" :=
+  (fun functions =>
+     (forall a0,
+        .. (forall an,
+              (forall g0,
+                  .. (forall gn,
+                         (forall tr mem,
+                             pre ->
+                             WeakestPrecondition.call
+                               functions name tr mem (cons a0 .. (cons an nil) ..)
+                               (fun tr' mem' rets =>
+                                  (exists r0,
+                                      .. (exists rn,
+                                             rets = (cons r0 .. (cons rn nil) ..) /\
+                                             post) ..)))) ..)) ..))
+    (at level 200,
+     name at level 0,
+     a0 binder, an binder,
+     g0 binder, gn binder,
+     r0 closed binder, rn closed binder,
+     tr name, tr' name, mem name, mem' name,
+     pre at level 200,
+     post at level 200).
+
+Notation "'fnspec!' name a0 .. an '/' g0 .. gn ',' '{' 'requires' tr mem := pre ';' 'ensures' tr' mem' ':=' post '}'" :=
+  (fun functions =>
+     (forall a0,
+        .. (forall an,
+              (forall g0,
+                  .. (forall gn,
+                         (forall tr mem,
+                             pre ->
+                             WeakestPrecondition.call
+                               functions name tr mem (cons a0 .. (cons an nil) ..)
+                               (fun tr' mem' rets =>
+                                  rets = nil /\ post))) ..)) ..))
+    (at level 200,
+     name at level 0,
+     a0 binder, an binder,
+     g0 binder, gn binder,
+     tr name, tr' name, mem name, mem' name,
+     pre at level 200,
+     post at level 200).
+
+Notation "'fnspec!' name a0 .. an '~>' r0 .. rn ',' '{' 'requires' tr mem := pre ';' 'ensures' tr' mem' ':=' post '}'" :=
+  (fun functions =>
+     (forall a0,
+        .. (forall an,
+               (forall tr mem,
+                   pre ->
+                   WeakestPrecondition.call
+                     functions name tr mem (cons a0 .. (cons an nil) ..)
+                     (fun tr' mem' rets =>
+                        (exists r0,
+                            .. (exists rn,
+                                   rets = (cons r0 .. (cons rn nil) ..) /\
+                                   post) ..)))) ..))
+    (at level 200,
+     name at level 0,
+     a0 binder, an binder,
+     r0 closed binder, rn closed binder,
+     tr name, tr' name, mem name, mem' name,
+     pre at level 200,
+     post at level 200).
+
+Notation "'fnspec!' name '/' g0 .. gn '~>' r0 .. rn ',' '{' 'requires' tr mem := pre ';' 'ensures' tr' mem' ':=' post '}'" :=
+  (fun functions =>
+        (forall an,
+              (forall g0,
+                  .. (forall gn,
+                         (forall tr mem,
+                             pre ->
+                             WeakestPrecondition.call
+                               functions name tr mem nil
+                               (fun tr' mem' rets =>
+                                  (exists r0,
+                                      .. (exists rn,
+                                             rets = (cons r0 .. (cons rn nil) ..) /\
+                                             post) ..)))) ..)))
+    (at level 200,
+     name at level 0,
+     g0 binder, gn binder,
+     r0 closed binder, rn closed binder,
+     tr name, tr' name, mem name, mem' name,
+     pre at level 200,
+     post at level 200).
+
+Notation "'fnspec!' name a0 .. an ',' '{' 'requires' tr mem := pre ';' 'ensures' tr' mem' ':=' post '}'" :=
+  (fun functions =>
+     (forall a0,
+        .. (forall an,
+               (forall tr mem,
+                   pre ->
+                   WeakestPrecondition.call
+                     functions name tr mem (cons a0 .. (cons an nil) ..)
+                     (fun tr' mem' rets =>
+                        rets = nil /\ post))) ..))
+    (at level 200,
+     name at level 0,
+     a0 binder, an binder,
+     tr name, tr' name, mem name, mem' name,
+     pre at level 200,
+     post at level 200).
+
+Notation "'fnspec!' name '/' g0 .. gn ',' '{' 'requires' tr mem := pre ';' 'ensures' tr' mem' ':=' post '}'" :=
+  (fun functions =>
+              (forall g0,
+                  .. (forall gn,
+                         (forall tr mem,
+                             pre ->
+                             WeakestPrecondition.call
+                               functions name tr mem nil
+                               (fun tr' mem' rets =>
+                                  rets = nil /\ post))) ..))
+    (at level 200,
+     name at level 0,
+     g0 binder, gn binder,
+     tr name, tr' name, mem name, mem' name,
+     pre at level 200,
+     post at level 200).
+
+Notation "'fnspec!' name '~>' r0 .. rn ',' '{' 'requires' tr mem := pre ';' 'ensures' tr' mem' ':=' post '}'" :=
+  (fun functions =>
+     (forall tr mem,
+         pre ->
+         WeakestPrecondition.call
+           functions name tr mem nil
+           (fun tr' mem' rets =>
+              (exists r0,
+                  .. (exists rn,
+                         rets = (cons r0 .. (cons rn nil) ..) /\
+                         post) ..))))
+    (at level 200,
+     name at level 0,
+     r0 closed binder, rn closed binder,
+     tr name, tr' name, mem name, mem' name,
+     pre at level 200,
+     post at level 200).
