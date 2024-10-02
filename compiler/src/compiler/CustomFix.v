16a43d33c5f530882a21e6a4286224c3d97f0a50
Owen Conoly <otconoly@hotmail.com>
2024-10-02 17:44:21 -0400

    new version of Fix with dependent types as well as arbitrary equivalence relations

--- compiler/src/compiler/CustomFix.v
+++ compiler/src/compiler/CustomFix.v
@@ -107,6 +107,60 @@ Qed.
 
 End FixPoint.
 
+Section FixPoint.
+
+Variable A : Type.
+Variable R : A -> A -> Prop.
+Hypothesis Rwf : well_founded R.
+Variable P : A -> Type. (* really I want to say that P gives one type for each equivalence class
+                      of A wrt the equivalence relation E.  Not clear how to say this though.*)
+Variable F : forall x:A, (forall y:A, R y x -> P y) -> P x.
+Variable E1 : A -> A -> Prop.
+Variable E2 : forall a1 a2, P a1 -> P a2 -> Prop.
+
+(* No need to make this my own thing; should just use Fix_F and assume it's non-dependent.*)
+
+Fixpoint my_Fix_F' (x:A) (a:Acc R x) : P x :=
+  F x (fun (y:A) (h:R y x) => my_Fix_F' y (Acc_inv a h)).
+
+Lemma my_Fix_F_eq' (x:A) (r:Acc R x) :
+  F x (fun (y:A) (p:R y x) => my_Fix_F' y (Acc_inv r p)) = my_Fix_F' x r.
+Proof.
+  destruct r using Acc_inv_dep; auto.
+Qed.
+
+Definition my_Fix' (x:A) := my_Fix_F' x (Rwf x).
+
+(** Proof that [well_founded_induction] satisfies the fixpoint equation.
+    It requires an extra property of the functional *) Check E2.
+
+Hypothesis
+  F_ext :
+  forall (x1 x2:A) (f1:forall y:A, R y x1 -> P y) (f2:forall y:A, R y x2 -> P y),
+    E1 x1 x2 ->
+    (forall (y1 y2:A) (p1:R y1 x1) (p2:R y2 x2),
+        E1 y1 y2 -> E2 y1 y2 (f1 y1 p1) (f2 y2 p2)) -> E2 x1 x2 (F x1 f1) (F x2 f2).
+
+Lemma my_Fix_F_inv' : forall (x1 x2:A) (r1:Acc R x1) (r2:Acc R x2),
+    E1 x1 x2 -> E2 _ _ (my_Fix_F' x1 r1) (my_Fix_F' x2 r2).
+Proof.
+  intro x1; induction (Rwf x1); intros x2 r1 r2.
+  rewrite <- (my_Fix_F_eq' x r1); rewrite <- (my_Fix_F_eq' x2 r2); intros.
+  apply F_ext; auto.
+Qed.
+
+Lemma my_Fix_eq' : forall (x1 x2:A),
+    E1 x1 x2 -> E2 _ _ (my_Fix' x1) (F x2 (fun (y:A) (p:R y x2) => my_Fix' y)).
+Proof.
+  intro x; unfold my_Fix'.
+  rewrite <- my_Fix_F_eq'.
+  intros. apply F_ext; intros.
+  - assumption.
+  - apply my_Fix_F_inv'. assumption.
+Qed.
+
+End FixPoint.
+
 Require Import bedrock2.Semantics.
 Require Import compiler.FlatImp.
 Require Import Coq.ZArith.ZArith.
