257f5446b25440a27e2be37866a7f00398cf6df2
Andres Erbsen <andres-github@andres.systems>
2025-06-23 12:51:01 -0400

    add wsize, make BasicC64Semantics Export (#469)

--- bedrock2/src/bedrock2/BasicC64Semantics.v
+++ bedrock2/src/bedrock2/BasicC64Semantics.v
@@ -5,19 +5,20 @@ Require Import coqutil.Word.Interface coqutil.Map.SortedListWord.
 Require coqutil.Word.Naive.
 Require Export coqutil.Word.Bitwidth64.
 
-#[global] Instance word: word.word 64 := Naive.word64.
-#[global] Instance mem: Interface.map.map word Byte.byte := SortedListWord.map _ _.
-#[global] Instance locals: Interface.map.map String.string word := SortedListString.map _.
-#[global] Instance env: Interface.map.map String.string (list String.string * list String.string * cmd) :=
+#[export] Existing Instances Bitwidth64.BW64.
+#[export] Instance word: word.word 64 := Naive.word64.
+#[export] Instance mem: Interface.map.map word Byte.byte := SortedListWord.map _ _.
+#[export] Instance locals: Interface.map.map String.string word := SortedListString.map _.
+#[export] Instance env: Interface.map.map String.string (list String.string * list String.string * cmd) :=
   SortedListString.map _.
-#[global] Instance ext_spec: ExtSpec := fun _ _ _ _ _ => False.
+#[export] Instance ext_spec: ExtSpec := fun _ _ _ _ _ => False.
 
 Arguments word: simpl never.
 Arguments mem: simpl never.
 Arguments locals: simpl never.
 Arguments env: simpl never.
 
-#[global] Instance weaken_ext_spec trace m0 act args :
+#[export] Instance weaken_ext_spec trace m0 act args :
   Morphisms.Proper
     (Morphisms.respectful
        (Morphisms.pointwise_relation Interface.map.rep
@@ -30,16 +31,16 @@ Proof.
   assumption.
 Qed.
 
-#[global] Instance localsok: coqutil.Map.Interface.map.ok locals := SortedListString.ok _.
-#[global] Instance envok: coqutil.Map.Interface.map.ok env := SortedListString.ok _.
-#[global] Instance mapok: coqutil.Map.Interface.map.ok mem := SortedListWord.ok Naive.word64 _.
-#[global] Instance wordok: coqutil.Word.Interface.word.ok word := Naive.word64_ok.
+#[export] Instance localsok: coqutil.Map.Interface.map.ok locals := SortedListString.ok _.
+#[export] Instance envok: coqutil.Map.Interface.map.ok env := SortedListString.ok _.
+#[export] Instance mapok: coqutil.Map.Interface.map.ok mem := SortedListWord.ok Naive.word64 _.
+#[export] Instance wordok: coqutil.Word.Interface.word.ok word := Naive.word64_ok.
 Add Ring wring : (Properties.word.ring_theory (word := word))
       (preprocess [autorewrite with rew_word_morphism],
        morphism (Properties.word.ring_morph (word := word)),
        constants [Properties.word_cst]).
 
-#[global] Instance ext_spec_ok : ext_spec.ok ext_spec.
+#[export] Instance ext_spec_ok : ext_spec.ok ext_spec.
 Proof.
   constructor; intros; try contradiction.
   apply weaken_ext_spec.
--- bedrock2/src/bedrock2Examples/shrd.v
+++ bedrock2/src/bedrock2Examples/shrd.v
@@ -1,4 +1,4 @@
-Require Import bedrock2.NotationsCustomEntry.
+Require Import bedrock2.NotationsCustomEntry bedrock2.wsize.
 
 Import Syntax BinInt String.
 Local Open Scope string_scope.
@@ -8,34 +8,46 @@ Definition shrd :=
   func! (lo, hi, n) ~> (res) {
       res = lo >> n;
       if n {
-        res = hi << ($64 - n) | res
+        res = hi << ($wsize - n) | res
       }
     }.
 
-From bedrock2 Require Import WeakestPrecondition ProgramLogic BasicC64Semantics.
+Import coqutil.Word.Interface coqutil.Word.Bitwidth coqutil.Map.Interface Coq.Init.Byte Coq.Strings.String.
+Import coqutil.Map.SortedListString. Local Existing Instances SortedListString.map SortedListString.ok.
+From bedrock2 Require Import Semantics WeakestPrecondition ProgramLogic.
 Import ProgramLogic.Coercions.
 
-Local Instance spec_of_shrd : spec_of "shrd" :=
+Section WithSemantics.
+Context {width} {BW: Bitwidth width}.
+Context {word: word.word width} {mem: map.map word byte}.
+Context {ext_spec: ExtSpec} {ext_spec_ok : ext_spec.ok ext_spec}.
+Context {word_ok: word.ok word} {mem_ok: map.ok mem}.
+
+Definition spec_of_shrd : spec_of "shrd" :=
   fnspec! "shrd" (lo hi n : word) ~> res,
-    { requires t m := n < 64;
+    { requires t m := n < width;
       ensures T M :=
         M = m /\ T = t /\
-        word.unsigned res = Z.lor (Z.shiftl hi (64 - n)) (Z.shiftr lo n) mod 2^64
+        word.unsigned res = Z.lor (Z.shiftl hi (width - n)) (Z.shiftr lo n) mod 2^width
     }.
 
 Require Import bedrock2.ZnWords Coq.ZArith.ZArith Lia.
 
-Lemma shrd_ok : program_logic_goal_for_function! shrd.
+Local Ltac mlia := case BW as [ [ -> | -> ] ]; ZnWords.
+
+Lemma shrd_ok :
+  let '_ := spec_of_shrd in
+  program_logic_goal_for_function! shrd.
 Proof.
   repeat (straightline || apply WeakestPreconditionProperties.dexpr_expr).
   split; intro n_cond; repeat (straightline || unfold cmd_body).
   {
-    cbv [v res].
-    rewrite word.unsigned_or, word.unsigned_slu, word.unsigned_sub, word.unsigned_of_Z, word.unsigned_sru by ZnWords.
+    subst v res. cbn [interp_op1]; rewrite eval_wsize'.
+    rewrite word.unsigned_or, word.unsigned_slu, word.unsigned_sub, word.unsigned_of_Z, word.unsigned_sru by mlia.
     cbv [word.wrap].
-    change (64 mod 2^64) with 64.
-    rewrite (Z.mod_small (64 - _)) by ZnWords.
-    rewrite <-!Z.land_ones, !Z.land_lor_distr_l, !Z.land_ones by lia.
+    
+    rewrite (Z.mod_small width), (Z.mod_small (_ - _)) by mlia.
+    rewrite <-!Z.land_ones, !Z.land_lor_distr_l, !Z.land_ones by mlia.
     rewrite !Zmod_mod.
     trivial.
   }
@@ -43,8 +55,7 @@ Proof.
     cbv [res].
     rewrite word.unsigned_sru by lia.
     cbv [word.wrap].
-    rewrite n_cond, Z.shiftr_0_r.
-    change (64 - 0) with 64.
+    rewrite n_cond, Z.shiftr_0_r, Z.sub_0_r.
     rewrite <-!Z.land_ones, !Z.land_lor_distr_l, !Z.land_ones by lia.
     rewrite Z.shiftl_mul_pow2, Z.mod_mul by lia.
     rewrite Z.lor_0_l.
@@ -52,40 +63,41 @@ Proof.
   }
 Qed.
 
-Section ArithmeticSpec.
-  Local Instance spec_of_shrd_arith : spec_of "shrd" :=
-    fnspec! "shrd" (lo hi n : word) ~> res,
-      { requires t m := word.unsigned n < 64;
-        ensures T M :=
-          M = m /\ T = t /\
-          res = (lo + 2^64 * hi) / 2^n mod 2^64 :> Z
-      }.
+Definition spec_of_shrd_arith : spec_of "shrd" :=
+  fnspec! "shrd" (lo hi n : word) ~> res,
+    { requires t m := word.unsigned n < width;
+      ensures T M :=
+        M = m /\ T = t /\
+        res = (lo + 2^width * hi) / 2^n mod 2^width :> Z
+    }.
 
-  Lemma shdr_ok_arith : program_logic_goal_for_function! shrd.
-  Proof.
-    cbv [program_logic_goal_for spec_of_shrd_arith].
-    intros.
-    eapply WeakestPreconditionProperties.Proper_call; [|apply shrd_ok]; trivial.
-    intros ? ? ? spec.
-    destruct spec as (res & ? & ? & ? & spec).
-    exists res.
-    repeat split; trivial.
-    rewrite spec.
-    rewrite <-BitOps.or_to_plus.
-    {
-      rewrite Z.mul_comm.
-      rewrite <-Z.shiftl_mul_pow2 by lia.
-      rewrite <-Z.shiftr_div_pow2, Z.shiftr_lor, Z.shiftr_shiftl_l by ZnWords.
-      rewrite Z.lor_comm.
-      trivial.
-    }
-    {
-      rewrite <-Properties.word.wrap_unsigned.
-      rewrite <-Z.land_ones by lia.
-      rewrite <-Z.land_assoc, (Z.land_comm (Z.ones 64) _).
-      rewrite Z.land_ones, Z.mul_comm, Z.mod_mul by lia.
-      rewrite Z.land_0_r.
-      trivial.
-    }
-  Qed.
-End ArithmeticSpec.
+Lemma shdr_ok_arith :
+  let '_ := spec_of_shrd_arith  in
+  program_logic_goal_for_function! shrd.
+Proof.
+  cbv [program_logic_goal_for spec_of_shrd_arith].
+  intros.
+  eapply WeakestPreconditionProperties.Proper_call; [|apply shrd_ok]; trivial.
+  intros ? ? ? spec.
+  destruct spec as (res & ? & ? & ? & spec).
+  exists res.
+  repeat split; trivial.
+  rewrite spec.
+  rewrite <-BitOps.or_to_plus.
+  {
+    rewrite Z.mul_comm.
+    rewrite <-Z.shiftl_mul_pow2 by mlia.
+    rewrite <-Z.shiftr_div_pow2, Z.shiftr_lor, Z.shiftr_shiftl_l by ZnWords.
+    rewrite Z.lor_comm.
+    trivial.
+  }
+  {
+    rewrite <-Properties.word.wrap_unsigned.
+    rewrite <-Z.land_ones by mlia.
+    rewrite <-Z.land_assoc, (Z.land_comm (Z.ones width) _).
+    rewrite Z.land_ones, Z.mul_comm, Z.mod_mul by mlia.
+    rewrite Z.land_0_r.
+    trivial.
+  }
+Qed.
+End WithSemantics.
