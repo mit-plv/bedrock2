3700339d151297247853ccaebc49d54b01453446
Owen Conoly <otconoly@hotmail.com>
2025-06-26 19:59:03 -0400

    more small-step stuff

--- compiler/src/compiler/SmallStep.v
+++ compiler/src/compiler/SmallStep.v
@@ -3,15 +3,16 @@ Require Import coqutil.sanity coqutil.Byte.
 Require Import bedrock2.MetricLeakageSemantics.
 Require Import BinIntDef coqutil.Word.Interface coqutil.Word.Bitwidth.
 Require Import coqutil.Map.Interface.
-Print runsTo.
+
 Section step.
   Context (State : Type) (step : State -> (State -> Prop) -> Prop).
   Context {width: Z} {BW: Bitwidth width} {word: word.word width} {mem: map.map word byte}.
 
   Definition State' : Type := State * AEP.
   Inductive step' : State' -> (State' -> Prop) -> Prop :=
-  | step_usual s1 aep P : step s1 (fun s2 => P (s2, aep)) ->
-                          step' (s1, aep) P
+  | step_usual s aep mid P : step s mid ->
+                          (forall s', mid s' -> P (s', aep)) -> 
+                          step' (s, aep) P
   | step_A s aep P : (forall x, P (s, aep x)) ->
                      step' (s, AEP_A aep) P
   | step_E s aep P : (exists x, P (s, aep x)) ->
@@ -24,19 +25,17 @@ Section step.
     | AEP_P X => fun post => fun s' => post X s'
     end.
 
-  Lemma step_impl_step' s aep post :
+  Lemma step_impl_step' s aep post P :
     runsTo step s post ->
-    runsTo step' (s, aep) (fun '(s', aep') => post s' /\ aep' = aep).
+    (forall s', post s' -> P (s', aep)) ->
+    runsTo step' (s, aep) P.
   Proof.
     intros H. induction H.
-    - apply runsToDone. auto.
-    - eapply runsTo_weaken.
-      { apply runsToStep_cps. apply step_usual. eapply weaken_step. 2: eassumption.
-        eassumption. }
-      simpl. auto.
+    - intros. apply runsToDone. auto.
+    - intros. apply runsToStep_cps. eapply step_usual. 1: eassumption. eauto.
   Qed.
   
-  Lemma step'_to_step s aep post : runsTo step' (s, aep)
+  Lemma step'_iff_step s aep post : runsTo step' (s, aep)
                                 (fun '(s', aep') =>
                                    match aep' with
                                    | AEP_P P => post P s'
@@ -56,11 +55,109 @@ Section step.
         apply runsToDone. assumption.
       + destruct initial. specialize H1 with (2 := X). clear X.
         simpl. inversion H; subst.
-        -- eapply runsToStep. 1: eassumption. simpl. intros. apply H1 in H2. apply H2.
+        -- eapply runsToStep. 1: eassumption. intros. apply H6 in H2. apply H1 in H2. apply H2.
         -- apply runsToDone. simpl. intros. specialize (H5 n). apply H1 in H5. apply H5.         -- apply runsToDone. destruct H5 as [x H5]. simpl. exists x. apply H1 in H5. apply H5.
     - intros H. remember (post_of _ _) as post'.
       assert (Hpost' : forall s, post' s -> post_of aep post s) by (subst; auto).
       clear Heqpost'. revert aep Hpost'. induction H; intros aep Hpost.
-      + apply Hpost in H. clear Hpost P. induction aep.
-        -- apply runsToStep_cps. apply step_A. intros x. simpl in H. specialize (H x).
-           specialize (H0 x). apply runsTo_trans_cps. apply runsTo_weaken. apply step_impl_step'. apply step_A. intros. apply H0. simpl in *.
+      + apply Hpost in H. clear Hpost P. revert initial H.
+        induction aep; intros initial Haep.
+        -- apply runsToStep_cps. apply step_A. intros x.
+           simpl in Haep. specialize (Haep x).
+           apply runsTo_trans_cps. eapply step_impl_step'. 1: eassumption.
+           intros ? H1. specialize H with (1 := H1). apply H.
+        -- apply runsToStep_cps. apply step_E.
+           simpl in Haep. destruct Haep as [x Haep]. exists x.
+           apply runsTo_trans_cps. eapply step_impl_step'. 1: eassumption.
+           intros ? H1. specialize H with (1 := H1). apply H.
+        -- apply runsToDone. simpl in Haep. apply Haep.
+      + apply runsToStep_cps. eapply step_usual. 1: eassumption. intros s' Hs'.
+        specialize H1 with (1 := Hs') (2 := Hpost). apply H1.
+  Qed.
+
+  CoInductive stream (T : Type) :=
+  | scons (a : T) (rest : stream T).
+
+  Fixpoint nth {T : Type} (n : nat) (s : stream T) : T :=
+    match s, n with
+    | scons _ a rest, S n' => nth n' rest
+    | scons _ a rest, O => a
+    end.
+  
+  CoInductive possible {T : Type} (step_ : T -> T -> Prop) : stream T -> Prop :=
+  | poss a b rest : step_ b a ->
+                    possible step_ (scons _ b rest) ->
+                    possible step_ (scons _ a (scons _ b rest)).
+
+  Fixpoint trace_pred_of {T : Type} aep : _ -> stream T -> Prop :=
+    match aep with
+    | AEP_A aep' => fun post => fun str => forall x, trace_pred_of (aep' x) post str
+    | AEP_E aep' => fun post => fun str => exists x, trace_pred_of (aep' x) post str
+    | AEP_P X => fun post => fun str => exists n, post X (nth n str)
+    end.
+
+  Require Import Coq.Logic.ChoiceFacts.
+  Require Import Coq.Logic.ClassicalFacts.
+  Lemma naen (A : Type) (P : A -> Prop) :
+    excluded_middle ->
+    ~(forall y, P y) ->
+    exists y, ~P y.
+  Proof.
+    intros em. clear -em. cbv [excluded_middle]. intros H. assert (H1 := em (exists y, ~P y)).
+    destruct H1 as [H1|H1].
+    - assumption.
+    - exfalso. apply H. clear H. intros y. assert (H2 := em (P y)).
+      destruct H2 as [H2|H2].
+      + assumption.
+      + exfalso. apply H1. exists y. assumption.
+  Qed.
+
+  Lemma chains_finite_implies_Acc' (A : Type) (R : A -> A -> Prop) x str :
+    nth O str = x ->
+    possible R str ->
+    ~ Acc R x.
+  Proof.
+    intros H1 H2 H3. revert str H1 H2. induction H3. intros str H1 H2. inversion H2.
+    subst. simpl in *. specialize H0 with (3 := H4). specialize H0 with (2 := eq_refl).
+    apply H0. apply H3.
+  Qed.
+
+  Lemma notnot :
+    excluded_middle ->
+    forall P,
+    ~~P ->
+    P.
+  Proof. intros em P nnP. specialize (em P). tauto. Qed.
+
+  Lemma chains_finite_implies_Acc (A : Type) (R : A -> A -> Prop) x :
+    excluded_middle ->
+    ~ (forall str, possible R str -> nth O str = x -> False) ->
+    Acc R x.
+  Proof.
+    intros em H. apply (notnot em). intros H'. apply H. intros. clear H. Search excluded_middle. Print GodedlDummett.
+
+  Lemma runsTo_iff_trace_pred {T : Type} (step_ : T -> _ -> Prop) s P :
+    runsTo step_ s P <-> (forall str, possible step_ str ->
+                               nth O str = s ->
+                               exists n, P (nth n str)).
+  Proof.
+    split; intros H.
+    - induction H.
+      + intros. destruct str; inversion H1. subst. exists O. simpl. assumption.
+      + intros. inversion H2. subst. 
+        specialize H4 with (1 := H). specialize H0 with (1 := H4).
+        specialize H1 with (1 := H4). specialize H1 with (1 := H5).
+        specialize (H1 eq_refl). destruct H1 as [n H1]. exists (S n). simpl. assumption.
+    - 
+        
+  Lemma step'_iff_trace_pred s aep post :
+    runsTo step' (s, aep)
+      (fun '(s', aep') =>
+         match aep' with
+         | AEP_P P => post P s'
+         | _ => False
+         end) <->
+      (forall str, possible str -> trace_pred_of aep post str).
+  Proof.
+    split; intros H.
+    - intros str Hstr.
