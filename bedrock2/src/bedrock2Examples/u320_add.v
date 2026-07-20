16f1c978cc4bb6ce2d68cfcf38fac67d0cafd569
Klinashka <klinashka@google.com>
2026-07-20 12:32:48 +0000

    Changed function to store sum in first parameter.

--- bedrock2/src/bedrock2Examples/u320_add.v
+++ bedrock2/src/bedrock2Examples/u320_add.v
@@ -9,20 +9,19 @@ Local Notation eval := (fold_right (fun (a : word) (s : Z) => a + 2^64*s) 0).
 Local Notation array := (array scalar (word.of_Z 8)).
 
 Local Instance spec_of_u320_add : spec_of "u320_add" := 
-    fnspec! "u320_add" (p_x p_y p_r : word) / (x y r : list word) R ~> b,
+    fnspec! "u320_add" (p_x p_y : word) / (x y r : list word) R ~> b,
     {
         requires t m :=
-            m =* array p_x x ⋆ array p_y y ⋆ array p_r r ⋆ R 
-            /\ length x = 5%nat /\ length y = 5%nat /\ 
-            length r = 5%nat ;
-        ensures T M := T = t /\ exists (r : list word), M =* array p_x x ⋆ array p_y y ⋆ array p_r r ⋆ R /\ 
+            m =* array p_x x ⋆ array p_y y ⋆ R 
+            /\ length x = 5%nat /\ length y = 5%nat ;
+        ensures T M := T = t /\ exists (r : list word), M =* array p_x r ⋆ array p_y y ⋆ R /\ 
         length r = 5%nat /\ 2^320*b + eval r = eval x + eval y  
     }.
 
 (** * Implementation *)
 Require Import bedrock2.NotationsCustomEntry bedrock2Examples.full_add.
 
-Definition u320_add := func! (p_x, p_y, p_r) ~> b {
+Definition u320_add := func! (p_x, p_y) ~> b {
     b = $0;
     unpack! s0, b = br_full_add(load(p_x), load(p_y), b);
     unpack! s1, b = br_full_add(load(p_x + $8), load(p_y + $8), b);
@@ -30,11 +29,11 @@ Definition u320_add := func! (p_x, p_y, p_r) ~> b {
     unpack! s3, b = br_full_add(load(p_x + $8 + $8 + $8), load(p_y + $8 + $8 + $8), b);
     unpack! s4, b = br_full_add(load(p_x + $8 + $8 + $8 + $8), load(p_y + $8 + $8 + $8 + $8), b);
 
-    store(p_r, s0);
-    store(p_r + $8, s1);
-    store(p_r + $8 + $8, s2);
-    store(p_r + $8 + $8 + $8, s3);
-    store(p_r + $8 + $8 + $8 + $8, s4)
+    store(p_x, s0);
+    store(p_x + $8, s1);
+    store(p_x + $8 + $8, s2);
+    store(p_x + $8 + $8 + $8, s3);
+    store(p_x + $8 + $8 + $8 + $8, s4)
 }.
 
 (** * Proof *)
